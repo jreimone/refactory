da9861226e4392bf8b55f435fb7ec7558808925f
jreimann <jreimann@2290cd21-d4b4-4435-9c65-06e28a4c4f12>
2010-01-28 18:39:36 +0000

    PL/0 language resources plugin

---
+++ trunk/Refactoring/Languages/org.emftext.language.pl0.resource.pl0/src-gen/org/emftext/language/pl0/resource/pl0/mopp/Pl0FoldingInformationProvider.java
@@ -0,0 +1,16 @@
+/**
+ * <copyright>
+ * </copyright>
+ *
+ * 
+ */
+package org.emftext.language.pl0.resource.pl0.mopp;
+
+public class Pl0FoldingInformationProvider {
+	
+	public org.eclipse.emf.ecore.EClass[] getFoldableClasses() {
+		return new org.eclipse.emf.ecore.EClass[] {
+		};
+	}
+	
+}
