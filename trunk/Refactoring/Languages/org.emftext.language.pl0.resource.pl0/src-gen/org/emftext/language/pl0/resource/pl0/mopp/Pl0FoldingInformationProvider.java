e121c25caf7045721450cb1eb9f2f4a17cbab0f6
jreimann <jreimann@2290cd21-d4b4-4435-9c65-06e28a4c4f12>
2010-02-01 10:34:53 +0000

    Foldable

--- trunk/Refactoring/Languages/org.emftext.language.pl0.resource.pl0/src-gen/org/emftext/language/pl0/resource/pl0/mopp/Pl0FoldingInformationProvider.java
+++ trunk/Refactoring/Languages/org.emftext.language.pl0.resource.pl0/src-gen/org/emftext/language/pl0/resource/pl0/mopp/Pl0FoldingInformationProvider.java
@@ -10,6 +10,9 @@ public class Pl0FoldingInformationProvider {
 	
 	public org.eclipse.emf.ecore.EClass[] getFoldableClasses() {
 		return new org.eclipse.emf.ecore.EClass[] {
+			org.emftext.language.pl0.PL0Package.eINSTANCE.getProgram(),
+			org.emftext.language.pl0.PL0Package.eINSTANCE.getProcedureDeclaration(),
+			org.emftext.language.pl0.PL0Package.eINSTANCE.getBody(),
 		};
 	}
 	
