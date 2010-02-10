373e1a11922123e96dbcc12c808fdf087979c32a
jreimann <jreimann@2290cd21-d4b4-4435-9c65-06e28a4c4f12>
2010-02-10 22:24:57 +0000

    test project and files

---
+++ trunk/Refactoring/Tests/EMFTextTest/syntax/java.newfile.java
@@ -0,0 +1,19 @@
+public class HelloWorld {
+	
+	private String aName = "Jan";
+	
+	public static void main(String[] args) {
+		System.out.println("Hello world!");
+		printName();
+		printName();printName();
+	}
+	
+	private void printName(){
+		System.out.println("Hello world!");
+		System.out.println("Hello world!");
+		System.out.print(aName);
+		System.out.print(aName);
+		System.out.print(aName);
+		
+	}
+}
