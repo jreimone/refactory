3f25a0d75c038c8038bdb7b337796e1a45264db1
Jan <jreimone@users.noreply.github.com>
2010-02-11 18:59:56 +0000

    began with cs

---
+++ Refactoring/Plugins/org.emftext.language.refactoring.rolemapping/src-gen/org/emftext/language/refactoring/rolemapping/Mapping.java
@@ -0,0 +1,102 @@
+/**
+ * <copyright>
+ * </copyright>
+ *
+ * $Id$
+ */
+package org.emftext.language.refactoring.rolemapping;
+
+import org.eclipse.emf.common.util.EList;
+
+import org.eclipse.emf.ecore.EObject;
+
+import org.emftext.language.refactoring.roles.RoleModel;
+
+/**
+ * <!-- begin-user-doc -->
+ * A representation of the model object '<em><b>Mapping</b></em>'.
+ * <!-- end-user-doc -->
+ *
+ * <p>
+ * The following features are supported:
+ * <ul>
+ *   <li>{@link org.emftext.language.refactoring.rolemapping.Mapping#getName <em>Name</em>}</li>
+ *   <li>{@link org.emftext.language.refactoring.rolemapping.Mapping#getMappedRoleModel <em>Mapped Role Model</em>}</li>
+ *   <li>{@link org.emftext.language.refactoring.rolemapping.Mapping#getRoleToMetaelement <em>Role To Metaelement</em>}</li>
+ * </ul>
+ * </p>
+ *
+ * @see org.emftext.language.refactoring.rolemapping.RolemappingPackage#getMapping()
+ * @model
+ * @generated
+ */
+public interface Mapping extends EObject {
+	/**
+   * Returns the value of the '<em><b>Name</b></em>' attribute.
+   * <!-- begin-user-doc -->
+	 * <p>
+	 * If the meaning of the '<em>Name</em>' attribute isn't clear,
+	 * there really should be more of a description here...
+	 * </p>
+	 * <!-- end-user-doc -->
+   * @return the value of the '<em>Name</em>' attribute.
+   * @see #setName(String)
+   * @see org.emftext.language.refactoring.rolemapping.RolemappingPackage#getMapping_Name()
+   * @model required="true"
+   * @generated
+   */
+	String getName();
+
+	/**
+   * Sets the value of the '{@link org.emftext.language.refactoring.rolemapping.Mapping#getName <em>Name</em>}' attribute.
+   * <!-- begin-user-doc -->
+	 * <!-- end-user-doc -->
+   * @param value the new value of the '<em>Name</em>' attribute.
+   * @see #getName()
+   * @generated
+   */
+	void setName(String value);
+
+	/**
+   * Returns the value of the '<em><b>Mapped Role Model</b></em>' reference.
+   * <!-- begin-user-doc -->
+	 * <p>
+	 * If the meaning of the '<em>Mapped Role Model</em>' reference isn't clear,
+	 * there really should be more of a description here...
+	 * </p>
+	 * <!-- end-user-doc -->
+   * @return the value of the '<em>Mapped Role Model</em>' reference.
+   * @see #setMappedRoleModel(RoleModel)
+   * @see org.emftext.language.refactoring.rolemapping.RolemappingPackage#getMapping_MappedRoleModel()
+   * @model required="true"
+   * @generated
+   */
+	RoleModel getMappedRoleModel();
+
+	/**
+   * Sets the value of the '{@link org.emftext.language.refactoring.rolemapping.Mapping#getMappedRoleModel <em>Mapped Role Model</em>}' reference.
+   * <!-- begin-user-doc -->
+	 * <!-- end-user-doc -->
+   * @param value the new value of the '<em>Mapped Role Model</em>' reference.
+   * @see #getMappedRoleModel()
+   * @generated
+   */
+	void setMappedRoleModel(RoleModel value);
+
+	/**
+   * Returns the value of the '<em><b>Role To Metaelement</b></em>' containment reference list.
+   * The list contents are of type {@link org.emftext.language.refactoring.rolemapping.ConcreteMapping}.
+   * <!-- begin-user-doc -->
+	 * <p>
+	 * If the meaning of the '<em>Role To Metaelement</em>' containment reference list isn't clear,
+	 * there really should be more of a description here...
+	 * </p>
+	 * <!-- end-user-doc -->
+   * @return the value of the '<em>Role To Metaelement</em>' containment reference list.
+   * @see org.emftext.language.refactoring.rolemapping.RolemappingPackage#getMapping_RoleToMetaelement()
+   * @model containment="true" required="true"
+   * @generated
+   */
+	EList<ConcreteMapping> getRoleToMetaelement();
+
+} // Mapping
---
+++ Refactoring/Plugins/org.emftext.language.refactoring.rolemapping/src-gen/org/emftext/language/refactoring/rolemapping/RolemappingFactory.java
@@ -0,0 +1,64 @@
+/**
+ * <copyright>
+ * </copyright>
+ *
+ * $Id$
+ */
+package org.emftext.language.refactoring.rolemapping;
+
+import org.eclipse.emf.ecore.EFactory;
+
+/**
+ * <!-- begin-user-doc -->
+ * The <b>Factory</b> for the model.
+ * It provides a create method for each non-abstract class of the model.
+ * <!-- end-user-doc -->
+ * @see org.emftext.language.refactoring.rolemapping.RolemappingPackage
+ * @generated
+ */
+public interface RolemappingFactory extends EFactory {
+	/**
+   * The singleton instance of the factory.
+   * <!-- begin-user-doc -->
+	 * <!-- end-user-doc -->
+   * @generated
+   */
+	RolemappingFactory eINSTANCE = org.emftext.language.refactoring.rolemapping.impl.RolemappingFactoryImpl.init();
+
+	/**
+   * Returns a new object of class '<em>Role Mapping Model</em>'.
+   * <!-- begin-user-doc -->
+	 * <!-- end-user-doc -->
+   * @return a new object of class '<em>Role Mapping Model</em>'.
+   * @generated
+   */
+	RoleMappingModel createRoleMappingModel();
+
+	/**
+   * Returns a new object of class '<em>Mapping</em>'.
+   * <!-- begin-user-doc -->
+	 * <!-- end-user-doc -->
+   * @return a new object of class '<em>Mapping</em>'.
+   * @generated
+   */
+	Mapping createMapping();
+
+	/**
+   * Returns a new object of class '<em>Concrete Mapping</em>'.
+   * <!-- begin-user-doc -->
+	 * <!-- end-user-doc -->
+   * @return a new object of class '<em>Concrete Mapping</em>'.
+   * @generated
+   */
+	ConcreteMapping createConcreteMapping();
+
+	/**
+   * Returns the package supported by this factory.
+   * <!-- begin-user-doc -->
+	 * <!-- end-user-doc -->
+   * @return the package supported by this factory.
+   * @generated
+   */
+	RolemappingPackage getRolemappingPackage();
+
+} //RolemappingFactory
