c1af1fc13bf2bd28b4401d0cff8de361d8e40563
mseifert <mseifert@2290cd21-d4b4-4435-9c65-06e28a4c4f12>
2010-02-12 13:38:43 +0000

    sharing text resource plug-in for role mappings

---
+++ trunk/Refactoring/Plugins/org.emftext.language.refactoring.rolemapping.resource.rolemapping/src/org/emftext/language/refactoring/rolemapping/resource/rolemapping/analysis/MappingMappedRoleModelReferenceResolver.java
@@ -0,0 +1,33 @@
+/*******************************************************************************
+ * Copyright (c) 2006-2010 
+ * Software Technology Group, Dresden University of Technology
+ * 
+ * All rights reserved. This program and the accompanying materials
+ * are made available under the terms of the Eclipse Public License v1.0 
+ * which accompanies this distribution, and is available at
+ * http://www.eclipse.org/legal/epl-v10.html
+ * 
+ * Contributors:
+ *   Software Technology Group - TU Dresden, Germany 
+ *      - initial API and implementation
+ ******************************************************************************/
+
+package org.emftext.language.refactoring.rolemapping.resource.rolemapping.analysis;
+
+public class MappingMappedRoleModelReferenceResolver implements org.emftext.language.refactoring.rolemapping.resource.rolemapping.IRolemappingReferenceResolver<org.emftext.language.refactoring.rolemapping.Mapping, org.emftext.language.refactoring.roles.RoleModel> {
+	
+	private org.emftext.language.refactoring.rolemapping.resource.rolemapping.analysis.RolemappingDefaultResolverDelegate<org.emftext.language.refactoring.rolemapping.Mapping, org.emftext.language.refactoring.roles.RoleModel> delegate = new org.emftext.language.refactoring.rolemapping.resource.rolemapping.analysis.RolemappingDefaultResolverDelegate<org.emftext.language.refactoring.rolemapping.Mapping, org.emftext.language.refactoring.roles.RoleModel>();
+	
+	public void resolve(java.lang.String identifier, org.emftext.language.refactoring.rolemapping.Mapping container, org.eclipse.emf.ecore.EReference reference, int position, boolean resolveFuzzy, final org.emftext.language.refactoring.rolemapping.resource.rolemapping.IRolemappingReferenceResolveResult<org.emftext.language.refactoring.roles.RoleModel> result) {
+		delegate.resolve(identifier, container, reference, position, resolveFuzzy, result);
+	}
+	
+	public java.lang.String deResolve(org.emftext.language.refactoring.roles.RoleModel element, org.emftext.language.refactoring.rolemapping.Mapping container, org.eclipse.emf.ecore.EReference reference) {
+		return delegate.deResolve(element, container, reference);
+	}
+	
+	public void setOptions(java.util.Map<?,?> options) {
+		// save options in a field or leave method empty if this resolver does not depend on any option
+	}
+	
+}
---
+++ trunk/Refactoring/Plugins/org.emftext.language.refactoring.rolemapping.resource.rolemapping/src/org/emftext/language/refactoring/rolemapping/resource/rolemapping/analysis/RoleMappingModelTargetMetamodelReferenceResolver.java
@@ -0,0 +1,33 @@
+/*******************************************************************************
+ * Copyright (c) 2006-2010 
+ * Software Technology Group, Dresden University of Technology
+ * 
+ * All rights reserved. This program and the accompanying materials
+ * are made available under the terms of the Eclipse Public License v1.0 
+ * which accompanies this distribution, and is available at
+ * http://www.eclipse.org/legal/epl-v10.html
+ * 
+ * Contributors:
+ *   Software Technology Group - TU Dresden, Germany 
+ *      - initial API and implementation
+ ******************************************************************************/
+
+package org.emftext.language.refactoring.rolemapping.resource.rolemapping.analysis;
+
+public class RoleMappingModelTargetMetamodelReferenceResolver implements org.emftext.language.refactoring.rolemapping.resource.rolemapping.IRolemappingReferenceResolver<org.emftext.language.refactoring.rolemapping.RoleMappingModel, org.eclipse.emf.ecore.EPackage> {
+	
+	private org.emftext.language.refactoring.rolemapping.resource.rolemapping.analysis.RolemappingDefaultResolverDelegate<org.emftext.language.refactoring.rolemapping.RoleMappingModel, org.eclipse.emf.ecore.EPackage> delegate = new org.emftext.language.refactoring.rolemapping.resource.rolemapping.analysis.RolemappingDefaultResolverDelegate<org.emftext.language.refactoring.rolemapping.RoleMappingModel, org.eclipse.emf.ecore.EPackage>();
+	
+	public void resolve(java.lang.String identifier, org.emftext.language.refactoring.rolemapping.RoleMappingModel container, org.eclipse.emf.ecore.EReference reference, int position, boolean resolveFuzzy, final org.emftext.language.refactoring.rolemapping.resource.rolemapping.IRolemappingReferenceResolveResult<org.eclipse.emf.ecore.EPackage> result) {
+		delegate.resolve(identifier, container, reference, position, resolveFuzzy, result);
+	}
+	
+	public java.lang.String deResolve(org.eclipse.emf.ecore.EPackage element, org.emftext.language.refactoring.rolemapping.RoleMappingModel container, org.eclipse.emf.ecore.EReference reference) {
+		return delegate.deResolve(element, container, reference);
+	}
+	
+	public void setOptions(java.util.Map<?,?> options) {
+		// save options in a field or leave method empty if this resolver does not depend on any option
+	}
+	
+}
