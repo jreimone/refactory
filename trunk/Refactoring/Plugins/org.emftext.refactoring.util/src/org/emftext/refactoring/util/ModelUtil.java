ca8289c5a1f3a07e115631f9ce630c8c01d3975f
jreimann <jreimann@2290cd21-d4b4-4435-9c65-06e28a4c4f12>
2010-02-16 17:54:51 +0000

    util plugin

---
+++ trunk/Refactoring/Plugins/org.emftext.refactoring.util/src/org/emftext/refactoring/util/ModelUtil.java
@@ -0,0 +1,74 @@
+/**
+ * 
+ */
+package org.emftext.refactoring.util;
+
+import java.util.Iterator;
+
+import org.eclipse.emf.common.util.BasicEList;
+import org.eclipse.emf.common.util.EList;
+import org.eclipse.emf.ecore.EAttribute;
+import org.eclipse.emf.ecore.EClass;
+import org.eclipse.emf.ecore.EObject;
+
+/**
+ * This class provides useful functionality to work with, query or filter models.
+ * 
+ * @author Jan Reimann
+ *
+ */
+public class ModelUtil {
+
+	private ModelUtil(){
+		// just do nothing
+	}
+	
+	/**
+	 * Gets an iterator as input and filters the elements by the given type. Then returns a list containing
+	 * only elements of type <code>type</code>. 
+	 * 
+	 * @param <T> the class by which the iterator will be filtered
+	 * @param iterator the iterator, normally a TreeIterator for a model
+	 * @param type the filter type
+	 * @return a list containing only elements of the given type
+	 */
+	@SuppressWarnings("unchecked")
+	public static <T> EList<T> getObjectsByType(Iterator<EObject> iterator, Class<T> type){
+		EList<T> filteredModel = new BasicEList<T>();
+		while (iterator.hasNext()) {
+			EObject eObject = (EObject) iterator.next();
+			if(type.isInstance(eObject)){
+				filteredModel.add((T) eObject);
+			}
+		}
+		return filteredModel;
+	}
+
+	/**
+	 * Searchs the <code>contents</code> for elements having an attribute with the name <code>attributeName</code>
+	 * and a set value <code>value</code>.
+	 * 
+	 * @param contents the contents to be filtered
+	 * @param attributeName the name of the attribute to check
+	 * @param value the value of the attribute
+	 * @return a list containing all elements of the contents having an attribute with the given name and value
+	 */
+	public static EList<EObject> filterObjectsByAttribute(
+			Iterator<? extends EObject> contents, String attributeName, Object value) {
+		EList<EObject> filteredElements = new BasicEList<EObject>();
+		while(contents.hasNext()){
+			EObject element = contents.next();
+			EClass clazz = element.eClass();
+			EList<EAttribute> attributes = clazz.getEAllAttributes();
+			for (EAttribute eAttribute : attributes) {
+				if(attributeName.equals(eAttribute.getName())){
+					Object elementsAttributeValue = element.eGet(eAttribute, true);
+					if(elementsAttributeValue.equals(value)){
+						filteredElements.add(element);
+					}
+				}
+			}
+		}
+		return filteredElements;
+	}
+}
