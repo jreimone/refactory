33ce8ca611abac53228e7365e712bddd009ba6a9
jreimann <jreimann@2290cd21-d4b4-4435-9c65-06e28a4c4f12>
2010-02-16 17:52:25 +0000

    more tests

--- trunk/Refactoring/Tests/org.emftext.refactoring.tests/src/org/emftext/language/refactoring/roles/postprocessing/EmptyOutgoingRelationTest.java
+++ trunk/Refactoring/Tests/org.emftext.refactoring.tests/src/org/emftext/language/refactoring/roles/postprocessing/EmptyOutgoingRelationTest.java
@@ -3,22 +3,12 @@
  */
 package org.emftext.language.refactoring.roles.postprocessing;
 
-import java.io.File;
-
-import junit.framework.TestCase;
-
 import org.eclipse.emf.common.util.BasicEList;
 import org.eclipse.emf.common.util.EList;
-import org.eclipse.emf.common.util.URI;
-import org.eclipse.emf.ecore.EObject;
-import org.eclipse.emf.ecore.resource.Resource;
-import org.eclipse.emf.ecore.resource.ResourceSet;
-import org.eclipse.emf.ecore.resource.impl.ResourceSetImpl;
 import org.emftext.language.refactoring.roles.Relation;
 import org.emftext.language.refactoring.roles.Role;
 import org.emftext.language.refactoring.roles.RoleModel;
-import org.emftext.language.refactoring.roles.RolesPackage;
-import org.emftext.language.refactoring.roles.resource.rolestext.mopp.RolestextResourceFactory;
+import org.emftext.refactoring.test.AbstractRefactoringTest;
 import org.junit.Before;
 
 /**
@@ -27,26 +17,18 @@ import org.junit.Before;
  * @author Jan Reimann
  *
  */
-public class EmptyOutgoingRelationTest extends TestCase {
+public class EmptyOutgoingRelationTest extends AbstractRefactoringTest {
 
 	private String path = "resources/ExtractMethod.rolestext";
-	private Resource resource;
+	private RoleModel model;
 	
 	@Before
 	public void setUp() throws Exception {
-		ResourceSet resourceSet = new ResourceSetImpl();
-		resourceSet.getResourceFactoryRegistry().getExtensionToFactoryMap().put("rolestext", new RolestextResourceFactory());
-		resourceSet.getPackageRegistry().put(RolesPackage.eNS_URI, RolesPackage.eINSTANCE);
-		File file = new File(path);
-		assertTrue("File must exist", file.exists());
-		resource = resourceSet.getResource(URI.createFileURI(file.getAbsolutePath()), true);
-		assertNotNull("Resource mustn't be null", resource);
+		super.setUp();
+		model = getExpectedModelFromFile(path, RoleModel.class);
 	}
 	
 	public void testEmptyOutgoingRelations(){
-		EObject root = resource.getContents().get(0);
-		assertTrue("root object must be of type RoleModel", root instanceof RoleModel);
-		RoleModel model = (RoleModel) root;
 		EList<Relation> relations = model.getRelations();
 		Role origContainerRole = null;
 		EList<Relation> outgoingRelations1 = new BasicEList<Relation>();
