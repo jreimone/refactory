1ace827feffb86516004c91d43680b09be023678
jreimann <jreimann@2290cd21-d4b4-4435-9c65-06e28a4c4f12>
2010-02-16 17:54:10 +0000

    allowed "*" for "-1"

--- trunk/Refactoring/Plugins/org.emftext.language.refactoring.roles.resource.rolestext/src/org/emftext/language/refactoring/roles/resource/rolestext/analysis/RolestextNUMBERTokenResolver.java
+++ trunk/Refactoring/Plugins/org.emftext.language.refactoring.roles.resource.rolestext/src/org/emftext/language/refactoring/roles/resource/rolestext/analysis/RolestextNUMBERTokenResolver.java
@@ -7,20 +7,24 @@
 package org.emftext.language.refactoring.roles.resource.rolestext.analysis;
 
 public class RolestextNUMBERTokenResolver implements org.emftext.language.refactoring.roles.resource.rolestext.IRolestextTokenResolver {
-	
+
 	private org.emftext.language.refactoring.roles.resource.rolestext.analysis.RolestextDefaultTokenResolver defaultTokenResolver = new org.emftext.language.refactoring.roles.resource.rolestext.analysis.RolestextDefaultTokenResolver();
-	
+
 	public java.lang.String deResolve(java.lang.Object value, org.eclipse.emf.ecore.EStructuralFeature feature, org.eclipse.emf.ecore.EObject container) {
 		java.lang.String result = defaultTokenResolver.deResolve(value, feature, container);
-		return result;
+		return ("-1".equals(result))? "*" : result;
 	}
-	
+
 	public void resolve(java.lang.String lexem, org.eclipse.emf.ecore.EStructuralFeature feature, org.emftext.language.refactoring.roles.resource.rolestext.IRolestextTokenResolveResult result) {
-		defaultTokenResolver.resolve(lexem, feature, result);
+		if("*".equals(lexem)){
+			defaultTokenResolver.resolve("-1", feature, result);
+		} else {
+			defaultTokenResolver.resolve(lexem, feature, result);
+		}
 	}
-	
+
 	public void setOptions(java.util.Map<?,?> options) {
 		defaultTokenResolver.setOptions(options);
 	}
-	
+
 }
