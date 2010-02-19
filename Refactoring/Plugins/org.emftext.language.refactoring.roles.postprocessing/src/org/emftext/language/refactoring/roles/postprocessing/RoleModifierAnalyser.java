f17b531e54e75e3fdfe60d244b78c432990c8d9d
Jan <jreimone@users.noreply.github.com>
2010-02-19 13:41:28 +0000

    extended the postprocessor for analysis attribute modifiers to

--- Refactoring/Plugins/org.emftext.language.refactoring.roles.postprocessing/src/org/emftext/language/refactoring/roles/postprocessing/RoleModifierAnalyser.java
+++ Refactoring/Plugins/org.emftext.language.refactoring.roles.postprocessing/src/org/emftext/language/refactoring/roles/postprocessing/RoleModifierAnalyser.java
@@ -8,7 +8,10 @@ import java.util.HashSet;
 import java.util.List;
 import java.util.Set;
 
+import org.eclipse.emf.common.util.EList;
+import org.emftext.language.refactoring.roles.NamedElement;
 import org.emftext.language.refactoring.roles.Role;
+import org.emftext.language.refactoring.roles.RoleAttribute;
 import org.emftext.language.refactoring.roles.RoleModel;
 import org.emftext.language.refactoring.roles.RoleModifier;
 import org.emftext.language.refactoring.roles.RolesPackage;
@@ -23,8 +26,10 @@ import org.emftext.language.refactoring.roles.resource.rolestext.util.RolestextE
  */
 public class RoleModifierAnalyser extends AbstractPostProcessor {
 
-	private static final String UNIQUE_ROLE_MODIFIER = "The modifier \"%1$s\" of Role %2$s mustn't be set repeatedly.";
+	private static final String UNIQUE_ROLE_MODIFIER = "The modifier \"%1$s\" of %2$s %3$s mustn't be set repeatedly.";
 	private static final String NO_INPUT_ROLE = "The Role Model contains no roles specified for input";
+	
+	private boolean inputRoleSet;
 	
 	/* (non-Javadoc)
 	 * @see org.emftext.language.refactoring.roles.resource.rolestext.options.AbstractPostProcessor#analyse(org.emftext.language.refactoring.roles.resource.rolestext.mopp.RolestextResource, org.emftext.language.refactoring.roles.RoleModel)
@@ -32,21 +37,14 @@ public class RoleModifierAnalyser extends AbstractPostProcessor {
 	@Override
 	public void analyse(RolestextResource resource, RoleModel model) {
 		Collection<Role> roles = RolestextEObjectUtil.getObjectsByType(model.eAllContents(), RolesPackage.eINSTANCE.getRole());
-		boolean inputRoleSet = false;
+		inputRoleSet = false;
 		for (Role role : roles) {
-			Set<RoleModifier> uniqueModifier = new HashSet<RoleModifier>();
 			List<RoleModifier> modifier = role.getModifier();
-			for (RoleModifier roleModifier : modifier) {
-				if(roleModifier.equals(RoleModifier.INPUT)){
-					inputRoleSet = true;
-				}
-				boolean unique = uniqueModifier.add(roleModifier);
-				if(!unique){
-					addProblem(resource
-							, ERoleModelProblemType.ROLE_MODIFIER_ALREADY_SET
-							, String.format(UNIQUE_ROLE_MODIFIER, roleModifier.getName(), role.getName())
-							, role);
-				}
+			analyseModifier(resource, role, modifier);
+			EList<RoleAttribute> attributes = role.getAttributes();
+			for (RoleAttribute roleAttribute : attributes) {
+				EList<RoleModifier> attModifier = roleAttribute.getModifier();
+				analyseModifier(resource, roleAttribute, attModifier);
 			}
 		}
 		if(!inputRoleSet){
@@ -54,6 +52,27 @@ public class RoleModifierAnalyser extends AbstractPostProcessor {
 					, ERoleModelProblemType.INPUT_ROLE_NOT_SET
 					, NO_INPUT_ROLE
 					, model);
+		}
+	}
+
+	/**
+	 * @param resource
+	 * @param nameable
+	 * @param modifier
+	 */
+	private void analyseModifier(RolestextResource resource, NamedElement nameable, List<RoleModifier> modifier) {
+		Set<RoleModifier> uniqueModifier = new HashSet<RoleModifier>();
+		for (RoleModifier roleModifier : modifier) {
+			if(roleModifier.equals(RoleModifier.INPUT)){
+				inputRoleSet = true;
+			}
+			boolean unique = uniqueModifier.add(roleModifier);
+			if(!unique){
+				addProblem(resource
+						, ERoleModelProblemType.ROLE_MODIFIER_ALREADY_SET
+						, String.format(UNIQUE_ROLE_MODIFIER, roleModifier.getName(), nameable.eClass().getName(), nameable.getName())
+						, nameable);
+			}
 		}
 	}
 
