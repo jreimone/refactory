e029a8ada4cf3d08e6c1635db592a53a190c9dba
Jan <jreimone@users.noreply.github.com>
2010-02-23 13:16:46 +0000

    implemented dependencies to index connector

--- Refactoring/Plugins/org.emftext.refactoring.specification.interpreter/src/org/emftext/refactoring/interpreter/internal/RefactoringInterpreterContext.java
+++ Refactoring/Plugins/org.emftext.refactoring.specification.interpreter/src/org/emftext/refactoring/interpreter/internal/RefactoringInterpreterContext.java
@@ -4,6 +4,7 @@
 package org.emftext.refactoring.interpreter.internal;
 
 import org.eclipse.emf.common.util.EList;
+import org.eclipse.emf.ecore.EObject;
 import org.emftext.language.refactoring.refactoring_specification.RefactoringSpecification;
 import org.emftext.language.refactoring.refactoring_specification.Variable;
 
@@ -15,13 +16,27 @@ import org.emftext.language.refactoring.refactoring_specification.Variable;
  */
 public class RefactoringInterpreterContext {
 
-	EList<Variable> variables;
+	private EList<Variable> variables;
+	private EObject model;
+	private EList<EObject> selection;
+	
+	protected void setInitialContext(EObject model, EList<EObject> selection){
+		this.model = model;
+		this.selection = selection;
+	}
 	
 	/**
 	 * Initializes the variables in the context as they are declared in the given RefactoringSpecification. 
 	 * @param refSpec
 	 */
 	public void initializeVariables(RefactoringSpecification refSpec){
-		
+		// TODO init variables to instances
+	}
+
+	/**
+	 * @return the model
+	 */
+	public EObject getModel() {
+		return model;
 	}
 }
