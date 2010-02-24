f53be3d357ad78d382fc000ca2160b1e8802a987
Jan <jreimone@users.noreply.github.com>
2010-02-24 18:09:06 +0000

    new util functions

--- Refactoring/Plugins/org.emftext.refactoring.util/src/org/emftext/refactoring/util/ModelUtil.java
+++ Refactoring/Plugins/org.emftext.refactoring.util/src/org/emftext/refactoring/util/ModelUtil.java
@@ -7,9 +7,11 @@ import java.util.Iterator;
 
 import org.eclipse.emf.common.util.BasicEList;
 import org.eclipse.emf.common.util.EList;
+import org.eclipse.emf.common.util.TreeIterator;
 import org.eclipse.emf.ecore.EAttribute;
 import org.eclipse.emf.ecore.EClass;
 import org.eclipse.emf.ecore.EObject;
+import org.eclipse.emf.ecore.EReference;
 
 /**
  * This class provides useful functionality to work with, query or filter models.
@@ -70,5 +72,63 @@ public class ModelUtil {
 			}
 		}
 		return filteredElements;
+	}
+	
+	public static <T extends EObject> boolean bothModelsAreEqual(T first, T second){
+		TreeIterator<EObject> itFirst = first.eAllContents();
+		TreeIterator<EObject> itSecond = second.eAllContents();
+		while (itFirst.hasNext()) {
+			if(!itSecond.hasNext()){
+				return false;
+			}
+			EObject obFirst = itFirst.next();
+			EObject obSecond = itSecond.next();
+			boolean equal = bothEObjectsAreEqual(obFirst, obSecond);
+			if(!equal){
+				return false;
+			}
+		}
+		return true;
+	}
+
+	public static boolean bothEObjectsAreEqual(EObject first, EObject second){
+		EClass class1 = first.eClass();
+		EClass class2 = second.eClass();
+		if(!class1.equals(class2)){
+			return false;
+		}
+		EList<EAttribute> attributes1 = class1.getEAllAttributes();
+		EList<EAttribute> attributes2 = class2.getEAllAttributes();
+		if(!attributes1.containsAll(attributes2)){
+			return false;
+		}
+		for (EAttribute attribute : attributes2) {
+			Object value1 = first.eGet(attribute);
+			Object value2 = second.eGet(attribute);
+			if(!value1.equals(value2)){
+				return false;
+			}
+		}
+		EList<EReference> references1 = class1.getEAllReferences();
+		EList<EReference> references2 = class1.getEAllReferences();
+		if(references1.size() != references2.size()){
+			return false;
+		}
+		for (int i = 0; i < references1.size(); i++) {
+			EReference ref1 = references1.get(i);
+			EReference ref2 = references2.get(i);
+			if(!ref1.getName().equals(ref2.getName())){
+				return false;
+			}
+			if(!ref1.getEType().equals(ref2.getEType())){
+				return false;
+			}
+			Object value1 = first.eGet(ref1);
+			Object value2 = second.eGet(ref2);
+			if(!value1.equals(value2)){
+				return false;
+			}
+		}
+		return true;
 	}
 }
