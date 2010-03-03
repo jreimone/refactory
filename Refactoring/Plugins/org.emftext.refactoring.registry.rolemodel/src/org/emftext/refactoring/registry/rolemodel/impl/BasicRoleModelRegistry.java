d0cb0f008b3fe1daff39023264b8a1c12a258319
Jan <jreimone@users.noreply.github.com>
2010-03-03 16:50:32 +0000

    minor changes

--- Refactoring/Plugins/org.emftext.refactoring.registry.rolemodel/src/org/emftext/refactoring/registry/rolemodel/impl/BasicRoleModelRegistry.java
+++ Refactoring/Plugins/org.emftext.refactoring.registry.rolemodel/src/org/emftext/refactoring/registry/rolemodel/impl/BasicRoleModelRegistry.java
@@ -2,21 +2,16 @@ package org.emftext.refactoring.registry.rolemodel.impl;
 
 import java.util.Collection;
 import java.util.HashMap;
+import java.util.List;
 import java.util.Map;
 
-import org.eclipse.core.runtime.IConfigurationElement;
 import org.eclipse.core.runtime.IStatus;
-import org.eclipse.core.runtime.Platform;
-import org.eclipse.emf.common.util.URI;
-import org.eclipse.emf.ecore.EObject;
-import org.eclipse.emf.ecore.resource.Resource;
-import org.eclipse.emf.ecore.resource.ResourceSet;
-import org.eclipse.emf.ecore.resource.impl.ResourceSetImpl;
 import org.emftext.language.refactoring.roles.RoleModel;
 import org.emftext.refactoring.registry.rolemodel.Activator;
 import org.emftext.refactoring.registry.rolemodel.IRoleModelExtensionPoint;
 import org.emftext.refactoring.registry.rolemodel.IRoleModelRegistry;
 import org.emftext.refactoring.registry.rolemodel.exceptions.RoleModelAlreadyRegisteredException;
+import org.emftext.refactoring.util.RegistryUtil;
 
 public class BasicRoleModelRegistry implements IRoleModelRegistry {
 
@@ -50,33 +45,14 @@ public class BasicRoleModelRegistry implements IRoleModelRegistry {
 	}
 
 	private void collectRegisteredRoleModels(){
-		ResourceSet rs = new ResourceSetImpl();
-		IConfigurationElement[] elements = Platform.getExtensionRegistry().getConfigurationElementsFor(IRoleModelExtensionPoint.ID);
-		for (IConfigurationElement element : elements) {
-			String value = element.getAttribute(IRoleModelExtensionPoint.RESOURCE_ATTRIBUTE);
-			String plugin = element.getContributor().getName();
-			URI uri = URI.createPlatformPluginURI("/" + plugin + "/" + value, true);
-			Resource resource = null;
-			try{
-				resource = rs.getResource(uri, true);
-			} catch (Exception e) {
-				Activator.getDefault().log(String.format("Resource '%1$s' couldn't be loaded", value), IStatus.ERROR);
-				continue;
-			}
-			if(resource == null){
-				Activator.getDefault().log(String.format("Resource '%1$s' couldn't be loaded", value), IStatus.ERROR);
-				continue;
-			}
-			EObject model = resource.getContents().get(0);
-			if(model instanceof RoleModel){
-				try {
-					registerRoleModel((RoleModel) model);
-				} catch (RoleModelAlreadyRegisteredException e) {
-					Activator.getDefault().log(String.format("A RoleModel with name '%1$s' was already registered", e.getRoleModel().getName()), IStatus.ERROR);
-				}
-			} else {
-				Activator.getDefault().log(String.format("The resource with URI '%1$s' doesn't contain a RoleModel", uri.toString())
-						, IStatus.ERROR);
+		List<RoleModel> models = RegistryUtil.collectRegisteredResources(IRoleModelExtensionPoint.ID
+				, IRoleModelExtensionPoint.RESOURCE_ATTRIBUTE
+				, RoleModel.class);
+		for (RoleModel roleModel : models) {
+			try {
+				registerRoleModel(roleModel);
+			} catch (RoleModelAlreadyRegisteredException e) {
+				Activator.getDefault().log(String.format("A RoleModel with name '%1$s' was already registered", e.getRoleModel().getName()), IStatus.ERROR);
 			}
 		}
 	}
