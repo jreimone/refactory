5ca7303ece81116cf8f710761eb195f12782e5fb
Jan <jreimone@users.noreply.github.com>
2010-03-03 19:36:01 +0000

    tests

--- Refactoring/Tests/org.emftext.refactoring.tests/src/org/emftext/refactoring/specification/interpreter/RefactoringInterpreterTest.java
+++ Refactoring/Tests/org.emftext.refactoring.tests/src/org/emftext/refactoring/specification/interpreter/RefactoringInterpreterTest.java
@@ -3,8 +3,7 @@
  */
 package org.emftext.refactoring.specification.interpreter;
 
-import static org.junit.Assert.assertNotNull;
-import static org.junit.Assert.assertTrue;
+import static org.junit.Assert.*;
 
 import java.io.File;
 import java.util.List;
@@ -16,11 +15,9 @@ import org.emftext.language.refactoring.refactoring_specification.RefactoringSpe
 import org.emftext.language.refactoring.rolemapping.Mapping;
 import org.emftext.language.refactoring.rolemapping.RoleMappingModel;
 import org.emftext.language.refactoring.roles.Role;
-import org.emftext.refactoring.indexconnector.IndexConnector;
-import org.emftext.refactoring.indexconnector.IndexConnectorFactory;
-import org.emftext.refactoring.indexconnector.MockIndexConnector;
 import org.emftext.refactoring.interpreter.IRefactorer;
 import org.emftext.refactoring.interpreter.RefactorerFactory;
+import org.emftext.refactoring.registry.rolemapping.IRoleMappingRegistry;
 import org.emftext.refactoring.test.QueryUtil;
 import org.emftext.refactoring.test.TestUtil;
 import org.emftext.refactoring.util.RoleUtil;
@@ -34,8 +31,8 @@ public class RefactoringInterpreterTest extends TestClass{
 	private static final String MODEL = "MODEL_";
 	private static final String PATH = "PATH_";
 	
-	private IRefactorer getRefactorer(Resource resource, IndexConnector indexConnector){
-		IRefactorer refactorer = RefactorerFactory.eINSTANCE.getRefactorer(resource, indexConnector);
+	private IRefactorer getRefactorer(Resource resource){
+		IRefactorer refactorer = RefactorerFactory.eINSTANCE.getRefactorer(resource);
 		assertNotNull(refactorer);
 		return refactorer;
 	}
@@ -46,11 +43,10 @@ public class RefactoringInterpreterTest extends TestClass{
 		TestDataSet dataSet = getTestDataSet();
 		File file = dataSet.getInputFileByPattern(MODEL);
 		Resource resource = TestUtil.getResourceFromFile(file);
-		IndexConnector connector = IndexConnectorFactory.defaultINSTANCE.getIndexConnector(MockIndexConnector.class);
-		assertNotNull(connector);
-		
+		IRoleMappingRegistry registry = IRoleMappingRegistry.INSTANCE;
+		assertNotNull(registry);
 		EObject model = TestUtil.getModelFromResource(resource);
-		RoleMappingModel mappingModel = connector.getRoleMapping(model.eClass().getEPackage().getNsURI());
+		RoleMappingModel mappingModel = registry.getRoleMappingForUri(model.eClass().getEPackage().getNsURI());
 		assertNotNull(mappingModel);
 		
 		File path = dataSet.getInputFileByPattern(PATH);
@@ -76,9 +72,7 @@ public class RefactoringInterpreterTest extends TestClass{
 		TestDataSet dataSet = getTestDataSet();
 		File file = dataSet.getInputFileByPattern(MODEL);
 		Resource resource = TestUtil.getResourceFromFile(file);
-		IndexConnector connector = IndexConnectorFactory.defaultINSTANCE.getIndexConnector(MockIndexConnector.class);
-		IRefactorer refactorer = getRefactorer(resource, connector);
-//		EObject model = TestUtil.getModelFromResource(resource);
+		IRefactorer refactorer = getRefactorer(resource);
 		
 		File path = dataSet.getInputFileByPattern(PATH);
 		String query = QueryUtil.getLineInFile(path, 1);
@@ -89,14 +83,18 @@ public class RefactoringInterpreterTest extends TestClass{
 		assertTrue(refSpecs.size() > 0);
 	}
 
-	
-//	public void refactoringInterpreter(){
-////		WorkspaceBuilder.setTest(this);
-////		WorkspaceBuilder.buildTestWorkspace();
-////		Resource resource = TestUtil.getResourceInWorkspace(this, path);
-//		Resource resource = null;
-//		IRefactorer refactorer = getRefactorer(resource);
-//		EObject refactoredModel = refactorer.refactor(null, true);
-//		assertNull(refactoredModel);
-//	}
+	@Test
+	@InputData({MODEL,PATH})
+	public void refactoringInterpreter(){
+		TestDataSet dataSet = getTestDataSet();
+		File file = dataSet.getInputFileByPattern(MODEL);
+		Resource resource = TestUtil.getResourceFromFile(file);
+		IRefactorer refactorer = getRefactorer(resource);
+		List<RefactoringSpecification> refSpecs = refactorer.getPossibleRefactorings(1.0);
+		assertNotNull(refSpecs);
+		assertTrue(refSpecs.size() > 0);
+		RefactoringSpecification refSpec = refSpecs.get(0);
+		EObject refactoredModel = refactorer.refactor(refSpec, true);
+		assertNotNull(refactoredModel);
+	}
 }
