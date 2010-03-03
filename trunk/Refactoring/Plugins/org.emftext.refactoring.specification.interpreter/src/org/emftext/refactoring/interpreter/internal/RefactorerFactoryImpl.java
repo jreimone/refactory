e45672347e6372a16acaaf12684d8ea4ab714037
jreimann <jreimann@2290cd21-d4b4-4435-9c65-06e28a4c4f12>
2010-03-03 19:35:50 +0000

    all work is done with registry now

--- trunk/Refactoring/Plugins/org.emftext.refactoring.specification.interpreter/src/org/emftext/refactoring/interpreter/internal/RefactorerFactoryImpl.java
+++ trunk/Refactoring/Plugins/org.emftext.refactoring.specification.interpreter/src/org/emftext/refactoring/interpreter/internal/RefactorerFactoryImpl.java
@@ -3,9 +3,12 @@
  */
 package org.emftext.refactoring.interpreter.internal;
 
+import org.eclipse.emf.ecore.EObject;
 import org.eclipse.emf.ecore.resource.Resource;
+import org.emftext.language.refactoring.rolemapping.RoleMappingModel;
 import org.emftext.refactoring.interpreter.IRefactorer;
 import org.emftext.refactoring.interpreter.RefactorerFactory;
+import org.emftext.refactoring.registry.rolemapping.IRoleMappingRegistry;
 
 /**
  * @author Jan Reimann
@@ -17,15 +20,13 @@ public class RefactorerFactoryImpl implements RefactorerFactory {
 	 * @see org.emftext.refactoring.interpreter.RefactoringInterpreterFactory#getRefactoringInterpreter(org.eclipse.emf.common.util.URI)
 	 */
 	public IRefactorer getRefactorer(Resource resource) {
-		throw new UnsupportedOperationException();
-//		IndexConnectorFactory factory = IndexConnectorFactory.defaultINSTANCE;
-//		IndexConnector indexConnector = factory.getIndexConnector();
-//		EObject root = resource.getContents().get(0);
-//		String mmUri = root.eClass().getEPackage().getNsURI();
-//		RoleMappingModel roleMapping = indexConnector.getRoleMapping(mmUri);
-//		if(roleMapping == null){
-//			return null;
-//		}
-//		return new Refactorer(root, roleMapping);
+		IRoleMappingRegistry registry = IRoleMappingRegistry.INSTANCE;
+		EObject root = resource.getContents().get(0);
+		String mmUri = root.eClass().getEPackage().getNsURI();
+		RoleMappingModel roleMapping = registry.getRoleMappingForUri(mmUri);
+		if(roleMapping == null){
+			return null;
+		}
+		return new Refactorer(root, roleMapping);
 	}
 }
