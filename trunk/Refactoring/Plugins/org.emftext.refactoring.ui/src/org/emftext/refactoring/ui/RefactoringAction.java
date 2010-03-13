bf5e4f483f01aedfdfe0569706850baca4e4c0e1
jreimann <jreimann@2290cd21-d4b4-4435-9c65-06e28a4c4f12>
2010-03-13 18:35:10 +0000

    refactoring context menu will be added dynamically now for all editors containing EObjects - so refactorings can be invoked in the normal Ecore editor too, if they are enabled for the displayed metamodel

--- trunk/Refactoring/Plugins/org.emftext.refactoring.ui/src/org/emftext/refactoring/ui/RefactoringAction.java
+++ trunk/Refactoring/Plugins/org.emftext.refactoring.ui/src/org/emftext/refactoring/ui/RefactoringAction.java
@@ -1,5 +1,7 @@
 package org.emftext.refactoring.ui;
 
+import org.eclipse.emf.ecore.EObject;
+import org.eclipse.emf.ecore.resource.Resource;
 import org.eclipse.jface.action.Action;
 import org.emftext.language.refactoring.rolemapping.Mapping;
 import org.emftext.refactoring.interpreter.IRefactorer;
@@ -12,11 +14,13 @@ public class RefactoringAction extends Action {
 
 	private Mapping mapping;
 	private IRefactorer refactorer;
+	private Resource resource;
 
-	public RefactoringAction(Mapping mapping, IRefactorer refactorer) {
+	public RefactoringAction(Mapping mapping, IRefactorer refactorer, Resource resource) {
 		super();
 		this.mapping = mapping;
 		this.refactorer = refactorer;
+		this.resource = resource;
 	}
 
 	/* (non-Javadoc)
@@ -24,6 +28,16 @@ public class RefactoringAction extends Action {
 	 */
 	@Override
 	public void run() {
-		refactorer.refactor(mapping, false);
+		EObject refactoredModel = refactorer.refactor(mapping, false);
+		// save or replace only modified parts 
+		// --> have to wait for Mirkos and Jendriks ideas of replacement and not simply overwrite all contents
+		try {
+			resource.getContents().set(0, refactoredModel);
+			resource.save(null);
+			resource.setModified(true);
+		} catch (Exception e) {
+			e.printStackTrace();
+			System.out.println("Error while saving refactored model");
+		}
 	}
 }
--- trunk/Refactoring/Plugins/org.emftext.refactoring.ui/src/org/emftext/refactoring/ui/RefactoringMenuContributor.java
+++ trunk/Refactoring/Plugins/org.emftext.refactoring.ui/src/org/emftext/refactoring/ui/RefactoringMenuContributor.java
@@ -2,6 +2,10 @@ package org.emftext.refactoring.ui;
 
 import java.util.List;
 
+import org.eclipse.core.expressions.EvaluationResult;
+import org.eclipse.core.expressions.Expression;
+import org.eclipse.core.expressions.IEvaluationContext;
+import org.eclipse.core.runtime.CoreException;
 import org.eclipse.emf.ecore.EObject;
 import org.eclipse.emf.ecore.resource.Resource;
 import org.eclipse.jface.action.Action;
@@ -12,6 +16,8 @@ import org.eclipse.jface.viewers.ISelection;
 import org.eclipse.jface.viewers.ISelectionProvider;
 import org.eclipse.jface.viewers.StructuredSelection;
 import org.eclipse.ui.IEditorPart;
+import org.eclipse.ui.IWorkbench;
+import org.eclipse.ui.IWorkbenchPart;
 import org.eclipse.ui.PlatformUI;
 import org.eclipse.ui.menus.ExtensionContributionFactory;
 import org.eclipse.ui.menus.IContributionRoot;
@@ -20,9 +26,11 @@ import org.emftext.access.EMFTextAccessProxy;
 import org.emftext.access.resource.IEditor;
 import org.emftext.access.resource.ILocationMap;
 import org.emftext.access.resource.IResource;
+import org.emftext.language.refactoring.refactoring_specification.RefactoringSpecification;
 import org.emftext.language.refactoring.rolemapping.Mapping;
 import org.emftext.refactoring.interpreter.IRefactorer;
 import org.emftext.refactoring.interpreter.RefactorerFactory;
+import org.emftext.refactoring.registry.refactoringspecification.IRefactoringSpecificationRegistry;
 import org.emftext.refactoring.util.StringUtil;
 
 public class RefactoringMenuContributor extends ExtensionContributionFactory {
@@ -37,7 +45,12 @@ public class RefactoringMenuContributor extends ExtensionContributionFactory {
 	@SuppressWarnings("unchecked")
 	@Override
 	public void createContributionItems(IServiceLocator serviceLocator, IContributionRoot additions) {
+		IWorkbenchPart activePart = PlatformUI.getWorkbench().getActiveWorkbenchWindow().getActivePage().getActivePart();
 		IEditorPart activeEditor = PlatformUI.getWorkbench().getActiveWorkbenchWindow().getActivePage().getActiveEditor();
+		if(!activePart.equals(activeEditor)){
+			System.out.println("Workbenchpart '" + activePart.getTitle() + "' is not an editor");
+			return;
+		}
 		ISelectionProvider selectionProvider = activeEditor.getEditorSite().getSelectionProvider();
 		ISelection selection = selectionProvider.getSelection();
 		List<EObject> selectedElements = null;
@@ -52,41 +65,53 @@ public class RefactoringMenuContributor extends ExtensionContributionFactory {
 			selectedElements = (List<EObject>)temp;
 			resource = selectedElements.get(0).eResource();
 		} else if(selection instanceof ITextSelection){
-			try {
-				IEditor emftextEditor = (IEditor) EMFTextAccessProxy.get(activeEditor, IEditor.class);
-				IResource emftextResource = emftextEditor.getResource();
-				resource = emftextResource;
-				ILocationMap locationMap = emftextResource.getLocationMap();
-				ITextSelection textSelection = (ITextSelection) selection;
-				int startOffset = textSelection.getOffset();
-				int endOffset = startOffset + textSelection.getLength();
-				if(startOffset == endOffset){
-					selectedElements = locationMap.getElementsAt(startOffset);
-				} else {
-					selectedElements = locationMap.getElementsBetween(startOffset, endOffset);
+//			if(EMFTextAccessProxy.isAccessibleWith(activeEditor, IEditor.class)){
+				try {
+
+					IEditor emftextEditor = (IEditor) EMFTextAccessProxy.get(activeEditor, IEditor.class);
+					IResource emftextResource = emftextEditor.getResource();
+					resource = emftextResource;
+					if(resource == null){
+						throw new Exception("just jump to catch block");
+					}
+					ILocationMap locationMap = emftextResource.getLocationMap();
+					ITextSelection textSelection = (ITextSelection) selection;
+					int startOffset = textSelection.getOffset();
+					int endOffset = startOffset + textSelection.getLength();
+					if(startOffset == endOffset){
+						selectedElements = locationMap.getElementsAt(startOffset);
+					} else {
+						selectedElements = locationMap.getElementsBetween(startOffset, endOffset);
+					}
+				} catch (Exception e) {
+					// probably another non EMFText generated editor
+					System.out.println(e.getMessage());
+					System.out.println("Editor " + activeEditor.getTitle() + " doesn't get Refactoring menu");
 				}
-			} catch (Exception e) {
-				// probably another non EMFText generated editor
-				System.out.println("Editor " + activeEditor.getTitle() + " doesn't get Refactoring menu");
-			}
+//			}
 		}
 		if(resource != null && selectedElements != null && selectedElements.size() >= 1){
 			for (EObject eObject : selectedElements) {
 				System.out.println("Selected: " + eObject);
 			}
 			IMenuManager rootMenu = new MenuManager(CONTEXT_MENU_ENTRY_TEXT, CONTEXT_MENU_ENTRY_ID);
-			
+
 			IRefactorer refactorer = RefactorerFactory.eINSTANCE.getRefactorer(resource);
 			refactorer.setInput(selectedElements);
 			if(refactorer != null){
 				List<Mapping> mappings = refactorer.getPossibleMappings(1.0);
+				boolean containsEntries = false;
 				for (Mapping mapping : mappings) {
-					String refactoringName = StringUtil.convertCamelCaseToWords(mapping.getName());
-					Action refactoringAction = new RefactoringAction(mapping, refactorer);
-					refactoringAction.setText(refactoringName);
-					rootMenu.add(refactoringAction);
+					RefactoringSpecification refSpec = IRefactoringSpecificationRegistry.INSTANCE.getRefSpec(mapping.getMappedRoleModel());
+					if(refSpec != null){
+						String refactoringName = StringUtil.convertCamelCaseToWords(mapping.getName());
+						Action refactoringAction = new RefactoringAction(mapping, refactorer, resource);
+						refactoringAction.setText(refactoringName);
+						rootMenu.add(refactoringAction);
+						containsEntries = true;
+					}
 				}
-				if(mappings != null && mappings.size() >= 1){
+				if(containsEntries){
 					additions.addContributionItem(rootMenu, null);
 				}
 			}
