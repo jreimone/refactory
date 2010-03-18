91fbc39dc2e4beb24bc607760dff16b2d1823155
Jan <jreimone@users.noreply.github.com>
2010-03-18 11:18:14 +0000

    -fixed interpreter -extract method works now

--- Refactoring/Plugins/org.emftext.refactoring.specification.interpreter/src/org/emftext/refactoring/interpreter/internal/CreatePathCreator.java
+++ Refactoring/Plugins/org.emftext.refactoring.specification.interpreter/src/org/emftext/refactoring/interpreter/internal/CreatePathCreator.java
@@ -10,8 +10,7 @@ public class CreatePathCreator extends AbstractPathCreator {
 
 	@SuppressWarnings("unchecked")
 	@Override
-	protected boolean onePairLeftIndexNotNull(Object children,
-			Integer index, ReferenceMetaClassPair referencePair, Object feature) {
+	protected boolean onePairLeftIndexNotNull(Object children, Integer index, ReferenceMetaClassPair referencePair, Object feature) {
 		if(((List<EObject>) feature).size() <= index){
 			return ((List<EObject>) feature).add((EObject) children);
 		}
@@ -21,15 +20,12 @@ public class CreatePathCreator extends AbstractPathCreator {
 
 	@SuppressWarnings("unchecked")
 	@Override
-	protected boolean onePairLeftIndexNull(Object children,
-			ReferenceMetaClassPair referencePair, Object feature) {
+	protected boolean onePairLeftIndexNull(Object children, ReferenceMetaClassPair referencePair, Object feature) {
 		return ((List<EObject>) feature).add((EObject) children);
 	}
 
 	@Override
-	protected void onePairLeftUnaryReference(EObject targetParent,
-			Object children,
-			ReferenceMetaClassPair referencePair) {
+	protected void onePairLeftUnaryReference(EObject targetParent, Object children, ReferenceMetaClassPair referencePair) {
 		EReference reference = referencePair.getReference();
 		if(reference.getEReferenceType().isSuperTypeOf(((EObject) children).eClass())){
 			targetParent.eSet(referencePair.getReference(), children);
