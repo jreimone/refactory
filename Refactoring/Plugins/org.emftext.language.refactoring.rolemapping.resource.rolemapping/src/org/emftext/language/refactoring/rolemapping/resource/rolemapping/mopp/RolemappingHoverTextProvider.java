641ba29c6918d2647d375b15dae9b41974fe74a0
Jan <jreimone@users.noreply.github.com>
2010-03-19 18:27:53 +0000

    added delegation of Role objects to RolesTextHoverProvider

---
+++ Refactoring/Plugins/org.emftext.language.refactoring.rolemapping.resource.rolemapping/src/org/emftext/language/refactoring/rolemapping/resource/rolemapping/mopp/RolemappingHoverTextProvider.java
@@ -0,0 +1,24 @@
+/**
+ * <copyright>
+ * </copyright>
+ *
+ * 
+ */
+package org.emftext.language.refactoring.rolemapping.resource.rolemapping.mopp;
+
+import org.emftext.language.refactoring.roles.RolesPackage;
+import org.emftext.language.refactoring.roles.resource.rolestext.mopp.RolestextHoverTextProvider;
+
+public class RolemappingHoverTextProvider implements org.emftext.language.refactoring.rolemapping.resource.rolemapping.IRolemappingHoverTextProvider {
+	
+	private org.emftext.language.refactoring.rolemapping.resource.rolemapping.ui.RolemappingDefaultHoverTextProvider defaultProvider = new org.emftext.language.refactoring.rolemapping.resource.rolemapping.ui.RolemappingDefaultHoverTextProvider();
+	
+	public java.lang.String getHoverText(org.eclipse.emf.ecore.EObject object) {
+		if(object.eClass().getEPackage().equals(RolesPackage.eINSTANCE)){
+			RolestextHoverTextProvider rolesHoverProvider = new RolestextHoverTextProvider();
+			return rolesHoverProvider.getHoverText(object);
+		}
+		return defaultProvider.getHoverText(object);
+	}
+	
+}
