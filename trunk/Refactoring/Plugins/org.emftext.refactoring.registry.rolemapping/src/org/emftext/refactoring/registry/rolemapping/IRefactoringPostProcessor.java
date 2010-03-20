e4ab53919d42b28d0f93c77629bce3c197f75fe9
jreimann <jreimann@2290cd21-d4b4-4435-9c65-06e28a4c4f12>
2010-03-20 17:42:28 +0000

    started with postprocessor registry and interface

---
+++ trunk/Refactoring/Plugins/org.emftext.refactoring.registry.rolemapping/src/org/emftext/refactoring/registry/rolemapping/IRefactoringPostProcessor.java
@@ -0,0 +1,38 @@
+/**
+ * 
+ */
+package org.emftext.refactoring.registry.rolemapping;
+
+import java.util.Map;
+
+import org.eclipse.emf.ecore.EObject;
+import org.emftext.language.refactoring.rolemapping.ConcreteMapping;
+import org.emftext.language.refactoring.rolemapping.Mapping;
+import org.emftext.language.refactoring.rolemapping.RoleMappingModel;
+import org.emftext.language.refactoring.roles.Role;
+
+/**
+ * This interface is intended to be implemented by postprocessors which can hook into the refactoring process, 
+ * after the specified steps, to do some additional calculation steps like e.g. inferring over the needed parameters
+ * of a method in the 'Extract Method' refactoring for Java. 
+ * <br><br>
+ * Every {@link IRefactoringPostProcessor} will be registered in the {@link IRoleMappingRegistry} and is therein
+ * connected to a {@link Mapping}. That means that every postprocessor belongs to a specific metamodel and a specific
+ * {@link Mapping mapping} in the {@link RoleMappingModel role mapping} registered for that metamodel.
+ * 
+ * @author Jan Reimann
+ *
+ */
+public interface IRefactoringPostProcessor {
+
+	/**
+	 * This method will be invoked after the refactoring process. Do some additional transformations here by using 
+	 * the given <code>roleRuntimeInstanceMap</code>. Its keys are the names of the {@link Role roles} used in the
+	 * {@link ConcreteMapping concrete mappings} of the {@link Mapping mapping} to which this postprocessor is connected.
+	 * The value of each key can be a single {@link EObject object} or a list of {@link EObject objects} representing
+	 * the concrete objects on which the roles were bound at runtime of the refactoring.
+	 * 
+	 * @param roleRuntimeInstanceMap the map containing {@link Role role} names as keys and its at runtime bound {@link EObject objects} as values 
+	 */
+	public void process(Map<String, Object> roleRuntimeInstanceMap);
+}
--- trunk/Refactoring/Plugins/org.emftext.refactoring.registry.rolemapping/src/org/emftext/refactoring/registry/rolemapping/IRoleMappingRegistry.java
+++ trunk/Refactoring/Plugins/org.emftext.refactoring.registry.rolemapping/src/org/emftext/refactoring/registry/rolemapping/IRoleMappingRegistry.java
@@ -2,12 +2,13 @@ package org.emftext.refactoring.registry.rolemapping;
 
 import java.util.Map;
 
+import org.emftext.language.refactoring.rolemapping.Mapping;
 import org.emftext.language.refactoring.rolemapping.RoleMappingModel;
 import org.emftext.refactoring.registry.rolemapping.exceptions.RoleMappingAlreadyRegistered;
 import org.emftext.refactoring.registry.rolemapping.impl.BasicRoleMappingRegistry;
 
 /**
- * Interface for {@link RoleMappingModel}s.
+ * Interface for the registry where {@link RoleMappingModel}s are registered to a specific metamodel.
  * 
  * @author jreimann
  *
@@ -35,4 +36,24 @@ public interface IRoleMappingRegistry {
 	 * @return
 	 */
 	public Map<String, RoleMappingModel> getRoleMappingMap();
+	
+	/**
+	 * Use this method to register a {@link IRefactoringPostProcessor postprocessor} for the given <code>roleMapping</code> and 
+	 * <code>mapping</code>.
+	 * 
+	 * @param roleMapping
+	 * @param mapping
+	 * @param postProcessor
+	 */
+	public void registerPostProcessor(RoleMappingModel roleMapping, Mapping mapping, IRefactoringPostProcessor postProcessor);
+	
+	/**
+	 * Returns the postprocessor registered to the metamodel, specified by the given <code>nsUri</code>, and the {@link Mapping mapping},
+	 * specified by the given <code>mappingName</code>. 
+	 * 
+	 * @param nsUri
+	 * @param mappingName
+	 * @return
+	 */
+	public IRefactoringPostProcessor getPostProcessor(String nsUri, String mappingName);
 }
