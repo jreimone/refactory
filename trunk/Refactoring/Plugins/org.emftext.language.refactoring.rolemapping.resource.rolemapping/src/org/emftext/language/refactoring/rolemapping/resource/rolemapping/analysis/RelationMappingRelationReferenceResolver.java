cfc1d9115736bc9b86e835a6ef684ec553066a0f
mseifert <mseifert@2290cd21-d4b4-4435-9c65-06e28a4c4f12>
2010-03-26 17:17:22 +0000

    fixed fuzzy resolving

--- trunk/Refactoring/Plugins/org.emftext.language.refactoring.rolemapping.resource.rolemapping/src/org/emftext/language/refactoring/rolemapping/resource/rolemapping/analysis/RelationMappingRelationReferenceResolver.java
+++ trunk/Refactoring/Plugins/org.emftext.language.refactoring.rolemapping.resource.rolemapping/src/org/emftext/language/refactoring/rolemapping/resource/rolemapping/analysis/RelationMappingRelationReferenceResolver.java
@@ -6,53 +6,58 @@
  */
 package org.emftext.language.refactoring.rolemapping.resource.rolemapping.analysis;
 
+import java.util.Map;
+
 import org.eclipse.emf.common.util.EList;
 import org.eclipse.emf.ecore.EObject;
+import org.eclipse.emf.ecore.EReference;
 import org.emftext.language.refactoring.rolemapping.ConcreteMapping;
+import org.emftext.language.refactoring.rolemapping.RelationMapping;
+import org.emftext.language.refactoring.rolemapping.resource.rolemapping.IRolemappingReferenceResolveResult;
+import org.emftext.language.refactoring.rolemapping.resource.rolemapping.IRolemappingReferenceResolver;
 import org.emftext.language.refactoring.roles.MultiplicityRelation;
 import org.emftext.language.refactoring.roles.Relation;
 import org.emftext.language.refactoring.roles.Role;
 
-public class RelationMappingRelationReferenceResolver implements org.emftext.language.refactoring.rolemapping.resource.rolemapping.IRolemappingReferenceResolver<org.emftext.language.refactoring.rolemapping.RelationMapping, org.emftext.language.refactoring.roles.MultiplicityRelation> {
+public class RelationMappingRelationReferenceResolver implements IRolemappingReferenceResolver<RelationMapping, MultiplicityRelation> {
 
-	private org.emftext.language.refactoring.rolemapping.resource.rolemapping.analysis.RolemappingDefaultResolverDelegate<org.emftext.language.refactoring.rolemapping.RelationMapping, org.emftext.language.refactoring.roles.MultiplicityRelation> delegate = new org.emftext.language.refactoring.rolemapping.resource.rolemapping.analysis.RolemappingDefaultResolverDelegate<org.emftext.language.refactoring.rolemapping.RelationMapping, org.emftext.language.refactoring.roles.MultiplicityRelation>();
-
-	public void resolve(java.lang.String identifier, org.emftext.language.refactoring.rolemapping.RelationMapping container, org.eclipse.emf.ecore.EReference reference, int position, boolean resolveFuzzy, final org.emftext.language.refactoring.rolemapping.resource.rolemapping.IRolemappingReferenceResolveResult<org.emftext.language.refactoring.roles.MultiplicityRelation> result) {
+	public void resolve(String identifier, RelationMapping container, EReference reference, int position, boolean resolveFuzzy, IRolemappingReferenceResolveResult<MultiplicityRelation> result) {
 		EObject parent = container.eContainer();
-		if(!(parent instanceof ConcreteMapping)){
+		if (!(parent instanceof ConcreteMapping)) {
 			result.setErrorMessage("Invalid RoleMapping Model");
 			return;
 		}
 		ConcreteMapping concreteMapping = (ConcreteMapping) parent;
 		String targetName = identifier;
-		if(targetName == null){
+		if (targetName == null) {
 			targetName = "";
 		}
 		targetName = targetName.trim();
 		Role mappedRole = concreteMapping.getRole();
 		EList<Relation> outgoingRelations = mappedRole.getOutgoing();
 		for (Relation relation : outgoingRelations) {
-			if(relation instanceof MultiplicityRelation){
+			if (relation instanceof MultiplicityRelation) {
 				String relationTargetName = ((MultiplicityRelation) relation).getTargetName();
-				if(relationTargetName == null){
+				if (relationTargetName == null) {
 					relationTargetName = "";
 				}
 				relationTargetName = relationTargetName.trim();
-				if(targetName.equals(relationTargetName)){
-					result.addMapping(targetName, (MultiplicityRelation) relation);
-					return;
+				if (targetName.equals(relationTargetName) || resolveFuzzy) {
+					result.addMapping(relationTargetName, (MultiplicityRelation) relation);
+					if (!resolveFuzzy) {
+						return;
+					}
 				}
 			}
 		}
 		result.setErrorMessage("Role '" + mappedRole.getName() + "' has no outgoing relation with target name '" + targetName + "'");
 	}
 
-	public java.lang.String deResolve(org.emftext.language.refactoring.roles.MultiplicityRelation element, org.emftext.language.refactoring.rolemapping.RelationMapping container, org.eclipse.emf.ecore.EReference reference) {
+	public String deResolve(MultiplicityRelation element, RelationMapping container, EReference reference) {
 		return element.getTargetName();
 	}
 
-	public void setOptions(java.util.Map<?,?> options) {
+	public void setOptions(Map<?,?> options) {
 		// save options in a field or leave method empty if this resolver does not depend on any option
 	}
-
 }
