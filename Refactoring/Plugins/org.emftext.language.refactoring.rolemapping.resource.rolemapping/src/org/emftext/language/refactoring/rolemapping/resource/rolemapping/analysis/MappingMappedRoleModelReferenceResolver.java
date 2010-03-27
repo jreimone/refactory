3546ef7c2bb4b363dd5eaae8ebb62e6d1f158661
Jan <jreimone@users.noreply.github.com>
2010-03-27 15:55:12 +0000

    tried to activate fuzzy resolving

--- Refactoring/Plugins/org.emftext.language.refactoring.rolemapping.resource.rolemapping/src/org/emftext/language/refactoring/rolemapping/resource/rolemapping/analysis/MappingMappedRoleModelReferenceResolver.java
+++ Refactoring/Plugins/org.emftext.language.refactoring.rolemapping.resource.rolemapping/src/org/emftext/language/refactoring/rolemapping/resource/rolemapping/analysis/MappingMappedRoleModelReferenceResolver.java
@@ -28,6 +28,11 @@ public class MappingMappedRoleModelReferenceResolver implements org.emftext.lang
 	public void resolve(java.lang.String identifier, org.emftext.language.refactoring.rolemapping.Mapping container, org.eclipse.emf.ecore.EReference reference, int position,
 			boolean resolveFuzzy, final org.emftext.language.refactoring.rolemapping.resource.rolemapping.IRolemappingReferenceResolveResult<org.emftext.language.refactoring.roles.RoleModel> result) {
 		IRoleModelRegistry registry = IRoleModelRegistry.INSTANCE;
+		if(resolveFuzzy){
+			for (RoleModel roleModel : registry.getAllRegisteredRoleModels()) {
+				result.addMapping(identifier, roleModel);
+			}
+		}
 		RoleModel model = registry.getRoleModelByName(identifier);
 		if(model != null){
 			result.addMapping(identifier, model);
