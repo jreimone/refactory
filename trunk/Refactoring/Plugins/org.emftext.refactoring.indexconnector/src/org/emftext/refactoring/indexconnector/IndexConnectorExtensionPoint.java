6f18f7339bdd420cdffaf7791a018b0794f73c85
jreimann <jreimann@2290cd21-d4b4-4435-9c65-06e28a4c4f12>
2010-03-27 11:35:44 +0000

    fixed extension point id

--- trunk/Refactoring/Plugins/org.emftext.refactoring.indexconnector/src/org/emftext/refactoring/indexconnector/IndexConnectorExtensionPoint.java
+++ trunk/Refactoring/Plugins/org.emftext.refactoring.indexconnector/src/org/emftext/refactoring/indexconnector/IndexConnectorExtensionPoint.java
@@ -2,6 +2,6 @@ package org.emftext.refactoring.indexconnector;
 
 public interface IndexConnectorExtensionPoint {
 
-	public final static String ID 				= "org.emftext.refactoring.indexconnector";
+	public final static String ID 				= "org.emftext.registry.indexconnector";
 	public final static String IMPLEMENTATION	= "implementation";
 }
