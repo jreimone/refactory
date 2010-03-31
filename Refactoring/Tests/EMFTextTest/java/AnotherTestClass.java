c3a8275252d08b6e57a03beb36dfb6b3adccd590
Jan <jreimone@users.noreply.github.com>
2010-03-31 14:39:17 +0000

    tests

--- Refactoring/Tests/EMFTextTest/java/AnotherTestClass.java
+++ Refactoring/Tests/EMFTextTest/java/AnotherTestClass.java
@@ -1,11 +1,11 @@
 
-public class AnotherTestClass {
 
+
+public class AnotherTestClass { 
 	/**
 	 * @param args
-	 */
-	public static void main(String[] args) {
-		HelloWorld.doPrinting();
-	}
+	 */public static void main (String [] args ) { 
+		HelloWorld .doPrinting ( ) ; 
+	} 
+} 
 
-}
