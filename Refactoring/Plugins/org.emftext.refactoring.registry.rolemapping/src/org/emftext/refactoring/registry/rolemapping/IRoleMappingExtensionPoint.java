7d806aa593a38040905fdb5f3387ebb549411b0f
Jan <jreimone@users.noreply.github.com>
2010-04-03 16:53:54 +0000

    introduced new sub entries in rolemapping registry for specifying icons

--- Refactoring/Plugins/org.emftext.refactoring.registry.rolemapping/src/org/emftext/refactoring/registry/rolemapping/IRoleMappingExtensionPoint.java
+++ Refactoring/Plugins/org.emftext.refactoring.registry.rolemapping/src/org/emftext/refactoring/registry/rolemapping/IRoleMappingExtensionPoint.java
@@ -2,6 +2,11 @@ package org.emftext.refactoring.registry.rolemapping;
 
 public interface IRoleMappingExtensionPoint {
 
-	public static final String ID 					= "org.emftext.refactoring.rolemapping";
-	public static final String RESOURCE_ATTRIBUTE 	= "rolemapping_resource";
+	public static final String ID 						= "org.emftext.refactoring.rolemapping";
+	public static final String RESOURCE_ATTRIBUTE 		= "rolemapping_resource";
+	public static final String DEFAULT_ICON_ATTRIBUTE	= "defaultIcon";
+	
+	public static final String SUB_MAPPING_ICON_ID		= "mappingIcon";
+	public static final String SUB_MAPPING_NAME 		= "mapping_name";
+	public static final String SUB_ICON_RESOURCE 		= "icon";
 }
