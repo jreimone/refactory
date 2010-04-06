c96e8c348340088bdc24ded644b2d3380a6fe3b7
jreimann <jreimann@2290cd21-d4b4-4435-9c65-06e28a4c4f12>
2010-04-06 17:46:08 +0000

    tried to integrate compare as previewer

--- trunk/Refactoring/Plugins/org.emftext.refactoring.ui/src/org/emftext/refactoring/ltk/ModelChangePreviewer.java
+++ trunk/Refactoring/Plugins/org.emftext.refactoring.ui/src/org/emftext/refactoring/ltk/ModelChangePreviewer.java
@@ -1,35 +1,25 @@
 package org.emftext.refactoring.ltk;
 
 import java.lang.reflect.InvocationTargetException;
-import java.util.LinkedHashMap;
-import java.util.Map;
 
-import org.eclipse.compare.CompareConfiguration;
-import org.eclipse.compare.CompareEditorInput;
-import org.eclipse.compare.CompareViewerPane;
-import org.eclipse.core.runtime.IProgressMonitor;
 import org.eclipse.core.runtime.NullProgressMonitor;
-import org.eclipse.emf.compare.diff.metamodel.DiffModel;
-import org.eclipse.emf.compare.diff.service.DiffService;
-import org.eclipse.emf.compare.match.MatchOptions;
-import org.eclipse.emf.compare.match.metamodel.MatchModel;
-import org.eclipse.emf.compare.match.service.MatchService;
 import org.eclipse.emf.ecore.EObject;
 import org.eclipse.ltk.ui.refactoring.ChangePreviewViewerInput;
 import org.eclipse.ltk.ui.refactoring.IChangePreviewViewer;
 import org.eclipse.swt.SWT;
+import org.eclipse.swt.custom.ViewForm;
 import org.eclipse.swt.widgets.Composite;
 import org.eclipse.swt.widgets.Control;
 
 public class ModelChangePreviewer implements IChangePreviewViewer {
 
-	private Composite control;
+	private ViewForm control;
+	private RefactoringCompareEditorInput compare;
 	//	private CompareViewerPane pane;
 
 	public void createControl(Composite parent) {
-		control = new Composite(parent, SWT.NONE);
-
-		//		pane = new CompareViewerPane(control, SWT.NONE);
+		control = new ViewForm(parent, SWT.BORDER);
+		compare = new RefactoringCompareEditorInput(control);
 	}
 
 	public Control getControl() {
@@ -40,34 +30,21 @@ public class ModelChangePreviewer implements IChangePreviewViewer {
 		if(!(input.getChange() instanceof ModelRefactoringChange)){
 			return;
 		}
-		final ModelRefactoringChange change = (ModelRefactoringChange) input.getChange();
-		CompareConfiguration config = new CompareConfiguration();
-		config.setLeftEditable(false);
-		config.setRightEditable(false);
-		CompareEditorInput editorInput = new CompareEditorInput(config) {
-
-			@Override
-			protected Object prepareInput(IProgressMonitor monitor) throws InvocationTargetException, InterruptedException {
-				EObject original = change.getRefactorer().getOriginalModel();
-				EObject fakeRefactored = change.getRefactorer().getFakeRefactoredModel();
-				Map<String, Object> options = new LinkedHashMap<String, Object>();
-				options.put(MatchOptions.OPTION_DISTINCT_METAMODELS, false);
-				MatchModel match = MatchService.doContentMatch(fakeRefactored, original, options);
-				DiffModel diff = DiffService.doDiff(match, false);
-				System.out.println(diff);
-				return diff;
-			}
-		};
-		editorInput.createContents(control);
+		ModelRefactoringChange change = (ModelRefactoringChange) input.getChange();
+		EObject originalModel = change.getRefactorer().getOriginalModel();
+		EObject fakeRefactoredModel = change.getRefactorer().getFakeRefactoredModel();
+		compare.setOriginalModel(originalModel);
+		compare.setFakeRefactoredModel(fakeRefactoredModel);
 		try {
-			editorInput.run(new NullProgressMonitor());
+			compare.run(new NullProgressMonitor());
 		} catch (InterruptedException e) {
 			e.printStackTrace();
 		} catch (InvocationTargetException e) {
-			// TODO Auto-generated catch block
 			e.printStackTrace();
 		}
-		System.out.println(editorInput);
+		compare.getControl().pack(true);
+		compare.getControl().setVisible(true);
+		System.out.println(compare);
 	}
 
 }
