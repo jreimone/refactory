34d63d39087f895aa3384d2c0edf69414b81c8b4
Jan <jreimone@users.noreply.github.com>
2010-05-04 16:14:10 +0000

    removed attribute 'name' from RoleMapping

--- Refactoring/Plugins/org.emftext.refactoring.registry.rolemapping/src/org/emftext/refactoring/registry/rolemapping/impl/BasicRoleMappingRegistry.java
+++ Refactoring/Plugins/org.emftext.refactoring.registry.rolemapping/src/org/emftext/refactoring/registry/rolemapping/impl/BasicRoleMappingRegistry.java
@@ -4,7 +4,6 @@ import java.io.IOException;
 import java.io.InputStream;
 import java.net.MalformedURLException;
 import java.util.ArrayList;
-import java.util.Collections;
 import java.util.LinkedHashMap;
 import java.util.LinkedList;
 import java.util.List;
