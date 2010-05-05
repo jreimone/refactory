290127674332e20c468563127ccc9e8be8172bbd
jreimann <jreimann@2290cd21-d4b4-4435-9c65-06e28a4c4f12>
2010-05-05 17:25:04 +0000

    added test method

--- trunk/Refactoring/Tests/EMFTextTest/java/AnotherTestClass.java
+++ trunk/Refactoring/Tests/EMFTextTest/java/AnotherTestClass.java
@@ -4,11 +4,12 @@
 public class AnotherTestClass { 
 	/**
 	 * @param args
-	 */
-	 public static void main (String [] args, int count, boolean isSomething) { 
-		System.out.println(count);
-		HelloWorld .printOutput () ;
-		System.out.println(count);
+	 */public static void main (String [] args , int count , boolean isSomething ) { 
+		System .out .println ( count ) ; 
+		HelloWorld .printOutput ( ) ; 
+		System .out .println ( count ) ; 
+	} 
+	private void test () { 
 	} 
 } 
 
