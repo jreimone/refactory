cc5bc9124f5e03932fdf3ae289784c076851af45
Jan <jreimone@users.noreply.github.com>
2010-07-06 09:14:40 +0000

    -added nicer icon for RoleModel and registry view -registry view now lives in the category 'Refactory'

--- Refactoring/Plugins/org.emftext.language.refactoring.roles.edit/src/org/emftext/language/refactoring/roles/provider/RoleModelItemProvider.java
+++ Refactoring/Plugins/org.emftext.language.refactoring.roles.edit/src/org/emftext/language/refactoring/roles/provider/RoleModelItemProvider.java
@@ -101,11 +101,11 @@ public class RoleModelItemProvider
 	 * This returns RoleModel.gif.
 	 * <!-- begin-user-doc -->
 	 * <!-- end-user-doc -->
-	 * @generated
+	 * @generated NOT
 	 */
 	@Override
 	public Object getImage(Object object) {
-		return overlayImage(object, getResourceLocator().getImage("full/obj16/RoleModel"));
+		return overlayImage(object, getResourceLocator().getImage("new/rolemodel"));
 	}
 
 	/**
