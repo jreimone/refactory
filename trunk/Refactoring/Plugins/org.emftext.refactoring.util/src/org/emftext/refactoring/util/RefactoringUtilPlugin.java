3c19ebc40e0b25e574e77aed0d4154ab9d4769b4
jreimann <jreimann@2290cd21-d4b4-4435-9c65-06e28a4c4f12>
2010-07-10 10:46:45 +0000

    added null check and adapted tests

--- trunk/Refactoring/Plugins/org.emftext.refactoring.util/src/org/emftext/refactoring/util/RefactoringUtilPlugin.java
+++ trunk/Refactoring/Plugins/org.emftext.refactoring.util/src/org/emftext/refactoring/util/RefactoringUtilPlugin.java
@@ -7,17 +7,18 @@ public class RefactoringUtilPlugin extends Plugin {
 
 	// The plug-in ID
 	public static final String PLUGIN_ID = "org.emftext.refactoring.util";
-	
+
 	// The shared instance
 	private static RefactoringUtilPlugin plugin;
-	
+
 	public RefactoringUtilPlugin() {
 		// TODO Auto-generated constructor stub
 	}
 
 	/*
 	 * (non-Javadoc)
-	 * @see org.eclipse.core.runtime.Plugins#start(org.osgi.framework.BundleContext)
+	 * @see
+	 * org.eclipse.core.runtime.Plugins#start(org.osgi.framework.BundleContext)
 	 */
 	public void start(BundleContext context) throws Exception {
 		super.start(context);
@@ -26,7 +27,8 @@ public class RefactoringUtilPlugin extends Plugin {
 
 	/*
 	 * (non-Javadoc)
-	 * @see org.eclipse.core.runtime.Plugin#stop(org.osgi.framework.BundleContext)
+	 * @see
+	 * org.eclipse.core.runtime.Plugin#stop(org.osgi.framework.BundleContext)
 	 */
 	public void stop(BundleContext context) throws Exception {
 		plugin = null;
@@ -39,6 +41,9 @@ public class RefactoringUtilPlugin extends Plugin {
 	 * @return the shared instance
 	 */
 	public static RefactoringUtilPlugin getDefault() {
+		if (plugin == null) {
+			plugin = new RefactoringUtilPlugin();
+		}
 		return plugin;
 	}
 }
