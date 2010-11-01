0a182668a7f20292e8675b8a2f9f4fd12949c171
jreimann <jreimann@2290cd21-d4b4-4435-9c65-06e28a4c4f12>
2010-11-01 16:44:00 +0000

    added example postprocessor plugin for EcoreText

---
+++ trunk/Refactoring/Tests/org.emftext.language.ecore.resource.text.postprocessing/src/org/emftext/language/ecore/resource/text/postprocessing/AbstractPostProcessor.java
@@ -0,0 +1,41 @@
+package org.emftext.language.ecore.resource.text.postprocessing;
+
+import java.util.List;
+
+import org.eclipse.emf.ecore.EObject;
+import org.eclipse.emf.ecore.EPackage;
+import org.emftext.language.ecore.resource.text.ITextEcoreQuickFix;
+import org.emftext.language.ecore.resource.text.ITextEcoreResourcePostProcessor;
+import org.emftext.language.ecore.resource.text.ITextEcoreResourcePostProcessorProvider;
+import org.emftext.language.ecore.resource.text.mopp.TextEcoreProblem;
+import org.emftext.language.ecore.resource.text.mopp.TextEcoreResource;
+
+public abstract class AbstractPostProcessor implements
+		ITextEcoreResourcePostProcessorProvider,
+		ITextEcoreResourcePostProcessor {
+	
+	@Override
+	public ITextEcoreResourcePostProcessor getResourcePostProcessor() {
+		return this;
+	}
+
+	@Override
+	public void process(TextEcoreResource resource) {
+		List<EObject> contents = resource.getContents();
+		for (EObject eObject : contents) {
+			if(eObject instanceof EPackage){
+				analyse(resource, (EPackage) eObject);
+			}
+		}
+	}
+	
+	protected void addProblem(TextEcoreResource resource, ETextEcoreProblemType problemType, String message, EObject cause){
+		resource.addProblem(new TextEcoreProblem(message, problemType.getProblemType()), cause);
+	}
+	
+	protected void addProblem(TextEcoreResource resource, ETextEcoreProblemType problemType, String message, EObject cause, ITextEcoreQuickFix quickFix){
+		resource.addProblem(new TextEcoreProblem(message, problemType.getProblemType(), quickFix), cause);
+	}
+	
+	public abstract void analyse(TextEcoreResource resource, EPackage epackage);
+}
---
+++ trunk/Refactoring/Tests/org.emftext.language.ecore.resource.text.postprocessing/src/org/emftext/language/ecore/resource/text/postprocessing/TextEcoreOptionProvider.java
@@ -0,0 +1,29 @@
+package org.emftext.language.ecore.resource.text.postprocessing;
+
+import java.util.HashMap;
+import java.util.LinkedList;
+import java.util.List;
+import java.util.Map;
+
+import org.emftext.language.ecore.resource.text.ITextEcoreOptionProvider;
+import org.emftext.language.ecore.resource.text.ITextEcoreOptions;
+import org.emftext.language.ecore.resource.text.ITextEcoreResourcePostProcessorProvider;
+
+public class TextEcoreOptionProvider implements ITextEcoreOptionProvider {
+
+	public TextEcoreOptionProvider() {
+		super();
+	}
+
+	@Override
+	public Map<?, ?> getOptions() {
+		Map<String, Object> options = new HashMap<String, Object>();
+		
+		List<ITextEcoreResourcePostProcessorProvider> postProcessors = new LinkedList<ITextEcoreResourcePostProcessorProvider>();
+		postProcessors.add(new SameAttributesInHierarchyAnalyser());
+		
+		options.put(ITextEcoreOptions.RESOURCE_POSTPROCESSOR_PROVIDER, postProcessors);
+		return options;
+	}
+
+}
