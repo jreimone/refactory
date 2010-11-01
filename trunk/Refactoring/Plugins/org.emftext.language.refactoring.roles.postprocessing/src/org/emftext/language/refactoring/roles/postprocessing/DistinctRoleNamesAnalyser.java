3d588ab7eb0ccb666fab8bab91c4a99650351d48
jreimann <jreimann@2290cd21-d4b4-4435-9c65-06e28a4c4f12>
2010-11-01 17:55:32 +0000

    simplified quickfix by using facade

--- trunk/Refactoring/Plugins/org.emftext.language.refactoring.roles.postprocessing/src/org/emftext/language/refactoring/roles/postprocessing/DistinctRoleNamesAnalyser.java
+++ trunk/Refactoring/Plugins/org.emftext.language.refactoring.roles.postprocessing/src/org/emftext/language/refactoring/roles/postprocessing/DistinctRoleNamesAnalyser.java
@@ -12,11 +12,10 @@ import org.emftext.language.refactoring.rolemapping.RoleMapping;
 import org.emftext.language.refactoring.roles.Role;
 import org.emftext.language.refactoring.roles.RoleModel;
 import org.emftext.language.refactoring.roles.quickfixes.RenameRoleRefactoringQuickFix;
+import org.emftext.language.refactoring.roles.refactoring.RolesRefactoringFacade;
 import org.emftext.language.refactoring.roles.resource.rolestext.IRolestextQuickFix;
 import org.emftext.language.refactoring.roles.resource.rolestext.mopp.RolestextResource;
 import org.emftext.refactoring.interpreter.IRefactorer;
-import org.emftext.refactoring.interpreter.RefactorerFactory;
-import org.emftext.refactoring.registry.rolemapping.IRoleMappingRegistry;
 
 public class DistinctRoleNamesAnalyser extends AbstractPostProcessor {
 
@@ -26,7 +25,6 @@ public class DistinctRoleNamesAnalyser extends AbstractPostProcessor {
 
 	@Override
 	public void analyse(RolestextResource resource, RoleModel model) {
-		EcoreUtil.resolveAll(model);
 		roleNameMap = new HashMap<String, Role>();
 		List<Role> roles = model.getRoles();
 		for (Role role : roles) {
@@ -35,29 +33,21 @@ public class DistinctRoleNamesAnalyser extends AbstractPostProcessor {
 				roleNameMap.put(role.getName(), role);
 			} else {
 				if(!foundRole.equals(role)){
-					String originalName = role.getName();
-					IRefactorer refactorer = RefactorerFactory.eINSTANCE.getRefactorer(resource);
+					List<EObject> selectedElement = new ArrayList<EObject>();
+					selectedElement.add(foundRole);
+					
+					//// always the same?
+					RolesRefactoringFacade facade = new RolesRefactoringFacade(resource, selectedElement);
+					IRefactorer refactorer = facade.getRefactorer();
+					RoleMapping roleMapping = facade.getRenameRoleModelElementMapping();
+					URL iconBundlePath = facade.getRenameRoleModelElementIcon();
 					if(refactorer != null){
-						List<EObject> selectedElement = new ArrayList<EObject>();
-						selectedElement.add(foundRole);
-						refactorer.setInput(selectedElement);
-						List<RoleMapping> mappings = refactorer.getPossibleRoleMappings(1.0);
-						for (RoleMapping roleMapping : mappings) {
-							if(roleMapping.getName().toLowerCase().contains("rename")){
-								//								ImageDescriptor image = IRoleMappingRegistry.INSTANCE.getImageForMapping(roleMapping);
-								URL iconBundlePath = IRoleMappingRegistry.INSTANCE.getImagePathForMapping(roleMapping);
-								String iconBundlePathString = null;
-								if(iconBundlePath != null){
-									iconBundlePathString = iconBundlePath.toString();
-								}
-								IRolestextQuickFix quickfix = new RenameRoleRefactoringQuickFix(roleMapping, foundRole, role, originalName, refactorer, iconBundlePathString);
-								addProblem(resource
-										, ERoleModelProblemType.DUPLICATE_ROLE_NAMES
-										, String.format(DUPLICATE_ROLE_NAMES, foundRole.getName())
-										, role
-										, quickfix);
-							}
-						}
+						IRolestextQuickFix quickfix = new RenameRoleRefactoringQuickFix(roleMapping, foundRole, refactorer, (iconBundlePath != null) ? iconBundlePath.toString() : null);
+						addProblem(resource
+								, ERoleModelProblemType.DUPLICATE_ROLE_NAMES
+								, String.format(DUPLICATE_ROLE_NAMES, foundRole.getName())
+								, role
+								, quickfix);
 					} else {
 						addProblem(resource
 								, ERoleModelProblemType.DUPLICATE_ROLE_NAMES
--- trunk/Refactoring/Plugins/org.emftext.language.refactoring.roles.postprocessing/src/org/emftext/language/refactoring/roles/quickfixes/RenameRoleRefactoringQuickFix.java
+++ trunk/Refactoring/Plugins/org.emftext.language.refactoring.roles.postprocessing/src/org/emftext/language/refactoring/roles/quickfixes/RenameRoleRefactoringQuickFix.java
@@ -1,64 +1,35 @@
 package org.emftext.language.refactoring.roles.quickfixes;
 
-import java.util.ArrayList;
-import java.util.List;
-
-import org.eclipse.core.runtime.CoreException;
-import org.eclipse.core.runtime.IConfigurationElement;
 import org.eclipse.ui.IEditorPart;
 import org.eclipse.ui.PlatformUI;
 import org.emftext.language.refactoring.rolemapping.RoleMapping;
 import org.emftext.language.refactoring.roles.Role;
 import org.emftext.language.refactoring.roles.resource.rolestext.mopp.RolestextQuickFix;
 import org.emftext.refactoring.editorconnector.IEditorConnector;
-import org.emftext.refactoring.editorconnector.IEditorConnectorExtensionPoint;
+import org.emftext.refactoring.editorconnector.IEditorConnectorRegistry;
 import org.emftext.refactoring.interpreter.IRefactorer;
 import org.emftext.refactoring.ui.RefactoringAction;
-import org.emftext.refactoring.util.RegistryUtil;
 
 public class RenameRoleRefactoringQuickFix extends RolestextQuickFix {
 
 	private static final String REFACTOR_RENAME_OTHER_ROLE = "Refactor: Rename other role";
 
 	private IRefactorer refactorer;
-	private Role secondRole;
-	private String originalName;
 	private RoleMapping mapping;
 
-	public RenameRoleRefactoringQuickFix(RoleMapping mapping, Role roleToBeRefactored, Role secondRole, String originalName, IRefactorer refactorer, String iconKey){
+	public RenameRoleRefactoringQuickFix(RoleMapping mapping, Role roleToBeRefactored, IRefactorer refactorer, String iconKey){
 		super(REFACTOR_RENAME_OTHER_ROLE, iconKey, roleToBeRefactored);
-		this.secondRole = secondRole;
 		this.refactorer = refactorer;
-		this.originalName = originalName;
 		this.mapping = mapping;
 	}
 
 	@Override
 	public void applyChanges() {
-		System.out.println("RenameRoleRefactoringQuickFix.applyChanges()");
-		IEditorConnector connector = determineEditorConnector();
-		RefactoringAction action = new RefactoringAction(mapping, refactorer, connector);
-//		action.setText(REFACTOR_RENAME_OTHER_ROLE);
-		action.run();
-	}
-
-	private IEditorConnector determineEditorConnector(){
-		IConfigurationElement[] elements = RegistryUtil.collectConfigurationElements(IEditorConnectorExtensionPoint.ID);
-		List<IEditorConnector> editorConnectors = new ArrayList<IEditorConnector>();
-		for (IConfigurationElement element : elements) {
-			try {
-				IEditorConnector connector = (IEditorConnector) element.createExecutableExtension(IEditorConnectorExtensionPoint.CONNECTOR_ATTRIBUTE);
-				editorConnectors.add(connector);
-			} catch (CoreException e) {
-				e.printStackTrace();
-			}
-		}
 		IEditorPart activeEditor = PlatformUI.getWorkbench().getActiveWorkbenchWindow().getActivePage().getActiveEditor();
-		for (IEditorConnector connector : editorConnectors) {
-			if(connector.canHandle(activeEditor)){
-				return connector;
-			}
+		IEditorConnector connector = IEditorConnectorRegistry.INSTANCE.getEditorConnectorForEditorPart(activeEditor);
+		if(connector != null){
+			RefactoringAction action = new RefactoringAction(mapping, refactorer, connector);
+			action.run();
 		}
-		return null;
 	}
 }
