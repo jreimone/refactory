2aacd91892258ec3285136fb631839f77c868ff7
Jan <jreimone@users.noreply.github.com>
2010-11-03 18:20:50 +0000

    new codegen plugin

---
+++ Refactoring/Plugins/org.emftext.refactoring.codegen/src/org/emftext/refactoring/codegen/GenerateFacadeHandler.java
@@ -0,0 +1,82 @@
+package org.emftext.refactoring.codegen;
+
+import java.util.LinkedList;
+import java.util.List;
+
+import org.eclipse.core.commands.AbstractHandler;
+import org.eclipse.core.commands.ExecutionEvent;
+import org.eclipse.core.commands.ExecutionException;
+import org.eclipse.core.resources.IFolder;
+import org.eclipse.core.resources.IProject;
+import org.eclipse.core.resources.IResource;
+import org.eclipse.core.runtime.CoreException;
+import org.eclipse.core.runtime.IPath;
+import org.eclipse.emf.common.util.URI;
+import org.eclipse.emf.ecore.EObject;
+import org.eclipse.emf.ecore.resource.Resource;
+import org.eclipse.emf.ecore.resource.ResourceSet;
+import org.eclipse.emf.ecore.resource.impl.ResourceSetImpl;
+import org.eclipse.jface.viewers.ISelection;
+import org.eclipse.jface.viewers.IStructuredSelection;
+import org.eclipse.ui.ISelectionService;
+import org.eclipse.ui.PlatformUI;
+import org.emftext.language.refactoring.rolemapping.RoleMapping;
+import org.emftext.language.refactoring.rolemapping.RoleMappingModel;
+
+public class GenerateFacadeHandler extends AbstractHandler {
+
+	private List<RoleMapping> foundMappings;
+	private IFolder mappingsFolder;
+	
+	public Object execute(ExecutionEvent event) throws ExecutionException {
+		IProject project = mappingsFolder.getProject();
+		if(project != null && project.exists()){
+			GenerateRoleMappingFacadesJob job = new GenerateRoleMappingFacadesJob(project, foundMappings);
+			job.setUser(true);
+			job.schedule();
+		}
+		return null;
+	}
+
+	@Override
+	public boolean isEnabled() {
+		foundMappings = new LinkedList<RoleMapping>();
+		mappingsFolder = null;
+		ISelectionService selectionService = PlatformUI.getWorkbench().getActiveWorkbenchWindow().getSelectionService();
+		ISelection selection = selectionService.getSelection();
+		if(selection instanceof IStructuredSelection){
+			IStructuredSelection structuredSelection = (IStructuredSelection) selection;
+			Object firstElement = structuredSelection.getFirstElement();
+			if(firstElement instanceof IFolder){
+				mappingsFolder = (IFolder) firstElement;
+				try {
+					IResource[] members = mappingsFolder.members();
+					ResourceSet rs = new ResourceSetImpl();
+					for (IResource resource : members) {
+						try {
+							IPath path = resource.getLocation();
+							URI uri = URI.createFileURI(path.toFile().getAbsolutePath());
+							Resource modelResource = rs.getResource(uri, true);
+							if(modelResource != null){
+								List<EObject> contents = modelResource.getContents();
+								for (EObject eObject : contents) {
+									if(eObject instanceof RoleMappingModel){
+										foundMappings.addAll(((RoleMappingModel) eObject).getMappings());
+									}
+								}
+							}
+						} catch (Exception e) {
+							// just do nothing
+						}
+					}
+					if(mappingsFolder != null && foundMappings.size() > 0){
+						return true;
+					}
+				} catch (CoreException e) {
+					return false;
+				}
+			}
+		}
+		return false;
+	}
+}
---
+++ Refactoring/Plugins/org.emftext.refactoring.codegen/src/org/emftext/refactoring/codegen/GenerateRoleMappingFacadesJob.java
@@ -0,0 +1,222 @@
+package org.emftext.refactoring.codegen;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.LinkedHashMap;
+import java.util.LinkedList;
+import java.util.List;
+import java.util.Map;
+
+import org.eclipse.core.resources.IFolder;
+import org.eclipse.core.resources.IProject;
+import org.eclipse.core.resources.IProjectNature;
+import org.eclipse.core.resources.IResource;
+import org.eclipse.core.runtime.CoreException;
+import org.eclipse.core.runtime.IProgressMonitor;
+import org.eclipse.core.runtime.IStatus;
+import org.eclipse.core.runtime.Status;
+import org.eclipse.core.runtime.jobs.Job;
+import org.eclipse.emf.ecore.EPackage;
+import org.eclipse.jdt.core.IClasspathEntry;
+import org.eclipse.jdt.core.IJavaProject;
+import org.eclipse.jdt.core.IPackageFragment;
+import org.eclipse.jdt.core.IPackageFragmentRoot;
+import org.eclipse.jdt.core.JavaCore;
+import org.eclipse.jdt.core.JavaModelException;
+import org.emftext.language.refactoring.rolemapping.RoleMapping;
+import org.emftext.refactoring.util.StringUtil;
+import org.emftext.sdk.codegen.composites.JavaComposite;
+
+public class GenerateRoleMappingFacadesJob extends Job {
+
+	private static final String GENERAL_JOB_NAME 		= "Generate Facade(s) for RoleMappings";
+	private static final String GENERATE_FACADE_PACKAGE	= "Generate facade package: %1$s";
+	private static final String FACADE_POSTFIX			= ".facade";
+	private static final String CLASS_NAME_POSTFIX		= "RefactoringFacade";
+
+	private IProject project;
+	private List<RoleMapping> foundMappings;
+	private IPackageFragment fragment;
+	private Map<EPackage, List<RoleMapping>> metamodelRoleMappings;
+
+	public GenerateRoleMappingFacadesJob(IProject project, List<RoleMapping> foundMappings) {
+		super(GENERAL_JOB_NAME);
+		this.project = project;
+		this.foundMappings = foundMappings;
+	}
+
+	@Override
+	protected IStatus run(IProgressMonitor monitor) {
+		metamodelRoleMappings = new LinkedHashMap<EPackage, List<RoleMapping>>();
+		int ticks = 0;
+		for (RoleMapping roleMapping : foundMappings) {
+			EPackage metamodel = roleMapping.getOwningMappingModel().getTargetMetamodel();
+			List<RoleMapping> specificRoleMappings = metamodelRoleMappings.get(metamodel);
+			if(specificRoleMappings == null){
+				specificRoleMappings = new LinkedList<RoleMapping>();
+				metamodelRoleMappings.put(metamodel, specificRoleMappings);
+				// one tick for every Java file
+				ticks++;
+			}
+			specificRoleMappings.add(roleMapping);
+			// one tick for every method for a RoleMapping
+			ticks++;
+		}
+		// one tick for creating the package
+		ticks++;
+		monitor.beginTask(GENERAL_JOB_NAME, ticks);
+		monitor.subTask(String.format(GENERATE_FACADE_PACKAGE, project.getName() + FACADE_POSTFIX));
+		if(!project.isOpen()){
+			try {
+				project.open(monitor);
+			} catch (CoreException e) {
+				e.printStackTrace();
+			}
+		}
+		fragment = createFacadePackage(monitor);
+		monitor.worked(1);
+		addPluginDependencies();
+		generateCompilationUnits(monitor);
+		try {
+			project.refreshLocal(IResource.DEPTH_INFINITE, monitor);
+		} catch (CoreException e) {
+			e.printStackTrace();
+		}
+		monitor.done();
+		return Status.OK_STATUS;
+	}
+
+	private void generateCompilationUnits(IProgressMonitor monitor) {
+		for (EPackage epackage : metamodelRoleMappings.keySet()) {
+			String className = StringUtil.firstLetterUpperCase(epackage.getName()) + CLASS_NAME_POSTFIX;
+			monitor.subTask("Generating class " + className);
+			JavaComposite sc = new JavaComposite();
+			sc.add("package " + fragment.getElementName() + ";");
+			sc.addLineBreak();
+			addImports(sc);
+			sc.addJavadoc("Provides a facade for simpler access to the registered {@link RoleMapping}s for"
+					, "the metamodel: http://www.emftext.org/language/roles.<br>"
+					, "For every registered mapping two methods are contained in this facade:"
+					, "<li> a method for returning the {@link RoleMapping} itself"
+					, "<li> a method for returning the registered icon if one exists"
+					, "<br>"
+					, "@author jreimann");
+			sc.add("public class " + className + " {");
+			sc.addLineBreak();
+			sc.add("private Resource resource;");
+			sc.add("private List<EObject> selectedElement;");
+			sc.add("private Map<String, RoleMapping> mappingMap;");
+			sc.add("private IRefactorer refactorer;");
+			sc.addLineBreak();
+			sc.add("public " + className + "(Resource resource, List<EObject> selectedElement){");
+			sc.add("super();");
+			sc.add("this.resource = resource;");
+			sc.add("this.selectedElement = selectedElement;");
+			sc.add("initialize();");
+			sc.add("}");
+			sc.addLineBreak();
+			sc.add("private void initialize() {");
+			sc.add("mappingMap = new LinkedHashMap<String, RoleMapping>();");
+			sc.add("refactorer = RefactorerFactory.eINSTANCE.getRefactorer(resource);");
+			sc.add("if(refactorer != null){");
+			sc.add("refactorer.setInput(selectedElement);");
+			sc.add("List<RoleMapping> mappings = refactorer.getPossibleRoleMappings(1.0);");
+			sc.add("for (RoleMapping roleMapping : mappings) {");
+			sc.add("mappingMap.put(roleMapping.getName(), roleMapping);");
+			sc.add("}");
+			sc.add("}");
+			sc.add("}");
+			sc.addLineBreak();
+			sc.addJavadoc("Returns the refactorer.");
+			sc.add("public IRefactorer getRefactorer() {");
+			sc.add("return refactorer;");
+			sc.add("}");
+			sc.addLineBreak();
+			generateMethods(sc, metamodelRoleMappings.get(epackage), monitor);
+			sc.add("}");
+			try {
+				fragment.createCompilationUnit(className + ".java", sc.toString(), true, monitor);
+			} catch (JavaModelException e) {
+				e.printStackTrace();
+			}
+			monitor.worked(1);
+		}
+	}
+
+	private void generateMethods(JavaComposite sc, List<RoleMapping> list, IProgressMonitor monitor) {
+		for (RoleMapping roleMapping : list) {
+			String mappingName = roleMapping.getName();
+			sc.addJavadoc("Returns the {@link RoleMapping} with name '" + mappingName + "'.", "@return");
+			sc.add("public RoleMapping get" + StringUtil.convertWordsToCamelCase(mappingName) + "Mapping(){");
+			sc.add("return mappingMap.get(\"" + mappingName + "\");");
+			sc.add("}");
+			sc.addLineBreak();
+			sc.addLineBreak();
+			sc.addJavadoc("Returns the registered icon for the {@link RoleMapping} with name '" + mappingName + "'."
+					, "@return the registered icon or null if none is registered");
+			sc.add("public URL get" + StringUtil.convertWordsToCamelCase(mappingName) + "Icon(){");
+			sc.add("return IRoleMappingRegistry.INSTANCE.getImagePathForMapping(get" + StringUtil.convertWordsToCamelCase(mappingName) + "Mapping());");
+			sc.add("}");
+			sc.addLineBreak();
+			monitor.worked(1);
+		}
+	}
+
+	private void addImports(JavaComposite sc) {
+		sc.add("import java.net.URL;");
+		sc.add("import java.util.LinkedHashMap;");
+		sc.add("import java.util.List;");
+		sc.add("import java.util.Map;");
+		sc.addLineBreak();
+		sc.add("import org.eclipse.emf.ecore.EObject;");
+		sc.add("import org.eclipse.emf.ecore.resource.Resource;");
+		sc.add("import org.emftext.language.refactoring.rolemapping.RoleMapping;");
+		sc.add("import org.emftext.refactoring.interpreter.IRefactorer;");
+		sc.add("import org.emftext.refactoring.interpreter.RefactorerFactory;");
+		sc.add("import org.emftext.refactoring.registry.rolemapping.IRoleMappingRegistry;");
+		sc.addLineBreak();
+	}
+
+	private void addPluginDependencies() {
+		try {
+			IProjectNature nature = project.getNature("org.eclipse.pde.PluginNature");
+			
+			System.out.println();
+		} catch (CoreException e) {
+			e.printStackTrace();
+		}
+	}
+
+	private IPackageFragment createFacadePackage(IProgressMonitor monitor) {
+		IJavaProject javaProject = JavaCore.create(project);
+		IFolder genFolder = project.getFolder("src-gen");
+		try {
+			if(!genFolder.exists()){
+				genFolder.create(true, true, monitor);
+			}
+			IPackageFragmentRoot srcFolder = javaProject.getPackageFragmentRoot(genFolder);
+			IClasspathEntry[] classPathEntries = javaProject.getRawClasspath();
+			IClasspathEntry classPath = JavaCore.newSourceEntry(genFolder.getFullPath());
+			boolean contained = false;
+			for (IClasspathEntry entry : classPathEntries) {
+				if(entry.equals(classPath)){
+					contained = true;
+					break;
+				}
+			}
+			if(!contained){
+				List<IClasspathEntry> list = new ArrayList<IClasspathEntry>();
+				list.addAll(Arrays.asList(classPathEntries));
+				list.add(classPath);
+				javaProject.setRawClasspath(list.toArray(new IClasspathEntry[0]), monitor);
+			}
+			return srcFolder.createPackageFragment(project.getName() + ".facade", true, monitor);
+		} catch (JavaModelException e) {
+			e.printStackTrace();
+		} catch (CoreException e) {
+			e.printStackTrace();
+		}
+		return null;
+	}
+
+}
