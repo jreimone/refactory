958f7b5424465e7f0ba9130ce2264e04ac2b8427
Jan <jreimone@users.noreply.github.com>
2010-12-21 15:21:14 +0000

    fixed role constraint checker for role implication

--- Refactoring/Plugins/org.emftext.refactoring.roleconstraintchecker/src/org/emftext/refactoring/roleconstraintchecker/impl/BasicRoleConstraintValidator.java
+++ Refactoring/Plugins/org.emftext.refactoring.roleconstraintchecker/src/org/emftext/refactoring/roleconstraintchecker/impl/BasicRoleConstraintValidator.java
@@ -25,19 +25,22 @@ import org.emftext.refactoring.roleconstraintchecker.IRoleConstraintValidator;
 
 /**
  * @author Jan Reimann
- *
+ * 
  */
 public class BasicRoleConstraintValidator implements IRoleConstraintValidator {
 
 	private static final String ROLE_CORRECT_MAPPING = "Roles are mapped correctly";
 	private static final String ROLE_PROHIBITION = "Metaclass %1$s plays roles %2$s and %3$s but they have to be XOR'ed";
-	private static final String ROLE_PROHIBITION_TRANSITIVE = "Metaclass %1$s plays role %2$s and metaclass %3$s plays role %4$s. " +
-	"But both roles mustn't be contained in the same inheritance tree.";
-	private static final String ROLE_IMPLICATION = "Metaclass %1$s has to play both role %2$s and %3$s.";
+	private static final String ROLE_PROHIBITION_TRANSITIVE = "Metaclass %1$s plays role %2$s and metaclass %3$s plays role %4$s. "
+			+ "But both roles mustn't be contained in the same inheritance tree.";
 	private static final String ROLE_IMPLICATION_TRANSITIVE = "Metaclass %1$s plays role %2$s. Role %3$s must be played from a superclass of %1$s.";
 
-	/* (non-Javadoc)
-	 * @see org.emftext.refactoring.roleconstraintchecker.IRoleConstraintValidator#validate()
+	/*
+	 * (non-Javadoc)
+	 * 
+	 * @see
+	 * org.emftext.refactoring.roleconstraintchecker.IRoleConstraintValidator
+	 * #validate()
 	 */
 	public IStatus validate(RoleMappingModel mappingModel) {
 		EList<RoleMapping> mappings = mappingModel.getMappings();
@@ -46,24 +49,24 @@ public class BasicRoleConstraintValidator implements IRoleConstraintValidator {
 		for (RoleMapping mapping : mappings) {
 			List<IStatus> status = validateMapping(mapping);
 			for (IStatus iStatus : status) {
-				if(iStatus != null){
-					if(iStatus.getSeverity() > severity){
+				if (iStatus != null) {
+					if (iStatus.getSeverity() > severity) {
 						severity = iStatus.getSeverity();
 					}
 					stati.add(iStatus);
 				}
 			}
 		}
-		IStatus status = new MultiStatus(Activator.PLUGIN_ID
-				, severity
-				, stati.toArray(new IStatus[0])
-				, "Role Constraint validation result"
-				, null);
+		IStatus status = new MultiStatus(Activator.PLUGIN_ID, severity, stati.toArray(new IStatus[0]), "Role Constraint validation result", null);
 		return status;
 	}
 
-	/* (non-Javadoc)
-	 * @see org.emftext.refactoring.roleconstraintchecker.IRoleConstraintValidator#validateMapping(org.emftext.language.refactoring.rolemapping.Mapping)
+	/*
+	 * (non-Javadoc)
+	 * 
+	 * @see
+	 * org.emftext.refactoring.roleconstraintchecker.IRoleConstraintValidator
+	 * #validateMapping(org.emftext.language.refactoring.rolemapping.Mapping)
 	 */
 	public List<IStatus> validateMapping(RoleMapping mapping) {
 		List<IStatus> stati = new ArrayList<IStatus>();
@@ -74,15 +77,15 @@ public class BasicRoleConstraintValidator implements IRoleConstraintValidator {
 			EList<Collaboration> outgoings = role.getOutgoing();
 			for (Collaboration collaboration : outgoings) {
 				IStatus status = null;
-				if(collaboration instanceof RoleProhibition){
+				if (collaboration instanceof RoleProhibition) {
 					status = validateProhibition((RoleProhibition) collaboration, concreteMapping.getMetaclass(), mappings);
 
 				}
-				if(collaboration instanceof RoleImplication){
+				if (collaboration instanceof RoleImplication) {
 					status = validateImplication((RoleImplication) collaboration, concreteMapping.getMetaclass(), mappings);
 
 				}
-				if(status != null){
+				if (status != null) {
 					stati.add(status);
 				}
 			}
@@ -90,71 +93,49 @@ public class BasicRoleConstraintValidator implements IRoleConstraintValidator {
 		return stati;
 	}
 
-	private IStatus validateProhibition(RoleProhibition prohibition, EClass mappedEClass, EList<ConcreteMapping> mappings){
+	private IStatus validateProhibition(RoleProhibition prohibition, EClass mappedEClass, EList<ConcreteMapping> mappings) {
 		IStatus status = null;
 		Role sourceRole = prohibition.getSource();
 		Role targetRole = prohibition.getTarget();
 		EClass targetEClass = getMappedEClassByRole(mappings, targetRole);
-		if(!prohibition.getModifier().contains(CollaborationModifier.TRANSITIVE)){
-			if(mappedEClass.equals(targetEClass)){
-				status = new Status(IStatus.ERROR
-						, Activator.PLUGIN_ID
-						, String.format(ROLE_PROHIBITION, mappedEClass.getName(), sourceRole.getName(), targetRole.getName()));
+		if (!prohibition.getModifier().contains(CollaborationModifier.TRANSITIVE)) {
+			if (mappedEClass.equals(targetEClass)) {
+				status = new Status(IStatus.ERROR, Activator.PLUGIN_ID, String.format(ROLE_PROHIBITION, mappedEClass.getName(), sourceRole.getName(), targetRole.getName()));
 			} else {
-				status = new Status(IStatus.OK
-						, Activator.PLUGIN_ID
-						, ROLE_CORRECT_MAPPING);
+				status = new Status(IStatus.OK, Activator.PLUGIN_ID, ROLE_CORRECT_MAPPING);
 			}
 		} else {
 			EList<EClass> superTypesSource = mappedEClass.getEAllSuperTypes();
 			EList<EClass> superTypesTarget = targetEClass.getEAllSuperTypes();
-			if(superTypesSource.contains(targetEClass) || superTypesTarget.contains(mappedEClass)){
-				status = new Status(IStatus.ERROR
-						, Activator.PLUGIN_ID
-						, String.format(ROLE_PROHIBITION_TRANSITIVE, mappedEClass.getName(), sourceRole.getName(), targetEClass.getName(), targetRole.getName()));
+			if (superTypesSource.contains(targetEClass)
+					|| superTypesTarget.contains(mappedEClass)) {
+				status = new Status(IStatus.ERROR, Activator.PLUGIN_ID, String.format(ROLE_PROHIBITION_TRANSITIVE, mappedEClass.getName(), sourceRole.getName(), targetEClass.getName(), targetRole.getName()));
 			} else {
-				status = new Status(IStatus.OK
-						, Activator.PLUGIN_ID
-						, ROLE_CORRECT_MAPPING);
+				status = new Status(IStatus.OK, Activator.PLUGIN_ID, ROLE_CORRECT_MAPPING);
 			}
 		}
 		return status;
 	}
 
-	private IStatus validateImplication(RoleImplication implication, EClass mappedEClass, EList<ConcreteMapping> mappings){
+	private IStatus validateImplication(RoleImplication implication, EClass mappedEClass, EList<ConcreteMapping> mappings) {
 		IStatus status = null;
 		Role sourceRole = implication.getSource();
 		Role targetRole = implication.getTarget();
 		EClass targetEClass = getMappedEClassByRole(mappings, targetRole);
-		if(!implication.getModifier().contains(CollaborationModifier.TRANSITIVE)){
-			if(!mappedEClass.equals(targetEClass)){
-				status = new Status(IStatus.ERROR
-						, Activator.PLUGIN_ID
-						, String.format(ROLE_IMPLICATION, mappedEClass.getName(), sourceRole.getName(), targetRole.getName()));
-			} else {
-				status = new Status(IStatus.OK
-						, Activator.PLUGIN_ID
-						, ROLE_CORRECT_MAPPING);
-			}
+		EList<EClass> superTypesSource = mappedEClass.getEAllSuperTypes();
+		EList<EClass> superTypesTarget = targetEClass.getEAllSuperTypes();
+		if (!superTypesSource.contains(targetEClass)
+				&& !superTypesTarget.contains(mappedEClass)) {
+			status = new Status(IStatus.ERROR, Activator.PLUGIN_ID, String.format(ROLE_IMPLICATION_TRANSITIVE, mappedEClass.getName(), sourceRole.getName(), targetRole.getName()));
 		} else {
-			EList<EClass> superTypesSource = mappedEClass.getEAllSuperTypes();
-			EList<EClass> superTypesTarget = targetEClass.getEAllSuperTypes();
-			if(!superTypesSource.contains(targetEClass) && !superTypesTarget.contains(mappedEClass)){
-				status = new Status(IStatus.ERROR
-						, Activator.PLUGIN_ID
-						, String.format(ROLE_IMPLICATION_TRANSITIVE, mappedEClass.getName(), sourceRole.getName(), targetRole.getName()));
-			} else {
-				status = new Status(IStatus.OK
-						, Activator.PLUGIN_ID
-						, ROLE_CORRECT_MAPPING);
-			}
+			status = new Status(IStatus.OK, Activator.PLUGIN_ID, ROLE_CORRECT_MAPPING);
 		}
 		return status;
 	}
 
-	private static EClass getMappedEClassByRole(EList<ConcreteMapping> mappings, Role role){
+	private static EClass getMappedEClassByRole(EList<ConcreteMapping> mappings, Role role) {
 		for (ConcreteMapping concreteMapping : mappings) {
-			if(concreteMapping.getRole().equals(role)){
+			if (concreteMapping.getRole().equals(role)) {
 				return concreteMapping.getMetaclass();
 			}
 		}
