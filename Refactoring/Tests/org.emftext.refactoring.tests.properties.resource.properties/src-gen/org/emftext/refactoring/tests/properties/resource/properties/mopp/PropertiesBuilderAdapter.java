d9f6adf3c60ad85136f378c89a9d76a17f82584b
Mirko Seifert <mirkoseifert@users.noreply.github.com>
2010-12-30 07:46:15 +0000

    regenerated all languages

--- Refactoring/Tests/org.emftext.refactoring.tests.properties.resource.properties/src-gen/org/emftext/refactoring/tests/properties/resource/properties/mopp/PropertiesBuilderAdapter.java
+++ Refactoring/Tests/org.emftext.refactoring.tests.properties.resource.properties/src-gen/org/emftext/refactoring/tests/properties/resource/properties/mopp/PropertiesBuilderAdapter.java
@@ -15,7 +15,7 @@ public class PropertiesBuilderAdapter extends org.eclipse.core.resources.Increme
 	
 	private org.emftext.refactoring.tests.properties.resource.properties.IPropertiesBuilder builder = new org.emftext.refactoring.tests.properties.resource.properties.mopp.PropertiesBuilder();
 	
-	@SuppressWarnings("unchecked")	
+	@SuppressWarnings("rawtypes")
 	public org.eclipse.core.resources.IProject[] build(int kind, java.util.Map args, final org.eclipse.core.runtime.IProgressMonitor monitor) throws org.eclipse.core.runtime.CoreException {
 		return build(kind, args, monitor, builder, getProject());
 	}
