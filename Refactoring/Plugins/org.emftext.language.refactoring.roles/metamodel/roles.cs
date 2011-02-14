926eb84193068fb551198359389edf49a025b135
Mirko Seifert <mirkoseifert@users.noreply.github.com>
2011-02-14 10:48:36 +0000

    change path to licence header to be relative

--- Refactoring/Plugins/org.emftext.language.refactoring.roles/metamodel/roles.cs
+++ Refactoring/Plugins/org.emftext.language.refactoring.roles/metamodel/roles.cs
@@ -17,7 +17,7 @@ FOR <http://www.emftext.org/language/roles>
 START RoleModel
 
 OPTIONS {
-	licenceHeader ="platform:/resource/org.reuseware/licence.txt";
+	licenceHeader ="../../org.dropsbox/licence.txt";
 	usePredefinedTokens = "false";
 	reloadGeneratorModel = "true";
 	generateCodeFromGeneratorModel = "true";
