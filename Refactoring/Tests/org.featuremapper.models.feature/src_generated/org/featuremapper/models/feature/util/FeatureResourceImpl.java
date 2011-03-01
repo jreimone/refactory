24075a07a93cc189b9ef52ba255b15c8ad60ff51
Jan <jreimone@users.noreply.github.com>
2011-03-01 07:59:06 +0000

    added feature mapper metamodel because it isn't built from Hudson since it is contained in another repository

---
+++ Refactoring/Tests/org.featuremapper.models.feature/src_generated/org/featuremapper/models/feature/util/FeatureResourceImpl.java
@@ -0,0 +1,31 @@
+/**
+ * <copyright>
+ * </copyright>
+ *
+ * $Id$
+ */
+package org.featuremapper.models.feature.util;
+
+import org.eclipse.emf.common.util.URI;
+import org.eclipse.emf.ecore.xmi.impl.XMIResourceImpl;
+
+/**
+ * <!-- begin-user-doc -->
+ * The <b>Resource </b> associated with the package.
+ * <!-- end-user-doc -->
+ * @see org.featuremapper.models.feature.util.FeatureResourceFactoryImpl
+ * @generated
+ */
+public class FeatureResourceImpl extends XMIResourceImpl {
+	/**
+	 * Creates an instance of the resource.
+	 * <!-- begin-user-doc -->
+	 * <!-- end-user-doc -->
+	 * @param uri the URI of the new resource.
+	 * @generated
+	 */
+	public FeatureResourceImpl(URI uri) {
+		super(uri);
+	}
+
+} //FeatureResourceImpl
