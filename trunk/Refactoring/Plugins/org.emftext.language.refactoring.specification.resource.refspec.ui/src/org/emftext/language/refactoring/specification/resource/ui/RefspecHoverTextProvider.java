881db919e49169c264d8235927cf8b796289efec
mseifert <mseifert@2290cd21-d4b4-4435-9c65-06e28a4c4f12>
2011-03-11 09:43:10 +0000

    fixed compile error due to new method in IHoverTextProvider

--- trunk/Refactoring/Plugins/org.emftext.language.refactoring.specification.resource.refspec.ui/src/org/emftext/language/refactoring/specification/resource/ui/RefspecHoverTextProvider.java
+++ trunk/Refactoring/Plugins/org.emftext.language.refactoring.specification.resource.refspec.ui/src/org/emftext/language/refactoring/specification/resource/ui/RefspecHoverTextProvider.java
@@ -6,15 +6,21 @@
  */
 package org.emftext.language.refactoring.specification.resource.ui;
 
+import org.eclipse.emf.ecore.EObject;
 import org.emftext.language.refactoring.roles.RolesPackage;
 import org.emftext.language.refactoring.roles.resource.rolestext.ui.RolestextHoverTextProvider;
+import org.emftext.language.refactoring.specification.resource.IRefspecHoverTextProvider;
 
-public class RefspecHoverTextProvider implements org.emftext.language.refactoring.specification.resource.IRefspecHoverTextProvider {
+public class RefspecHoverTextProvider implements IRefspecHoverTextProvider {
 	
-private org.emftext.language.refactoring.specification.resource.ui.RefspecDefaultHoverTextProvider defaultProvider = new org.emftext.language.refactoring.specification.resource.ui.RefspecDefaultHoverTextProvider();
+	private RefspecDefaultHoverTextProvider defaultProvider = new RefspecDefaultHoverTextProvider();
 	
-	public java.lang.String getHoverText(org.eclipse.emf.ecore.EObject object) {
-		if(object.eClass().getEPackage().equals(RolesPackage.eINSTANCE)){
+	public String getHoverText(EObject container, EObject referencedObject) {
+		return getHoverText(referencedObject);
+	}
+	
+	public String getHoverText(EObject object) {
+		if (object.eClass().getEPackage().equals(RolesPackage.eINSTANCE)) {
 			RolestextHoverTextProvider rolesHoverProvider = new RolestextHoverTextProvider();
 			return rolesHoverProvider.getHoverText(object);
 		}
