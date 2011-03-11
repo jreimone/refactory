f5ab232f73d9c51cdd293d101b2ba58cbcf76767
Mirko Seifert <mirkoseifert@users.noreply.github.com>
2011-03-11 09:27:35 +0000

    fixed compile error due to new method in IHoverTextProvider

--- Refactoring/Plugins/org.emftext.language.refactoring.rolemapping.resource.rolemapping.ui/src/org/emftext/language/refactoring/rolemapping/resource/rolemapping/ui/RolemappingHoverTextProvider.java
+++ Refactoring/Plugins/org.emftext.language.refactoring.rolemapping.resource.rolemapping.ui/src/org/emftext/language/refactoring/rolemapping/resource/rolemapping/ui/RolemappingHoverTextProvider.java
@@ -6,15 +6,21 @@
  */
 package org.emftext.language.refactoring.rolemapping.resource.rolemapping.ui;
 
+import org.eclipse.emf.ecore.EObject;
+import org.emftext.language.refactoring.rolemapping.resource.rolemapping.IRolemappingHoverTextProvider;
 import org.emftext.language.refactoring.roles.RolesPackage;
 import org.emftext.language.refactoring.roles.resource.rolestext.ui.RolestextHoverTextProvider;
 
-public class RolemappingHoverTextProvider implements org.emftext.language.refactoring.rolemapping.resource.rolemapping.IRolemappingHoverTextProvider {
+public class RolemappingHoverTextProvider implements IRolemappingHoverTextProvider {
 	
-private org.emftext.language.refactoring.rolemapping.resource.rolemapping.ui.RolemappingDefaultHoverTextProvider defaultProvider = new org.emftext.language.refactoring.rolemapping.resource.rolemapping.ui.RolemappingDefaultHoverTextProvider();
+	private RolemappingDefaultHoverTextProvider defaultProvider = new RolemappingDefaultHoverTextProvider();
+
+	public String getHoverText(EObject container, EObject referencedObject) {
+		return getHoverText(referencedObject);
+	}
 	
-	public java.lang.String getHoverText(org.eclipse.emf.ecore.EObject object) {
-		if(object.eClass().getEPackage().equals(RolesPackage.eINSTANCE)){
+	public String getHoverText(EObject object) {
+		if (object.eClass().getEPackage().equals(RolesPackage.eINSTANCE)) {
 			RolestextHoverTextProvider rolesHoverProvider = new RolestextHoverTextProvider();
 			return rolesHoverProvider.getHoverText(object);
 		}
