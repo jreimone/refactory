f909149f085b921bf32eedbc13442bdce0c38dbe
Jan <jreimone@users.noreply.github.com>
2011-03-21 16:40:15 +0000

    added 'sub menu' option to extension point

--- Refactoring/Plugins/org.emftext.refactoring.registry.rolemapping/src/org/emftext/refactoring/registry/rolemapping/IRoleMappingExtensionPoint.java
+++ Refactoring/Plugins/org.emftext.refactoring.registry.rolemapping/src/org/emftext/refactoring/registry/rolemapping/IRoleMappingExtensionPoint.java
@@ -5,6 +5,7 @@ public interface IRoleMappingExtensionPoint {
 	public static final String ID 						= "org.emftext.refactoring.rolemapping";
 	public static final String RESOURCE_ATTRIBUTE 		= "rolemapping_resource";
 	public static final String DEFAULT_ICON_ATTRIBUTE	= "defaultIcon";
+	public static final String SUB_MENU_PATH			= "subMenuPath";
 	
 	public static final String SUB_MAPPING_ICON_ID		= "mappingIcon";
 	public static final String SUB_MAPPING_NAME 		= "mapping_name";
