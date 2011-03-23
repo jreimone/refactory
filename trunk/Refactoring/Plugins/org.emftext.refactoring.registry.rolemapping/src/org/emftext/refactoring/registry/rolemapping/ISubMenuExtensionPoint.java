42f1be416691389dd4dc9d1c3ddf5117b5beda5c
jreimann <jreimann@2290cd21-d4b4-4435-9c65-06e28a4c4f12>
2011-03-23 13:11:54 +0000

    renamed attribute in submenu extension point and adapted interface

--- trunk/Refactoring/Plugins/org.emftext.refactoring.registry.rolemapping/src/org/emftext/refactoring/registry/rolemapping/ISubMenuExtensionPoint.java
+++ trunk/Refactoring/Plugins/org.emftext.refactoring.registry.rolemapping/src/org/emftext/refactoring/registry/rolemapping/ISubMenuExtensionPoint.java
@@ -5,5 +5,5 @@ public interface ISubMenuExtensionPoint {
 	public static final String ID 						= "org.emftext.refactoring.submenu";
 	public static final String SUBMENU_ID_ATTRIBUTE		= "submenuID";
 	public static final String DISPLAY_STRING_ATTRIBUTE	= "displayString";
-	public static final String GENERALISES_ATTRIBUTE	= "generalises";
+	public static final String GENERALISES_ATTRIBUTE	= "specialises";
 }
