7c22dff7380e95806e1f47f4962befa848d1abcc
mmuck <mmuck@2290cd21-d4b4-4435-9c65-06e28a4c4f12>
2011-04-13 21:07:18 +0000

    Added refactorings to split and merge constraints

---
+++ trunk/Refactoring/Registrations/Mappings/tudresden.ocl20.pivot.language.ocl.refactoring/src/oclrefactoring/MergeInvariantsPP.java
@@ -0,0 +1,149 @@
+/**
+ * 
+ */
+package oclrefactoring;
+
+import java.util.Iterator;
+import java.util.List;
+import java.util.Map;
+import java.util.Set;
+
+import org.eclipse.core.runtime.IStatus;
+import org.eclipse.core.runtime.Status;
+import org.eclipse.emf.common.util.BasicEList;
+import org.eclipse.emf.common.util.EList;
+import org.eclipse.emf.ecore.EObject;
+import org.eclipse.emf.ecore.change.ChangeDescription;
+import org.eclipse.emf.ecore.resource.ResourceSet;
+import org.eclipse.emf.ecore.util.EcoreUtil;
+import org.emftext.language.refactoring.refactoring_specification.RefactoringSpecification;
+import org.emftext.language.refactoring.roles.Role;
+import org.emftext.refactoring.registry.rolemapping.IRefactoringPostProcessor;
+
+import tudresden.ocl20.pivot.datatypes.impl.DatatypesFactoryImpl;
+import tudresden.ocl20.pivot.language.ocl.ClassifierContextDeclarationCS;
+import tudresden.ocl20.pivot.language.ocl.ContextDeclarationCS;
+import tudresden.ocl20.pivot.language.ocl.DefinitionExpCS;
+import tudresden.ocl20.pivot.language.ocl.InvariantExpCS;
+import tudresden.ocl20.pivot.language.ocl.InvariantOrDefinitionCS;
+import tudresden.ocl20.pivot.language.ocl.LogicalAndOperationCallExpCS;
+import tudresden.ocl20.pivot.language.ocl.PackageDeclarationCS;
+import tudresden.ocl20.pivot.language.ocl.impl.OclFactoryImpl;
+import tudresden.ocl20.pivot.pivotmodel.impl.PivotModelFactoryImpl;
+
+/**
+ * @author Michael Muck
+ *
+ */
+public class MergeInvariantsPP implements IRefactoringPostProcessor {
+
+	
+	OclFactoryImpl myOclFactory;
+	DatatypesFactoryImpl myDataTypesFactory;
+	PivotModelFactoryImpl myPivotModelFactory;
+	ClassifierContextDeclarationCS selection;
+	PackageDeclarationCS myPackage;
+	PostProcessingHelper myPPH;
+	
+
+	public MergeInvariantsPP() {
+
+		myOclFactory = new OclFactoryImpl();
+		myDataTypesFactory = new DatatypesFactoryImpl();
+		myPivotModelFactory = new PivotModelFactoryImpl();
+		myPPH = new PostProcessingHelper();
+	}
+	
+	
+	@Override
+	public IStatus process(Map<Role, List<EObject>> roleRuntimeInstanceMap,
+			ResourceSet resourceSet, ChangeDescription change,
+			RefactoringSpecification refSpec) {
+
+		System.err.println("Merge Invariants postprocessor activated!");
+		Set<Role> keySet = roleRuntimeInstanceMap.keySet();
+		for (Role role : keySet) {
+			List<EObject> roleplayers = roleRuntimeInstanceMap.get(role);
+			if (role.getName().equals("Selection")) {
+//				System.out.println(roleplayers.size()+" roleplayers found for role "+role.getName()+":");
+//				Iterator<EObject> roleplayersIt = roleplayers.iterator();
+//				while (roleplayersIt.hasNext()) {
+//					System.out.println("  "+roleplayersIt.next());
+//				}				
+				if (roleplayers.size()==1 && roleplayers.get(0) instanceof ClassifierContextDeclarationCS) {
+					selection = (ClassifierContextDeclarationCS) roleplayers.get(0);
+					System.out.println("found selected element as: "+selection);
+				}
+			}
+		}
+		if (selection == null) {
+			System.err.println("No ClassifierContextDeclarations has been selected. Refactoring aborted!");
+			return Status.CANCEL_STATUS;
+		}
+		IStatus refactoringStatus = performSpecificTransformation();
+		
+		
+		return refactoringStatus;
+	}
+	
+	
+	private IStatus performSpecificTransformation() {
+		
+		//if the input element does not contain invariants or definitions there is nothing to merge
+		if (selection.getInvariantsAndDefinitions().isEmpty()) {
+			System.err.println("The selected input element does not contain any invariants or definitions. Refactoring aborted.");
+			return Status.CANCEL_STATUS;
+		}
+		//collecting all invariants from selection
+		EList<InvariantOrDefinitionCS> invariantOrDefinitionList = selection.getInvariantsAndDefinitions();
+		EList<InvariantExpCS> invariantList = new BasicEList<InvariantExpCS>();
+		Iterator<InvariantOrDefinitionCS> invariantOrDefinitionListIterator = invariantOrDefinitionList.iterator();
+		while (invariantOrDefinitionListIterator.hasNext()) {
+			InvariantOrDefinitionCS akt = invariantOrDefinitionListIterator.next();
+			if (akt instanceof InvariantExpCS) invariantList.add((InvariantExpCS) akt);
+		}
+		
+		//since only invariants can be merged, if the selected element contains definitions, the refactoring wont work
+		if (invariantList.size() != invariantOrDefinitionList.size()) {
+			System.err.println("  The selections contains DefinitionExpressions, but only InvariantExpressions can be merged!");
+			return Status.CANCEL_STATUS;
+		}
+		
+		//at least 2 invariants have to be present, or there is nothing to be matched
+		if(invariantList.size()<2) {
+			System.err.println("There are less than 2 invariants present in the selection. No merging is possible!");
+			return Status.CANCEL_STATUS;
+		}
+		
+		//now merging the Invariants
+		InvariantExpCS result = invariantList.get(0);
+		for(int i=1;i<invariantList.size();i++) {
+			InvariantExpCS akt = invariantList.get(i);
+			LogicalAndOperationCallExpCS myAnd = myOclFactory.createLogicalAndOperationCallExpCS();
+			myAnd.setOperationName("and");
+			myAnd.setSource(result.getOclExpression());
+			myAnd.setTarget(akt.getOclExpression());
+			result.setOclExpression(myAnd);
+		}
+		for(int j=1;j<selection.getInvariantsAndDefinitions().size();j++) {
+			selection.getInvariantsAndDefinitions().remove(j);
+		}
+		
+		
+		
+		return Status.OK_STATUS;
+	}
+	
+
+	
+	
+	
+
+
+	@Override
+	public IStatus process(Map<Role, List<EObject>> roleRuntimeInstanceMap,
+			ResourceSet resourceSet, ChangeDescription change) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+}
---
+++ trunk/Refactoring/Registrations/Mappings/tudresden.ocl20.pivot.language.ocl.refactoring/src/oclrefactoring/SplitConstraintPP.java
@@ -0,0 +1,164 @@
+/**
+ * 
+ */
+package oclrefactoring;
+
+import java.util.Iterator;
+import java.util.List;
+import java.util.Map;
+import java.util.Set;
+
+import org.eclipse.core.runtime.IStatus;
+import org.eclipse.core.runtime.Status;
+import org.eclipse.emf.common.util.BasicEList;
+import org.eclipse.emf.common.util.EList;
+import org.eclipse.emf.ecore.EObject;
+import org.eclipse.emf.ecore.change.ChangeDescription;
+import org.eclipse.emf.ecore.resource.ResourceSet;
+import org.emftext.language.refactoring.refactoring_specification.RefactoringSpecification;
+import org.emftext.language.refactoring.roles.Role;
+import org.emftext.refactoring.registry.rolemapping.IRefactoringPostProcessor;
+
+import tudresden.ocl20.pivot.datatypes.impl.DatatypesFactoryImpl;
+import tudresden.ocl20.pivot.language.ocl.ClassifierContextDeclarationCS;
+import tudresden.ocl20.pivot.language.ocl.ContextDeclarationCS;
+import tudresden.ocl20.pivot.language.ocl.DefinitionExpCS;
+import tudresden.ocl20.pivot.language.ocl.InvariantExpCS;
+import tudresden.ocl20.pivot.language.ocl.InvariantOrDefinitionCS;
+import tudresden.ocl20.pivot.language.ocl.LogicalAndOperationCallExpCS;
+import tudresden.ocl20.pivot.language.ocl.PackageDeclarationCS;
+import tudresden.ocl20.pivot.language.ocl.impl.OclFactoryImpl;
+import tudresden.ocl20.pivot.pivotmodel.impl.PivotModelFactoryImpl;
+
+/**
+ * @author Michael Muck
+ *
+ */
+public class SplitConstraintPP implements IRefactoringPostProcessor {
+
+	
+	OclFactoryImpl myOclFactory;
+	DatatypesFactoryImpl myDataTypesFactory;
+	PivotModelFactoryImpl myPivotModelFactory;
+	private ClassifierContextDeclarationCS selected;
+	private int splitCount;
+	private boolean splitAtomic;
+	PackageDeclarationCS myPackage;
+	
+
+	public SplitConstraintPP() {
+
+		myOclFactory = new OclFactoryImpl();
+		myDataTypesFactory = new DatatypesFactoryImpl();
+		myPivotModelFactory = new PivotModelFactoryImpl();
+		splitCount = 0;
+		splitAtomic = false;
+	}
+	
+	
+	@Override
+	public IStatus process(Map<Role, List<EObject>> roleRuntimeInstanceMap,
+			ResourceSet resourceSet, ChangeDescription change,
+			RefactoringSpecification refSpec) {
+
+		System.err.println("Split Constraint postprocessor activated!");
+		Set<Role> keySet = roleRuntimeInstanceMap.keySet();
+		for (Role role : keySet) {
+			List<EObject> roleplayers = roleRuntimeInstanceMap.get(role);
+			if (role.getName().equals("Selection")) {
+				
+				if (roleplayers.size()==1 && roleplayers.get(0) instanceof ClassifierContextDeclarationCS) {
+					selected = (ClassifierContextDeclarationCS) roleplayers.get(0);
+				}
+			}
+		}
+		if (selected == null) {
+			System.err.println("No ContextDeclarationCS selected. Refactoring aborted!");
+			return Status.CANCEL_STATUS;
+		}
+		IStatus refactoringStatus = performSpecificTransformation();
+		
+		if (splitCount == 0) {
+			System.out.println("Constraint could not be split.");
+		} else {
+			System.out.println("Constraint has been split "+splitCount+" times.");
+		}
+		
+		return refactoringStatus;
+	}
+	
+	
+	private IStatus performSpecificTransformation() {
+		
+		if (selected.eContainer()instanceof PackageDeclarationCS) {
+			myPackage = (PackageDeclarationCS) selected.eContainer();
+			System.out.println("found package declaration as: "+myPackage);
+		} else {
+			System.err.println("package declaration not found - refactoring cancelled!");
+			return Status.CANCEL_STATUS;
+		}
+		
+		EList<InvariantOrDefinitionCS> invList = selected.getInvariantsAndDefinitions();
+		Iterator<InvariantOrDefinitionCS> invListIt = invList.iterator();
+		EList<InvariantExpCS> myNewInvList = new BasicEList<InvariantExpCS>();
+		while(invListIt.hasNext()) {
+			InvariantOrDefinitionCS invListElement = invListIt.next();
+			InvariantExpCS myInv = null;
+			LogicalAndOperationCallExpCS myAnd = null;
+			if (invListElement instanceof InvariantExpCS) {
+				myInv = (InvariantExpCS) invListElement;
+				if (myInv.getOclExpression() instanceof LogicalAndOperationCallExpCS) {
+					myAnd = (LogicalAndOperationCallExpCS) myInv.getOclExpression();
+					myInv.setOclExpression(myAnd.getSource());
+					InvariantExpCS myNewInv = myOclFactory.createInvariantExpCS();
+					myNewInv.setOclExpression(myAnd.getTarget());
+					myNewInvList.add(myNewInv);
+					splitCount++;
+				}
+			}
+		}
+		Iterator<InvariantExpCS> myNewInvListIterator = myNewInvList.iterator();
+		if (splitAtomic) {
+			while (myNewInvListIterator.hasNext()) {
+				InvariantExpCS aktexp = myNewInvListIterator.next();
+				ClassifierContextDeclarationCS myClassifierContextDeclarationCS = myOclFactory.createClassifierContextDeclarationCS();
+				myClassifierContextDeclarationCS.getInvariantsAndDefinitions().add(aktexp);
+				myClassifierContextDeclarationCS.setTypeName(selected.getTypeName());
+				myPackage.getContextDeclarations().add(myClassifierContextDeclarationCS);
+			}
+		} else {
+			while (myNewInvListIterator.hasNext()) {
+				InvariantExpCS aktexp = myNewInvListIterator.next();
+				selected.getInvariantsAndDefinitions().add(aktexp);
+			}
+		}
+		
+		
+		
+		
+		return Status.OK_STATUS;
+	}
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+
+
+	@Override
+	public IStatus process(Map<Role, List<EObject>> roleRuntimeInstanceMap,
+			ResourceSet resourceSet, ChangeDescription change) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+}
