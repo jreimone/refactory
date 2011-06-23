7159b6d775638dfdb1bf466ace116462ef631675
jreimann <jreimann@2290cd21-d4b4-4435-9c65-06e28a4c4f12>
2011-06-23 11:26:47 +0000

    adapted XtextEditorConnector to Xtext 2.0

--- trunk/Refactoring/Registrations/EditorConnectors/org.emftext.refactoring.editorconnector.xtext/src/org/eclipse/xtext/refactoring/editorconnector/XtextEditorConnector.java
+++ trunk/Refactoring/Registrations/EditorConnectors/org.emftext.refactoring.editorconnector.xtext/src/org/eclipse/xtext/refactoring/editorconnector/XtextEditorConnector.java
@@ -6,24 +6,27 @@ import java.util.List;
 import java.util.Set;
 
 import org.eclipse.emf.ecore.EObject;
-import org.eclipse.emf.ecore.EReference;
 import org.eclipse.emf.ecore.resource.Resource;
 import org.eclipse.emf.transaction.TransactionalEditingDomain;
 import org.eclipse.jface.text.ITextSelection;
 import org.eclipse.jface.viewers.ISelection;
 import org.eclipse.ui.IEditorPart;
 import org.eclipse.xtext.resource.EObjectAtOffsetHelper;
+import org.eclipse.xtext.resource.ILocationInFileProvider;
+import org.eclipse.xtext.resource.IResourceServiceProvider;
 import org.eclipse.xtext.resource.XtextResource;
 import org.eclipse.xtext.ui.editor.XtextEditor;
 import org.eclipse.xtext.ui.editor.model.IXtextDocument;
 import org.eclipse.xtext.ui.editor.model.XtextDocumentUtil;
-import org.eclipse.xtext.util.TextLocation;
+import org.eclipse.xtext.util.ITextRegion;
 import org.eclipse.xtext.util.concurrent.IUnitOfWork;
 import org.emftext.refactoring.editorconnector.IEditorConnector;
 
 public class XtextEditorConnector implements IEditorConnector {
 
 	private XtextEditor editor;
+	private EObjectAtOffsetHelper objectAtOffsetHelper;
+	private ILocationInFileProvider locationInFileProvider;
 
 	public XtextEditorConnector() {
 		//
@@ -32,7 +35,13 @@ public class XtextEditorConnector implements IEditorConnector {
 	public boolean canHandle(IEditorPart editor) {
 		if(editor instanceof XtextEditor){
 			this.editor = (XtextEditor) editor;
-			return true;
+			XtextResource resource = getXtextResource(this.editor);
+			IResourceServiceProvider resourceServiceProvider = resource.getResourceServiceProvider();
+			objectAtOffsetHelper = resourceServiceProvider.get(EObjectAtOffsetHelper.class);
+			locationInFileProvider = resourceServiceProvider.get(ILocationInFileProvider.class);
+			if(objectAtOffsetHelper != null && locationInFileProvider != null){
+				return true;
+			}
 		}
 		return false;
 	}
@@ -47,7 +56,7 @@ public class XtextEditorConnector implements IEditorConnector {
 				Set<EObject> elementSet = new LinkedHashSet<EObject>();
 				int offset = textSelection.getOffset();
 				for (int i = textSelection.getLength(); i > 0; i--) {
-					EObject element = EObjectAtOffsetHelper.resolveContainedElementAt(xtextResource, offset, null);
+					EObject element = objectAtOffsetHelper.resolveElementAt(xtextResource, offset);
 					elementSet.add(element);
 					offset++;
 				}
@@ -84,15 +93,14 @@ public class XtextEditorConnector implements IEditorConnector {
 		int offset = Integer.MAX_VALUE;
 		int length = 0;
 		for (EObject object : objectsToSelect) {
-			EReference containmentFeature = object.eContainmentFeature();
-			TextLocation location = EObjectAtOffsetHelper.getLocation(object, containmentFeature, 0);
-			if (location.getOffset() < offset){
+			ITextRegion textRegion = locationInFileProvider.getFullTextRegion(object);
+			if (textRegion.getOffset() < offset){
 				if(length != 0){
-					length += offset - location.getOffset();
+					length += offset - textRegion.getOffset();
 				} else {
-					length = location.getLength();
+					length = textRegion.getLength();
 				}
-				offset = location.getOffset();
+				offset = textRegion.getOffset();
 			}
 		}
 		if(length != 0){
