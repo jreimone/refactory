ccc2d5574a44f13231f0b3b037a86ee2701c3902
Jan <jreimone@users.noreply.github.com>
2011-06-27 09:00:12 +0000

    added IUnitOfWork

--- Refactoring/Registrations/EditorConnectors/org.emftext.refactoring.editorconnector.xtext/src/org/eclipse/xtext/refactoring/editorconnector/XtextEditorConnector.java
+++ Refactoring/Registrations/EditorConnectors/org.emftext.refactoring.editorconnector.xtext/src/org/eclipse/xtext/refactoring/editorconnector/XtextEditorConnector.java
@@ -5,12 +5,20 @@ import java.util.LinkedList;
 import java.util.List;
 import java.util.Set;
 
+import org.eclipse.core.resources.IFile;
+import org.eclipse.core.resources.ResourcesPlugin;
+import org.eclipse.core.runtime.IPath;
+import org.eclipse.core.runtime.Path;
+import org.eclipse.emf.common.ui.URIEditorInput;
+import org.eclipse.emf.common.util.URI;
 import org.eclipse.emf.ecore.EObject;
 import org.eclipse.emf.ecore.resource.Resource;
 import org.eclipse.emf.transaction.TransactionalEditingDomain;
 import org.eclipse.jface.text.ITextSelection;
 import org.eclipse.jface.viewers.ISelection;
+import org.eclipse.ui.IEditorInput;
 import org.eclipse.ui.IEditorPart;
+import org.eclipse.ui.part.FileEditorInput;
 import org.eclipse.xtext.resource.EObjectAtOffsetHelper;
 import org.eclipse.xtext.resource.ILocationInFileProvider;
 import org.eclipse.xtext.resource.IResourceServiceProvider;
@@ -27,6 +35,7 @@ public class XtextEditorConnector implements IEditorConnector {
 	private XtextEditor editor;
 	private EObjectAtOffsetHelper objectAtOffsetHelper;
 	private ILocationInFileProvider locationInFileProvider;
+	private Resource resource;
 
 	public XtextEditorConnector() {
 		//
@@ -35,10 +44,20 @@ public class XtextEditorConnector implements IEditorConnector {
 	public boolean canHandle(IEditorPart editor) {
 		if(editor instanceof XtextEditor){
 			this.editor = (XtextEditor) editor;
-			XtextResource resource = getXtextResource(this.editor);
-			IResourceServiceProvider resourceServiceProvider = resource.getResourceServiceProvider();
-			objectAtOffsetHelper = resourceServiceProvider.get(EObjectAtOffsetHelper.class);
-			locationInFileProvider = resourceServiceProvider.get(ILocationInFileProvider.class);
+			IXtextDocument doc = XtextDocumentUtil.get(editor);
+			resource = doc.modify(new IUnitOfWork<Resource, XtextResource>() {
+
+				public Resource exec(XtextResource state) throws Exception {
+					IResourceServiceProvider resourceServiceProvider = state.getResourceServiceProvider();
+					objectAtOffsetHelper = resourceServiceProvider.get(EObjectAtOffsetHelper.class);
+					locationInFileProvider = resourceServiceProvider.get(ILocationInFileProvider.class);
+					return state;
+				}
+			});
+//			XtextResource resource = getXtextResource(this.editor);
+//			IResourceServiceProvider resourceServiceProvider = resource.getResourceServiceProvider();
+//			objectAtOffsetHelper = resourceServiceProvider.get(EObjectAtOffsetHelper.class);
+//			locationInFileProvider = resourceServiceProvider.get(ILocationInFileProvider.class);
 			if(objectAtOffsetHelper != null && locationInFileProvider != null){
 				return true;
 			}
@@ -46,36 +65,62 @@ public class XtextEditorConnector implements IEditorConnector {
 		return false;
 	}
 
-	public List<EObject> handleSelection(ISelection selection) {
-		if(selection instanceof ITextSelection){
-			Resource resource = getXtextResource(editor);
-			System.out.println(resource);
-			if(resource instanceof XtextResource){
-				XtextResource xtextResource = (XtextResource) resource;
-				ITextSelection textSelection = (ITextSelection) selection;
-				Set<EObject> elementSet = new LinkedHashSet<EObject>();
-				int offset = textSelection.getOffset();
-				for (int i = textSelection.getLength(); i > 0; i--) {
-					EObject element = objectAtOffsetHelper.resolveElementAt(xtextResource, offset);
-					elementSet.add(element);
-					offset++;
-				}
-				List<EObject> nonProxyElements = new LinkedList<EObject>();
-				for (EObject object : elementSet) {
-					if(!object.eIsProxy()){
-						nonProxyElements.add(object);
+	public List<EObject> handleSelection(final ISelection selection) {
+		IXtextDocument doc = XtextDocumentUtil.get(editor);
+		List<EObject> selectedElements = doc.modify(new IUnitOfWork<List<EObject>, XtextResource>() {
+
+			public List<EObject> exec(XtextResource state) throws Exception {
+				System.out.println(state);
+				if(selection instanceof ITextSelection){
+					ITextSelection textSelection = (ITextSelection) selection;
+					Set<EObject> elementSet = new LinkedHashSet<EObject>();
+					int offset = textSelection.getOffset();
+					for (int i = textSelection.getLength(); i > 0; i--) {
+						EObject element = objectAtOffsetHelper.resolveElementAt(state, offset);
+						elementSet.add(element);
+						offset++;
 					}
+					List<EObject> nonProxyElements = new LinkedList<EObject>();
+					for (EObject object : elementSet) {
+						if(!object.eIsProxy()){
+							nonProxyElements.add(object);
+						}
+					}
+					return nonProxyElements;
 				}
-				return nonProxyElements;
+				return null;
 			}
-		}
-		return null;
+		});
+		return selectedElements;
+//		if(selection instanceof ITextSelection){
+//			resource = getXtextResource(editor);
+//			System.out.println(resource);
+//			if(resource instanceof XtextResource){
+//				XtextResource xtextResource = (XtextResource) resource;
+//				ITextSelection textSelection = (ITextSelection) selection;
+//				Set<EObject> elementSet = new LinkedHashSet<EObject>();
+//				int offset = textSelection.getOffset();
+//				for (int i = textSelection.getLength(); i > 0; i--) {
+//					EObject element = objectAtOffsetHelper.resolveElementAt(xtextResource, offset);
+//					elementSet.add(element);
+//					offset++;
+//				}
+//				List<EObject> nonProxyElements = new LinkedList<EObject>();
+//				for (EObject object : elementSet) {
+//					if(!object.eIsProxy()){
+//						nonProxyElements.add(object);
+//					}
+//				}
+//				return nonProxyElements;
+//			}
+//		}
+//		return null;
 	}
 
 	private XtextResource getXtextResource(XtextEditor editor){
 		IXtextDocument doc = XtextDocumentUtil.get(editor);
 		XtextResource resource;
-		resource = doc.readOnly(new IUnitOfWork<XtextResource, XtextResource>() {
+		resource = doc.modify(new IUnitOfWork<XtextResource, XtextResource>() {
 
 			public XtextResource exec(XtextResource state) throws Exception {
 				return state;
@@ -89,23 +134,37 @@ public class XtextEditorConnector implements IEditorConnector {
 		return null;
 	}
 
-	public void selectEObjects(List<EObject> objectsToSelect) {
-		int offset = Integer.MAX_VALUE;
-		int length = 0;
-		for (EObject object : objectsToSelect) {
-			ITextRegion textRegion = locationInFileProvider.getFullTextRegion(object);
-			if (textRegion.getOffset() < offset){
-				if(length != 0){
-					length += offset - textRegion.getOffset();
-				} else {
-					length = textRegion.getLength();
+	public void selectEObjects(final List<EObject> objectsToSelect) {
+		IXtextDocument doc = XtextDocumentUtil.get(editor);
+		doc.modify(new IUnitOfWork<String, XtextResource>() {
+
+			public String exec(XtextResource state) throws Exception {
+//				URI uri = resource.getURI();
+//				String pathString = uri.toPlatformString(true);
+//				IPath path = Path.fromOSString(pathString);
+//				IFile file = ResourcesPlugin.getWorkspace().getRoot().getFile(path);
+//				IEditorInput editorInput = new FileEditorInput(file);
+//				editor.setInput(editorInput);
+				int offset = Integer.MAX_VALUE;
+				int length = 0;
+				for (EObject object : objectsToSelect) {
+					ITextRegion textRegion = locationInFileProvider.getFullTextRegion(object);
+					if (textRegion.getOffset() < offset){
+						if(length != 0){
+							length += offset - textRegion.getOffset();
+						} else {
+							length = textRegion.getLength();
+						}
+						offset = textRegion.getOffset();
+					}
 				}
-				offset = textRegion.getOffset();
+				if(length != 0){
+					editor.selectAndReveal(offset, length);
+				}
+
+				return null;
 			}
-		}
-		if(length != 0){
-			editor.selectAndReveal(offset, length);
-		}
+		});
 	}
 
 }
