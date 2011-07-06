8ca18e8269eb4f7ed7d30642f8bc823396b313c6
Jan <jreimone@users.noreply.github.com>
2011-07-06 15:48:09 +0000

    refactorings for company language

---
+++ Refactoring/Registrations/Mappings/org.emftext.language.company.refactoring/src/org/emftext/language/company/refactoring/ExtractSubDepartmentPostProcessor.java
@@ -0,0 +1,53 @@
+package org.emftext.language.company.refactoring;
+
+import java.util.List;
+import java.util.Map;
+import java.util.Set;
+
+import org.eclipse.core.runtime.IStatus;
+import org.eclipse.core.runtime.Status;
+import org.eclipse.emf.ecore.EObject;
+import org.eclipse.emf.ecore.change.ChangeDescription;
+import org.eclipse.emf.ecore.resource.ResourceSet;
+import org.emftext.language.company.Department;
+import org.emftext.language.company.Employee;
+import org.emftext.language.refactoring.refactoring_specification.RefactoringSpecification;
+import org.emftext.language.refactoring.roles.Role;
+import org.emftext.refactoring.registry.rolemapping.IRefactoringPostProcessor;
+
+public class ExtractSubDepartmentPostProcessor implements
+		IRefactoringPostProcessor {
+
+	public ExtractSubDepartmentPostProcessor() {
+		// TODO Auto-generated constructor stub
+	}
+
+	@Override
+	public IStatus process(Map<Role, List<EObject>> roleRuntimeInstanceMap, ResourceSet resourceSet, ChangeDescription change) {
+		List<EObject> extractees = getInstancesByRoleName(roleRuntimeInstanceMap, "Extractee");
+		List<EObject> containers = getInstancesByRoleName(roleRuntimeInstanceMap, "NewContainer");
+		Department container = (Department) containers.get(0);
+		if(container != null && extractees != null && extractees.size() > 0){
+			Employee manager = (Employee) extractees.get(0);
+//			extractees.remove(0);
+			container.setManager(manager);
+		}
+		return Status.OK_STATUS;
+	}
+
+	@Override
+	public IStatus process(Map<Role, List<EObject>> roleRuntimeInstanceMap, ResourceSet resourceSet, ChangeDescription change, RefactoringSpecification refSpec) {
+		return process(roleRuntimeInstanceMap, resourceSet, change);
+	}
+
+	private List<EObject> getInstancesByRoleName(Map<Role, List<EObject>> roleRuntimeInstanceMap, String roleName){
+		Set<Role> roles = roleRuntimeInstanceMap.keySet();
+		for (Role role : roles) {
+			if(role.getName().equals(roleName)){
+				return roleRuntimeInstanceMap.get(role);
+			}
+		}
+		return null;
+	}
+
+}
