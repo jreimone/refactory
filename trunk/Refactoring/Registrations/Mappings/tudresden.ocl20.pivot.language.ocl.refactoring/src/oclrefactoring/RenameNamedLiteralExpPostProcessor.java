142b9d490285c5e914c15a3645b0fdf5835b85e3
mmuck <mmuck@2290cd21-d4b4-4435-9c65-06e28a4c4f12>
2011-07-24 06:44:39 +0000

    - Adapted RenameSimpleNameCS to use the SelectXWithVar refactoring instead of RenameX - modified renaming postprocessors - created rename variable refactoring for direct selection of a variable declaration

--- trunk/Refactoring/Registrations/Mappings/tudresden.ocl20.pivot.language.ocl.refactoring/src/oclrefactoring/RenameNamedLiteralExpPostProcessor.java
+++ trunk/Refactoring/Registrations/Mappings/tudresden.ocl20.pivot.language.ocl.refactoring/src/oclrefactoring/RenameNamedLiteralExpPostProcessor.java
@@ -3,111 +3,238 @@
  */
 package oclrefactoring;
 
+import tudresden.ocl20.pivot.language.ocl.VariableDeclarationCS;
+import java.util.Iterator;
 import java.util.List;
 import java.util.Map;
 import java.util.Set;
 
 import org.eclipse.core.runtime.IStatus;
 import org.eclipse.core.runtime.Status;
+import org.eclipse.emf.common.util.BasicEList;
+import org.eclipse.emf.common.util.EList;
+import org.eclipse.emf.common.util.EMap;
 import org.eclipse.emf.common.util.TreeIterator;
 import org.eclipse.emf.ecore.EObject;
 import org.eclipse.emf.ecore.change.ChangeDescription;
+import org.eclipse.emf.ecore.change.FeatureChange;
+import org.eclipse.emf.ecore.change.ListChange;
 import org.eclipse.emf.ecore.resource.ResourceSet;
+import org.eclipse.emf.ecore.util.EcoreUtil;
 import org.emftext.language.refactoring.refactoring_specification.RefactoringSpecification;
 import org.emftext.language.refactoring.roles.Role;
 import org.emftext.refactoring.ltk.IModelRefactoringWizardPage;
-import org.emftext.refactoring.registry.rolemapping.AbstractRefactoringPostProcessor;
+import org.emftext.refactoring.registry.rolemapping.IRefactoringPostProcessor;
 
 import tudresden.ocl20.pivot.language.ocl.BodyDeclarationCS;
 import tudresden.ocl20.pivot.language.ocl.DefinitionExpCS;
 import tudresden.ocl20.pivot.language.ocl.InvariantExpCS;
+import tudresden.ocl20.pivot.language.ocl.IterateExpCS;
+import tudresden.ocl20.pivot.language.ocl.IteratorExpCS;
+import tudresden.ocl20.pivot.language.ocl.IteratorExpVariableCS;
+import tudresden.ocl20.pivot.language.ocl.LetExpCS;
 import tudresden.ocl20.pivot.language.ocl.NamedLiteralExpCS;
+import tudresden.ocl20.pivot.language.ocl.OclExpressionCS;
+import tudresden.ocl20.pivot.language.ocl.PackageDeclarationCS;
 import tudresden.ocl20.pivot.language.ocl.SimpleNameCS;
+import tudresden.ocl20.pivot.language.ocl.VariableDeclarationWithInitCS;
+import tudresden.ocl20.pivot.language.ocl.impl.IterateExpCSImpl;
 import tudresden.ocl20.pivot.pivotmodel.NamedElement;
 import tudresden.ocl20.pivot.pivotmodel.Property;
-
 /**
  * @author Michael Muck
  *
  */
-public class RenameNamedLiteralExpPostProcessor extends AbstractRefactoringPostProcessor {
+public class RenameNamedLiteralExpPostProcessor implements IRefactoringPostProcessor {
+	
+	
+	String origName;
+	String newName;
+	NamedLiteralExpCS selection;
+	boolean refactoringSuccessful = false;
 
-	private NamedLiteralExpCS selected;
-	private Property newNameHolder;
-	private String newName;
-	private String originalName;
-	
-	
 	public RenameNamedLiteralExpPostProcessor() {
-		// TODO Auto-generated constructor stub
-	}
-
-	@Override
-	public IStatus process(Map<Role, List<EObject>> roleRuntimeInstanceMap,	EObject refactoredModel, ResourceSet resourceSet, ChangeDescription change, 
-			RefactoringSpecification refSpec, List<IModelRefactoringWizardPage> customWizardPages, boolean isFakeRun) {
-		System.err.println("postprocessor activated!");
-		Set<Role> keySet = roleRuntimeInstanceMap.keySet();
-		for (Role role : keySet) {
-			List<EObject> roleplayers = roleRuntimeInstanceMap.get(role);
-			if (role.getName().equals("Selection")) {
-				if (roleplayers.size()==1 && roleplayers.get(0) instanceof NamedLiteralExpCS) {
-					selected = (NamedLiteralExpCS) roleplayers.get(0);
-					System.out.println("found selected element as: "+selected);
-					originalName = selected.getNamedElement().getName();
-					System.out.println("original name is: "+ originalName);
-				}
-			}
-			else if(role.getName().equals("Nameable")) {
-				if (roleplayers.size() == 1 && roleplayers.get(0) instanceof Property) {
-					newNameHolder = (Property) roleplayers.get(0);
-					newName = newNameHolder.getName();
-					System.out.println("new name is: "+ newName);
-				}
-			}
-		}
 		
-		performSpecificTransformation();
-		
-		return Status.OK_STATUS;
-	}
-
-	private void performSpecificTransformation() {
-		EObject constraintRoot = selected.eContainer();
-		while (	!(constraintRoot instanceof InvariantExpCS) && 
-				!(constraintRoot instanceof DefinitionExpCS) && 
-				!(constraintRoot instanceof BodyDeclarationCS)
-		) {
-			
-			constraintRoot = constraintRoot.eContainer();
-			
-		}
-		
-		System.err.println("constraint root determined as: "+ constraintRoot.eClass());
-		//now the actual renaming
-		TreeIterator<EObject> eAllContents = constraintRoot.eAllContents();
-		while (eAllContents.hasNext()) {
-			EObject eObject = (EObject) eAllContents.next();
-			if (eObject instanceof NamedLiteralExpCS) {
-				NamedLiteralExpCS literal = (NamedLiteralExpCS) eObject;
-				NamedElement namedElement = literal.getNamedElement();
-				if(namedElement.getName().equals(originalName)) {
-					namedElement.setName(newName);
-				}
-			}
-			else if (eObject instanceof SimpleNameCS) {
-				SimpleNameCS simpleName = (SimpleNameCS) eObject;
-				if(simpleName.getSimpleName().equals(originalName)) {
-					simpleName.setSimpleName(newName);
-				}
-			}
-		}
 	}
 
 	@Override
 	public IStatus process(Map<Role, List<EObject>> roleRuntimeInstanceMap,
 			ResourceSet resourceSet, ChangeDescription change) {
 		// TODO Auto-generated method stub
+		System.out.println("--------- old postprocessor activated ------------");
 		return null;
 	}
 
+	@Override
+	public IStatus process(Map<Role, List<EObject>> roleRuntimeInstanceMap,
+			EObject refactoredModel, ResourceSet resourceSet,
+			ChangeDescription change, RefactoringSpecification refSpec,
+			List<IModelRefactoringWizardPage> customWizardPages,
+			boolean isFakeRun) {
+		
+		if (isFakeRun) {
+			System.out.println("---------------------postprocessor activated in fake mode - no processing done for now!--------------------");
+			return Status.OK_STATUS;
+		}
+		else System.out.println("---------------------postprocessor activated in real mode!--------------------");
+		
+		//extract selection, old name and new name
+
+		Set<Role> keySet = roleRuntimeInstanceMap.keySet();
+		List<EObject> mappedElements;
+		for (Role role : keySet) {
+			if (role.getName().equals("Selection")) {
+				mappedElements = roleRuntimeInstanceMap.get(role);
+				if (mappedElements != null && mappedElements.size() > 0
+						&& (mappedElements.get(0) instanceof NamedLiteralExpCS)) {
+					selection = (NamedLiteralExpCS) mappedElements.get(0);
+				}
+			}
+			else if (role.getName().equals("Nameable")) {
+				mappedElements = roleRuntimeInstanceMap.get(role);
+				if (mappedElements != null && mappedElements.size() >0 && (mappedElements.get(0)) instanceof SimpleNameCS) {
+					newName = ((SimpleNameCS) mappedElements.get(0)).getSimpleName();
+					
+				}
+			}
+		}
+		if (selection == null) {
+			System.err.println("No variable declaration could be found by postprocessor. Refactoring aborted.");
+			return Status.OK_STATUS;
+		}
+		origName = selection.getNamedElement().getName();
+		System.out.println("orig name: "+ origName);
+		System.out.println("new name: "+ newName);
+		
+		performPostProcessing();
+		if (refactoringSuccessful) return Status.OK_STATUS;
+		else return Status.CANCEL_STATUS;
+	}
+
+	private void performPostProcessing() {
+
+		EObject parent = getDefinitionParent(selection);
+		if (parent instanceof LetExpCS) {
+			refactoringSuccessful = performRenamingForLetExp((LetExpCS)parent);
+		}
+		if (parent instanceof IteratorExpCS) {
+			refactoringSuccessful = performRenamingForIteratorExp((IteratorExpCS)parent);
+		}
+		
+	}
+
+	
+
+	private boolean performRenamingForIteratorExp(IteratorExpCS parent) {
+		//finding and renaming the variable declaration
+		EList<IteratorExpVariableCS> variables = parent.getIteratorVariables();
+		for (int i = 0; i < variables.size(); i++) {
+			IteratorExpVariableCS akt = variables.get(i);
+			if (akt.getVariableName().getSimpleName().equals(origName)) {
+				akt.getVariableName().setSimpleName(newName);
+				i = variables.size();
+			}
+		}
+
+		//finding and renaming all references
+		Iterator<EObject> it = parent.eAllContents();
+		while (it.hasNext()) {
+			EObject akt = it.next();
+			if (akt instanceof NamedLiteralExpCS && ((NamedLiteralExpCS)akt).getNamedElement().getName().equals(origName)) {
+				//we found a variable reference with the original name
+				((NamedLiteralExpCS)akt).getNamedElement().setName(newName);
+			}
+		}
+		return true;
+	}
+
+	private EObject getDefinitionParent(EObject child) {
+
+		EObject parent = child.eContainer();
+		if (parent instanceof PackageDeclarationCS) return null; //break condition	
+		
+		if (parent instanceof LetExpCS) return parent;
+		if (parent instanceof IteratorExpCS) return parent;
+			
+		return getDefinitionParent(parent);
+		
+	}
+
+	private boolean performRenamingForLetExp(LetExpCS parent) {
+		// rename declaration
+		EList<VariableDeclarationWithInitCS> variables = parent.getVariableDeclarations();
+		for (int i = 0; i < variables.size(); i++) {
+			VariableDeclarationWithInitCS akt = variables.get(i);
+			if (akt.getVariableName().getSimpleName().equals(origName)) {
+				akt.getVariableName().setSimpleName(newName);
+				i = variables.size();
+			}
+		}
+		
+		EList<IteratorExpCS> overlappingIterators = calculateOverlappingIterators(parent.getOclExpression());
+		Boolean hasOverlapping = false;
+		if (overlappingIterators.size() > 0) hasOverlapping = true;
+		if (hasOverlapping) System.out.println("	- overlapping detected!");
+		else System.out.println("	- no overlapping detected!");
+		Iterator<EObject> scopeIt = parent.getOclExpression().eAllContents();
+		
+		
+		while (scopeIt.hasNext()) {
+			EObject akt = scopeIt.next();
+			if (akt instanceof NamedLiteralExpCS && ((NamedLiteralExpCS)akt).getNamedElement().getName().equals(origName)) {
+				//we found a variable reference with the original name
+				System.out.println("	- found variable to rename");
+				NamedLiteralExpCS aktRef = (NamedLiteralExpCS) akt;
+				//check if it can be renamed or if it is an iterator variable that does not reference the variable to rename
+				Boolean renamingOK = true;
+				if (hasOverlapping) {
+					renamingOK = checkIsReference(aktRef, overlappingIterators);
+				}
+				if (!renamingOK) System.out.println(" - variable is an iterator variable and will not be renamed");
+				if (renamingOK) System.out.println("	- variable is not an iterator variable and can be renamed");
+				if (renamingOK) aktRef.getNamedElement().setName(newName);
+				
+			}
+		}		
+		return true;
+	}
+
+	
+
+	private EList<IteratorExpCS> calculateOverlappingIterators(OclExpressionCS oclExpression) {
+
+		EList<IteratorExpCS> overlappingIterators = new BasicEList<IteratorExpCS>();
+		
+		Iterator<EObject> it = oclExpression.eAllContents();
+		while(it.hasNext()) {
+			EObject akt = it.next();
+			if (akt instanceof SimpleNameCS && ((SimpleNameCS)akt).getSimpleName().equals(origName)) {
+				//we found an iterator variable that has the same name like the selected variable to rename
+				//this iterator variable and all its references must not be renamed!
+				System.out.println("	- found an overlapping iterator variable");
+				System.out.println("	- akt.econtainer is: " +akt.eContainer());
+				System.out.println("	- akt.econtainer.econtainer is: "+akt.eContainer().eContainer());
+				if (akt.eContainer().eContainer() instanceof IteratorExpCS) overlappingIterators.add((IteratorExpCS) akt.eContainer().eContainer());
+			}
+		}		
+		return overlappingIterators;
+	}
+
+	//checks if the given variable is contained in one of the iterators or not
+	//if it is not, it is a reference to the variable to rename and renaming would be true
+	private Boolean checkIsReference(NamedLiteralExpCS aktRef, EList<IteratorExpCS> overlappingIterators) {
+		
+		for (int i = 0; i < overlappingIterators.size(); i++) {
+			Iterator<EObject> it = overlappingIterators.get(i).eAllContents();
+			while (it.hasNext()) {
+				EObject akt = it.next();
+				//now if one of the elements contained in the iterator is our variable, it is referencing the overlapping iterator variable
+				//and not the one that should be renamed
+				if (EcoreUtil.equals(akt, aktRef)) return false; 
+			}
+		}
+		
+		return true;
+	}
+	
 }
--- trunk/Refactoring/Registrations/Mappings/tudresden.ocl20.pivot.language.ocl.refactoring/src/oclrefactoring/RenameVariablePP.java
+++ trunk/Refactoring/Registrations/Mappings/tudresden.ocl20.pivot.language.ocl.refactoring/src/oclrefactoring/RenameVariablePP.java
@@ -11,6 +11,7 @@ import java.util.Set;
 
 import org.eclipse.core.runtime.IStatus;
 import org.eclipse.core.runtime.Status;
+import org.eclipse.emf.common.util.BasicEList;
 import org.eclipse.emf.common.util.EList;
 import org.eclipse.emf.common.util.EMap;
 import org.eclipse.emf.common.util.TreeIterator;
@@ -19,6 +20,7 @@ import org.eclipse.emf.ecore.change.ChangeDescription;
 import org.eclipse.emf.ecore.change.FeatureChange;
 import org.eclipse.emf.ecore.change.ListChange;
 import org.eclipse.emf.ecore.resource.ResourceSet;
+import org.eclipse.emf.ecore.util.EcoreUtil;
 import org.emftext.language.refactoring.refactoring_specification.RefactoringSpecification;
 import org.emftext.language.refactoring.roles.Role;
 import org.emftext.refactoring.ltk.IModelRefactoringWizardPage;
@@ -27,11 +29,15 @@ import org.emftext.refactoring.registry.rolemapping.IRefactoringPostProcessor;
 import tudresden.ocl20.pivot.language.ocl.BodyDeclarationCS;
 import tudresden.ocl20.pivot.language.ocl.DefinitionExpCS;
 import tudresden.ocl20.pivot.language.ocl.InvariantExpCS;
+import tudresden.ocl20.pivot.language.ocl.IterateExpCS;
+import tudresden.ocl20.pivot.language.ocl.IteratorExpCS;
 import tudresden.ocl20.pivot.language.ocl.LetExpCS;
 import tudresden.ocl20.pivot.language.ocl.NamedLiteralExpCS;
 import tudresden.ocl20.pivot.language.ocl.OclExpressionCS;
+import tudresden.ocl20.pivot.language.ocl.PackageDeclarationCS;
 import tudresden.ocl20.pivot.language.ocl.SimpleNameCS;
 import tudresden.ocl20.pivot.language.ocl.VariableDeclarationWithInitCS;
+import tudresden.ocl20.pivot.language.ocl.impl.IterateExpCSImpl;
 import tudresden.ocl20.pivot.pivotmodel.NamedElement;
 import tudresden.ocl20.pivot.pivotmodel.Property;
 /**
@@ -64,7 +70,11 @@ public class RenameVariablePP implements IRefactoringPostProcessor {
 			List<IModelRefactoringWizardPage> customWizardPages,
 			boolean isFakeRun) {
 		
-		System.out.println("---------------------postprocessor activated!--------------------");
+		if (isFakeRun) {
+			System.out.println("---------------------postprocessor activated in fake mode - no processing done for now!--------------------");
+			return Status.OK_STATUS;
+		}
+		else System.out.println("---------------------postprocessor activated in real mode!--------------------");
 		
 		//extract selection, old name and new name
 
@@ -95,10 +105,100 @@ public class RenameVariablePP implements IRefactoringPostProcessor {
 		System.out.println("new name: "+ newName);
 		System.out.println("var dec: "+ variableDeclaration);
 		
+		performPostProcessing();
+		
 		return Status.OK_STATUS;
 	}
 
+	private void performPostProcessing() {
+
+		EObject parent = getDefinitionParent(variableDeclaration);
+		if (parent instanceof LetExpCS) performRenamingForLetExp((LetExpCS)parent);
+		
+	}
 
 	
+
+	private EObject getDefinitionParent(EObject child) {
+
+		EObject parent = child.eContainer();
+		if (parent instanceof PackageDeclarationCS) return null; //break condition	
+		
+		if (parent instanceof LetExpCS) return parent;
+			
+		return getDefinitionParent(parent);
+		
+	}
+
+	private boolean performRenamingForLetExp(LetExpCS parent) {
+		// rename declaration
+		variableDeclaration.getVariableName().setSimpleName(newName);
+		
+		
+		EList<IteratorExpCS> overlappingIterators = calculateOverlappingIterators(parent.getOclExpression());
+		Boolean hasOverlapping = false;
+		if (overlappingIterators.size() > 0) hasOverlapping = true;
+		if (hasOverlapping) System.out.println("	- overlapping detected!");
+		else System.out.println("	- no overlapping detected!");
+		Iterator<EObject> scopeIt = parent.getOclExpression().eAllContents();
+		
+		
+		while (scopeIt.hasNext()) {
+			EObject akt = scopeIt.next();
+			if (akt instanceof NamedLiteralExpCS && ((NamedLiteralExpCS)akt).getNamedElement().getName().equals(origName)) {
+				//we found a variable reference with the original name
+				System.out.println("	- found variable to rename");
+				NamedLiteralExpCS aktRef = (NamedLiteralExpCS) akt;
+				//check if it can be renamed or if it is an iterator variable that does not reference the variable to rename
+				Boolean renamingOK = true;
+				if (hasOverlapping) {
+					renamingOK = checkIsReference(aktRef, overlappingIterators);
+				}
+				if (!renamingOK) System.out.println(" - variable is an iterator variable and will not be renamed");
+				if (renamingOK) System.out.println("	- variable is not an iterator variable and can be renamed");
+				if (renamingOK) aktRef.getNamedElement().setName(newName);
+				
+			}
+		}		
+		return true;
+	}
+
+	
+
+	private EList<IteratorExpCS> calculateOverlappingIterators(OclExpressionCS oclExpression) {
+
+		EList<IteratorExpCS> overlappingIterators = new BasicEList<IteratorExpCS>();
+		
+		Iterator<EObject> it = oclExpression.eAllContents();
+		while(it.hasNext()) {
+			EObject akt = it.next();
+			if (akt instanceof SimpleNameCS && ((SimpleNameCS)akt).getSimpleName().equals(origName)) {
+				//we found an iterator variable that has the same name like the selected variable to rename
+				//this iterator variable and all its references must not be renamed!
+				System.out.println("	- found an overlapping iterator variable");
+				System.out.println("	- akt.econtainer is: " +akt.eContainer());
+				System.out.println("	- akt.econtainer.econtainer is: "+akt.eContainer().eContainer());
+				if (akt.eContainer().eContainer() instanceof IteratorExpCS) overlappingIterators.add((IteratorExpCS) akt.eContainer().eContainer());
+			}
+		}		
+		return overlappingIterators;
+	}
+
+	//checks if the given variable is contained in one of the iterators or not
+	//if it is not, it is a reference to the variable to rename and renaming would be true
+	private Boolean checkIsReference(NamedLiteralExpCS aktRef, EList<IteratorExpCS> overlappingIterators) {
+		
+		for (int i = 0; i < overlappingIterators.size(); i++) {
+			Iterator<EObject> it = overlappingIterators.get(i).eAllContents();
+			while (it.hasNext()) {
+				EObject akt = it.next();
+				//now if one of the elements contained in the iterator is our variable, it is referencing the overlapping iterator variable
+				//and not the one that should be renamed
+				if (EcoreUtil.equals(akt, aktRef)) return false; 
+			}
+		}
+		
+		return true;
+	}
 	
 }
