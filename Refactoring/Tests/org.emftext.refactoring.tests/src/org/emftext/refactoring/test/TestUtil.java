74d7596771c56165e679d03cc6db51752f2ac9d5
Jendrik Johannes <jendrik.johannes@gmail.com>
2011-10-11 08:59:12 +0000

    removed imports of non-existing classes

--- Refactoring/Tests/org.emftext.refactoring.tests/src/org/emftext/refactoring/test/TestUtil.java
+++ Refactoring/Tests/org.emftext.refactoring.tests/src/org/emftext/refactoring/test/TestUtil.java
@@ -17,8 +17,6 @@ import org.eclipse.emf.ecore.resource.ResourceSet;
 import org.eclipse.emf.ecore.resource.impl.ResourceSetImpl;
 import org.emftext.language.java.JavaClasspath;
 import org.emftext.language.java.resource.java.IJavaOptions;
-import org.emftext.language.java.resource.util.JavaPostProcessor;
-import org.emftext.language.java.resource.util.UnicodeConverterProvider;
 
 /**
  * @author Jan Reimann
