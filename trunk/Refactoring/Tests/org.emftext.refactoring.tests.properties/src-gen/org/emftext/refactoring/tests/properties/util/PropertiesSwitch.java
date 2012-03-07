5f7dfa2f111ad06825b602eb510fce36a3232e1a
jreimann <jreimann@2290cd21-d4b4-4435-9c65-06e28a4c4f12>
2012-03-07 08:34:41 +0000

    regeneration

--- trunk/Refactoring/Tests/org.emftext.refactoring.tests.properties/src-gen/org/emftext/refactoring/tests/properties/util/PropertiesSwitch.java
+++ trunk/Refactoring/Tests/org.emftext.refactoring.tests.properties/src-gen/org/emftext/refactoring/tests/properties/util/PropertiesSwitch.java
@@ -1,18 +1,24 @@
 /**
- * <copyright>
- * </copyright>
- *
- * $Id$
+ * Copyright (c) 2006-2011
+ * Software Technology Group, Dresden University of Technology
+ * 
+ * All rights reserved. This program and the accompanying materials
+ * are made available under the terms of the Eclipse Public License v1.0 
+ * which accompanies this distribution, and is available at
+ * http://www.eclipse.org/legal/epl-v10.html
+ * 
+ * Contributors:
+ *   Software Technology Group - TU Dresden, Germany 
+ *      - initial API and implementation
+ * 
  */
 package org.emftext.refactoring.tests.properties.util;
 
-import java.util.List;
-
-import org.eclipse.emf.ecore.EClass;
 import org.eclipse.emf.ecore.EObject;
-
 import org.eclipse.emf.ecore.EPackage;
+
 import org.eclipse.emf.ecore.util.Switch;
+
 import org.emftext.refactoring.tests.properties.*;
 
 /**
