b64640a8c9514a5f6525879940ae912f5adef06c
Jendrik Johannes <jendrik.johannes@gmail.com>
2012-04-09 19:53:59 +0000

    updated license headers

--- Refactoring/Tests/org.featuremapper.models.feature/src_generated/org/featuremapper/models/feature/util/FeatureSwitch.java
+++ Refactoring/Tests/org.featuremapper.models.feature/src_generated/org/featuremapper/models/feature/util/FeatureSwitch.java
@@ -1,16 +1,18 @@
 /*******************************************************************************
- * Copyright (c) 2008-2011 Software Technology Group - TU Dresden, Germany.
- *
+ * Copyright (c) 2006-2012
+ * Software Technology Group, Dresden University of Technology
+ * DevBoost GmbH, Berlin, Amtsgericht Charlottenburg, HRB 140026
+ * 
  * All rights reserved. This program and the accompanying materials
- * are made available under the terms of the GNU Lesser Public License v3.0
+ * are made available under the terms of the Eclipse Public License v1.0
  * which accompanies this distribution, and is available at
- * http://www.gnu.org/licenses/lgpl-3.0.html
- *
+ * http://www.eclipse.org/legal/epl-v10.html
+ * 
  * Contributors:
- *     Software Technology Group - TU Dresden, Germany
- *     - initial API and implementation
+ *   Software Technology Group - TU Dresden, Germany;
+ *   DevBoost GmbH - Berlin, Germany
+ *      - initial API and implementation
  ******************************************************************************/
-
 /**
  * <copyright>
  * </copyright>
