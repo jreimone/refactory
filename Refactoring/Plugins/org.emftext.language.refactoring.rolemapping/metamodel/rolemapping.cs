5101f37de70386bb870e4d3d3a93b3b1e563e024
Jan <jreimone@users.noreply.github.com>
2012-04-20 09:48:51 +0000

    removed builders

--- Refactoring/Plugins/org.emftext.language.refactoring.rolemapping/metamodel/rolemapping.cs
+++ Refactoring/Plugins/org.emftext.language.refactoring.rolemapping/metamodel/rolemapping.cs
@@ -15,7 +15,7 @@ OPTIONS {
 	additionalDependencies = "org.emftext.refactoring.registry.rolemodel,org.emftext.refactoring.util,org.emftext.refactoring.registry.rolemapping";
 	additionalUIDependencies = "org.emftext.language.refactoring.roles.resource.rolestext.ui";
 	overrideHoverTextProvider = "false";
-	overrideBuilder = "false";
+	overrideBuilder = "true";
 	overrideUIPluginXML = "false";
 	disableLaunchSupport = "true";
 	disableDebugSupport = "true";
