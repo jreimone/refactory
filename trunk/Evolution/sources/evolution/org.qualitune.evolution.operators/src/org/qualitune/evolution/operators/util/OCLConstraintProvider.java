325c8509b4b8eb783980c280e58dacfdc5cc7898
jreimann <jreimann@2290cd21-d4b4-4435-9c65-06e28a4c4f12>
2012-05-24 06:43:39 +0000

    ASSIGN

---
+++ trunk/Evolution/sources/evolution/org.qualitune.evolution.operators/src/org/qualitune/evolution/operators/util/OCLConstraintProvider.java
@@ -0,0 +1,106 @@
+package org.qualitune.evolution.operators.util;
+
+import java.util.HashSet;
+import java.util.Set;
+
+import org.eclipse.core.runtime.IStatus;
+import org.eclipse.emf.common.notify.Notification;
+import org.eclipse.emf.ecore.EObject;
+import org.eclipse.emf.validation.AbstractModelConstraint;
+import org.eclipse.emf.validation.IValidationContext;
+import org.eclipse.emf.validation.model.Category;
+import org.eclipse.emf.validation.model.ConstraintSeverity;
+import org.eclipse.emf.validation.model.ConstraintStatus;
+import org.eclipse.emf.validation.model.EvaluationMode;
+import org.eclipse.emf.validation.model.IModelConstraint;
+import org.eclipse.emf.validation.service.AbstractConstraintDescriptor;
+import org.eclipse.emf.validation.service.IConstraintDescriptor;
+import org.qualitune.evolution.operators.Operator;
+
+public class OCLConstraintProvider extends AbstractModelConstraint implements IModelConstraint{
+	
+	/* (non-Javadoc)
+	 * @see org.eclipse.emf.validation.AbstractModelConstraint#validate(org.eclipse.emf.validation.IValidationContext)
+	 */
+	@Override
+	public IStatus validate(IValidationContext ctx) {
+		EObject target = ctx.getTarget();
+		if(target instanceof Operator){
+			boolean validate = OperatorsValidator.INSTANCE.validate(target, null, null);
+			if(!validate){
+				Set<EObject> targets = new HashSet<EObject>();
+				targets.add(target);
+				ConstraintStatus status = new ConstraintStatus(this, target, "A constraint could not be satisfied.", targets);
+				return status;
+			}
+		}
+		return ctx.createSuccessStatus();
+	}
+
+	@Override
+	public IConstraintDescriptor getDescriptor() {
+		return new AbstractConstraintDescriptor() {
+			public boolean targetsTypeOf(EObject eObject) {
+				return true;
+			}
+			
+			public boolean targetsEvent(Notification notification) {
+				return false;
+			}
+			
+			public void removeCategory(Category category) {
+			}
+			
+			public boolean isLive() {
+				return false;
+			}
+			
+			public boolean isBatch() {
+				return true;
+			}
+			
+			public int getStatusCode() {
+				return 0;
+			}
+			
+			public ConstraintSeverity getSeverity() {
+				return ConstraintSeverity.ERROR;
+			}
+			
+			public String getPluginId() {
+				return "org.qualitune.evolution.operators.";
+			}
+			
+			public String getName() {
+				return "name";
+			}
+			
+			public String getMessagePattern() {
+				return "message";
+			}
+			
+			public String getId() {
+				return null;
+			}
+			
+			public EvaluationMode<?> getEvaluationMode() {
+				return EvaluationMode.BATCH;
+			}
+			
+			public String getDescription() {
+				return "description";
+			}
+			
+			public Set<Category> getCategories() {
+				return null;
+			}
+			
+			public String getBody() {
+				return null;
+			}
+			
+			public void addCategory(Category category) {
+			}
+		};
+	}
+}
