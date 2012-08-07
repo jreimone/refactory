b9ebe9bc47ee77a26ce8d15da723eefed57687d2
Jan <jreimone@users.noreply.github.com>
2012-08-07 11:15:09 +0000

    simple tranasformation

--- Refactoring/Tests/org.emftext.refactoring.matching/src/org/emftext/refactoring/matching/Conversion.java
+++ Refactoring/Tests/org.emftext.refactoring.matching/src/org/emftext/refactoring/matching/Conversion.java
@@ -1,6 +1,10 @@
 package org.emftext.refactoring.matching;
 
+import static org.junit.Assert.*;
+
 import java.io.File;
+import java.io.IOException;
+import java.util.Collections;
 import java.util.Map;
 
 import org.eclipse.emf.common.util.URI;
@@ -10,72 +14,67 @@ import org.eclipse.emf.ecore.EPackage.Registry;
 import org.eclipse.emf.ecore.resource.Resource;
 import org.eclipse.emf.ecore.resource.ResourceSet;
 import org.eclipse.emf.ecore.resource.impl.ResourceSetImpl;
+import org.emftext.language.refactoring.roles.Role;
 import org.emftext.language.refactoring.roles.RoleModel;
 import org.emftext.language.refactoring.roles.RolesPackage;
 import org.emftext.language.refactoring.roles.resource.rolestext.mopp.RolestextResourceFactory;
 import org.junit.Test;
+import org.qualitune.guery.GueryFactory;
 import org.qualitune.guery.GueryPackage;
+import org.qualitune.guery.Motif;
+import org.qualitune.guery.MotifModel;
+import org.qualitune.guery.VertexSelection;
 import org.qualitune.guery.resource.guery.mopp.GueryResourceFactory;
 
 public class Conversion {
 
-	/**
-	 * @param args
-	 */
-
-	public static void main(String[] args) {
-		
-		// Register the appropriate resource factory to handle all file extensions.
-		//
-//		resourceSet.getResourceFactoryRegistry().getExtensionToFactoryMap().put
-//		("rolestext", new RolestextResourceFactory());
-//		
-//		resourceSet.getResourceFactoryRegistry().getExtensionToFactoryMap().put
-//		("guery", new GueryResourceFactory());
-		
-		// Register the package to ensure it is available during loading.
-		//
-		Registry registry = EPackage.Registry.INSTANCE;
-		registry.put(RolesPackage.eNS_URI, RolesPackage.eINSTANCE);
-		Map<String, Object> extensionToFactoryMap = Resource.Factory.Registry.INSTANCE.getExtensionToFactoryMap();
-		extensionToFactoryMap.put("rolestext", new RolestextResourceFactory());
-		EPackage.Registry.INSTANCE.put(GueryPackage.eNS_URI, GueryPackage.eINSTANCE);
-		extensionToFactoryMap.put("guery", new GueryResourceFactory());
-		
-//		resourceSet.getPackageRegistry().put
-//		(RolesPackage.eNS_URI, RolesPackage.eINSTANCE);
-//		resourceSet.getPackageRegistry().put
-//		(GueryPackage.eNS_URI, GueryPackage.eINSTANCE);
-		
-		File sourceFile  =new File("C:\\Users\\Robert\\gbswt\\org.emftext.refactoring.extractX\\rolemodel\\ExtractX.rolestext");
-		URI uri = URI.createURI(sourceFile.getAbsolutePath());
-		ResourceSet resourceSet = new ResourceSetImpl();
-		Resource resource = resourceSet.getResource(uri, true);
-		EObject model = resource.getContents().get(0);
-		RoleModel rolemodel = (RoleModel) model;
-		System.out.println(rolemodel.getName());
-	}
-	
 	@Test
-	public void testLoadng(){
-//		Registry registry = EPackage.Registry.INSTANCE;
-//		registry.put(RolesPackage.eNS_URI, RolesPackage.eINSTANCE);
-//		Map<String, Object> extensionToFactoryMap = Resource.Factory.Registry.INSTANCE.getExtensionToFactoryMap();
-//		extensionToFactoryMap.put("rolestext", new RolestextResourceFactory());
-//		EPackage.Registry.INSTANCE.put(GueryPackage.eNS_URI, GueryPackage.eINSTANCE);
-//		extensionToFactoryMap.put("guery", new GueryResourceFactory());
-		
+	public void testTransformationSimple(){
 		ResourceSet resourceSet = new ResourceSetImpl();
 		resourceSet.getPackageRegistry().put(RolesPackage.eNS_URI, RolesPackage.eINSTANCE);
 		resourceSet.getPackageRegistry().put(GueryPackage.eNS_URI, GueryPackage.eINSTANCE);
 		resourceSet.getResourceFactoryRegistry().getExtensionToFactoryMap().put("rolestext", new RolestextResourceFactory());
 		resourceSet.getResourceFactoryRegistry().getExtensionToFactoryMap().put("guery", new GueryResourceFactory());
-		
-		File sourceFile  =new File("C:\\Users\\Robert\\gbswt\\org.emftext.refactoring.extractX\\rolemodel\\ExtractX.rolestext");
-		URI uri = URI.createURI(sourceFile.getAbsolutePath());
+
+		File sourceFile = new File("..\\org.emftext.refactoring.extractX\\rolemodel\\ExtractX.rolestext");
+		assertTrue(sourceFile.exists());
+		System.out.println(sourceFile.getAbsolutePath());
+		URI uri = URI.createFileURI(sourceFile.getAbsolutePath());
 		Resource resource = resourceSet.getResource(uri, true);
 		EObject model = resource.getContents().get(0);
 		RoleModel rolemodel = (RoleModel) model;
-		System.out.println(rolemodel.getName());
+
+		// transformation
+		MotifModel motifModel = GueryFactory.eINSTANCE.createMotifModel();
+		Motif motif = GueryFactory.eINSTANCE.createMotif();
+		motifModel.getMotifs().add(motif);
+		motif.setName(rolemodel.getName());
+		VertexSelection vertexSelection = GueryFactory.eINSTANCE.createVertexSelection();
+		motif.setVertexSelection(vertexSelection);
+		for (Role role : rolemodel.getRoles()) {
+			org.qualitune.guery.Role gueryRole = GueryFactory.eINSTANCE.createRole();
+			gueryRole.setName(role.getName());
+			vertexSelection.getRoles().add(gueryRole);
+			// usw.
+		}
+		File targetFile = new File(rolemodel.getName() + ".guery");
+		if(targetFile.exists()){
+			targetFile.delete();
+		} 
+		try {
+			targetFile.createNewFile();
+		} catch (IOException e) {
+			e.printStackTrace();
+		}
+		assertTrue(targetFile.exists());
+		URI targetUri = URI.createFileURI(targetFile.getAbsolutePath());
+		Resource targetResource = resourceSet.createResource(targetUri);
+		assertNotNull(targetResource);
+		targetResource.getContents().add(motifModel);
+		try {
+			targetResource.save(Collections.EMPTY_MAP);
+		} catch (IOException e) {
+			e.printStackTrace();
+		}
 	}
 }
