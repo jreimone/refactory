e65ec5e3ece318afb7b2ab0b172be4fec1606ff5
Jan <jreimone@users.noreply.github.com>
2012-08-29 13:10:58 +0000

    Bugfixes

---
+++ smells/org.emftext.refactoring.smell.core/src/org/emftext/refactoring/smell/smell_model/ui/View_Quickfix.java
@@ -0,0 +1,28 @@
+package org.emftext.refactoring.smell.smell_model.ui;
+
+import org.eclipse.core.resources.IMarker;
+import org.eclipse.core.runtime.CoreException;
+import org.eclipse.ui.IMarkerResolution;
+import org.emftext.refactoring.smell.smell_model.impl.ModelSmellModelImpl;
+
+public class View_Quickfix implements IMarkerResolution {
+	
+	private String label;
+    
+    public View_Quickfix(String label) {
+    	setLabel(label);
+    }
+    
+    public void run(IMarker marker) {
+    	ModelSmellModelImpl.getMain().getQuickfixForMarker(marker).applyChanges();
+    }
+
+	public String getLabel() {
+		return label;
+	}
+
+	public void setLabel(String label) {
+		this.label = label;
+	}
+
+}
