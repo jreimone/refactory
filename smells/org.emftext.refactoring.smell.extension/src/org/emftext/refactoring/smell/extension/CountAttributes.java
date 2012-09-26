4daa10bb0843f51361020501c1569add08c87a22
Jan <jreimone@users.noreply.github.com>
2012-09-26 12:20:47 +0000

    bugfix

--- smells/org.emftext.refactoring.smell.extension/src/org/emftext/refactoring/smell/extension/CountAttributes.java
+++ smells/org.emftext.refactoring.smell.extension/src/org/emftext/refactoring/smell/extension/CountAttributes.java
@@ -9,11 +9,11 @@ import org.eclipse.emf.ecore.EObject;
 import org.eclipse.emf.ecore.EPackage;
 import org.eclipse.emf.ecore.impl.EObjectImpl;
 import org.eclipse.emf.ecore.resource.Resource;
-import org.emftext.refactoring.smell.smell_model.Metric;
+import org.emftext.refactoring.smell.smell_model.ModelMetric;
 
 
 
-public class CountAttributes extends EObjectImpl implements Metric {
+public class CountAttributes extends EObjectImpl implements ModelMetric {
 	
 	private String name;
 
@@ -35,7 +35,12 @@ public class CountAttributes extends EObjectImpl implements Metric {
 	public Map<EObject, Float> calculate(Resource loadedResource) {
 		Map<EObject, Float> map = new HashMap<EObject, Float>();
 		Float f = 0.0f;
-		EPackage epackage = (EPackage) loadedResource.getContents().get(0);
+		EPackage epackage = null;
+		if (loadedResource.getContents().get(0) instanceof org.eclipse.gmf.runtime.notation.impl.DiagramImpl){
+			epackage = (EPackage) ((org.eclipse.gmf.runtime.notation.impl.DiagramImpl) loadedResource.getContents().get(0)).getElement();
+		} else {
+			epackage = (EPackage) loadedResource.getContents().get(0);
+		}
 		List<EClassifier> classifiers = epackage.getEClassifiers();
 		for (EClassifier classifier : classifiers) {
 			if(classifier instanceof EClass){
