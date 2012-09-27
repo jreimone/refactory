3390be68b7b9279530ffd83032273637cd2dcac1
Jan <jreimone@users.noreply.github.com>
2012-09-27 13:19:37 +0000

    rename new metric added bugfixes

--- smells/org.emftext.refactoring.smell.extension/src/org/emftext/refactoring/smell/extension/CountAttributesOfMethods.java
+++ smells/org.emftext.refactoring.smell.extension/src/org/emftext/refactoring/smell/extension/CountAttributesOfMethods.java
@@ -1,24 +1,26 @@
 package org.emftext.refactoring.smell.extension;
-
 import java.util.ArrayList;
 import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
 
-import org.eclipse.emf.ecore.EClassifier;
 import org.eclipse.emf.ecore.EObject;
+import org.eclipse.emf.ecore.EOperation;
 import org.eclipse.emf.ecore.EPackage;
+import org.eclipse.emf.ecore.impl.EClassImpl;
 import org.eclipse.emf.ecore.impl.EObjectImpl;
 import org.eclipse.emf.ecore.resource.Resource;
 import org.emftext.refactoring.smell.smell_model.ModelMetric;
 
-public class CompareNamesOfClasses extends EObjectImpl implements ModelMetric {
+
+
+public class CountAttributesOfMethods extends EObjectImpl implements ModelMetric {
 	
 	private String name;
 	private List<EPackage> list;
-	
-	public CompareNamesOfClasses(){
-		setName("CompareNamesOfClasses");
+
+	public CountAttributesOfMethods() {
+		setName("CountAttributesOfMethods");
 		list = new ArrayList<EPackage>();
 	}
 
@@ -28,31 +30,34 @@ public class CompareNamesOfClasses extends EObjectImpl implements ModelMetric {
 	}
 
 	@Override
-	public void setName(String value) {
-		this.name = value;
+	public void setName(String name) {
+		this.name = name;
 	}
 
 	@Override
 	public Map<EObject, Float> calculate(Resource loadedResource) {
 		Map<EObject, Float> map = new HashMap<EObject, Float>();
+		Float f = 0.0f;
 		EPackage epackage = null;
 		if (loadedResource.getContents().get(0) instanceof org.eclipse.gmf.runtime.notation.impl.DiagramImpl){
 			epackage = (EPackage) ((org.eclipse.gmf.runtime.notation.impl.DiagramImpl) loadedResource.getContents().get(0)).getElement();
 		} else {
 			epackage = (EPackage) loadedResource.getContents().get(0);
 		}
-		if (epackage != null) {
+		if (epackage != null){
 			getList().add(epackage);
 			walkPackages(epackage.getESubpackages());
-			for (EPackage p : list) {
-				for (EClassifier c : p.getEClassifiers()){
-					for (EPackage p2 : list) {
-						for (EClassifier c2 : p2.getEClassifiers()){
-							if (!p.equals(p2) && !c.equals(c2) && c.getName().equals(c2.getName())){
-								if (!map.containsKey(c) && !map.containsKey(c2)){
-									map.put(c2, 1.0f);
-								}
+			for (EPackage ep : list) {
+				List<EObject> contents = ep.eContents();
+				for (EObject eo : contents) {
+					if (eo instanceof EClassImpl) {
+						for (EOperation eop : ((EClassImpl) eo).getEAllOperations()){
+							f = (float) eop.getEParameters().size();
+							f = f/5;
+							if (f > 1.0f){
+								f = 1.0f;
 							}
+							map.put(eop, f);
 						}
 					}
 				}
