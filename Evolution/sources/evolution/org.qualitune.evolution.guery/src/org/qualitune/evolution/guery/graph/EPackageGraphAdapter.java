2c08bc660cbcd9dd3c883ad5f5209b2377813129
Jan <jreimone@users.noreply.github.com>
2012-10-09 06:59:45 +0000

    Loesung Vererbungsproblem durch Duplizieren

--- Evolution/sources/evolution/org.qualitune.evolution.guery/src/org/qualitune/evolution/guery/graph/EPackageGraphAdapter.java
+++ Evolution/sources/evolution/org.qualitune.evolution.guery/src/org/qualitune/evolution/guery/graph/EPackageGraphAdapter.java
@@ -3,6 +3,7 @@
  */
 package org.qualitune.evolution.guery.graph;
 
+import java.util.Collection;
 import java.util.HashMap;
 import java.util.HashSet;
 import java.util.List;
@@ -21,13 +22,14 @@ import org.qualitune.evolution.guery.registry.EReferenceEdge;
 import edu.uci.ics.jung.graph.util.EdgeType;
 
 /**
- * @author jreimann
+ * @author jreimann, rhunger
  *
  */
-public class EPackageGraphAdapter extends EMFGraphAdapter<MetamodelVertex, EReferenceEdge> {
+public class EPackageGraphAdapter/*<Vertex extends EObjectVertex, Edge extends EReferenceEdge>*/ extends EMFGraphAdapter<MetamodelVertex, EReferenceEdge> {
 
 	private Map<EObject, EClassVertex> index;
 	private Map<EReference, Set<EReferenceEdge>> referenceIndex;
+	private Map<EClassVertex, Collection<EReferenceEdge>> targetToSource;
 	
 	public EPackageGraphAdapter(Resource resource) {
 		super(resource, new MetamodelGraphAdapterFactory());
@@ -37,6 +39,7 @@ public class EPackageGraphAdapter extends EMFGraphAdapter<MetamodelVertex, ERefe
 	protected void initialiseGraph() {
 		index = new HashMap<EObject, EClassVertex>();
 		referenceIndex = new HashMap<EReference, Set<EReferenceEdge>>();
+		targetToSource = new HashMap<EClassVertex, Collection<EReferenceEdge>>();
 		super.initialiseGraph();
 	}
 
@@ -55,6 +58,43 @@ public class EPackageGraphAdapter extends EMFGraphAdapter<MetamodelVertex, ERefe
 				}
 			}
 		}
+		createInheritedReferences();
+	}
+	
+	/**
+	 * Loest Vererbungsproblem
+	 * eingehende Referenzen der Oberklasse werden in Unterklasse geschrieben
+	 */
+	private void createInheritedReferences(){
+		List<EObject> contents = getResource().getContents();
+		for (EObject model : contents) {
+			if(model instanceof EPackage){
+				EPackage epackage = (EPackage) model;
+				EList<EClassifier> classifiers = epackage.getEClassifiers();
+				for (EClassifier classifier : classifiers) {
+					if(classifier instanceof EClass){
+						EClassVertex classVertexSub = (EClassVertex) getFactory().createVertex(classifier);
+						EList<EClass> allSuperTypes=((EClass) classifier).getEAllSuperTypes();
+						for (EClass clazz:allSuperTypes){
+							EClassVertex classVertexSuper = (EClassVertex) getFactory().createVertex(clazz);
+							if (targetToSource.containsKey(classVertexSuper)){
+								if (!targetToSource.get(classVertexSuper).isEmpty()){
+									Collection<EReferenceEdge> references=targetToSource.get(classVertexSuper);
+									for (EReferenceEdge reference:references){
+										EClass eclass=(EClass)reference.getStart().getModelElement();
+										EClassVertex classVertexSource = (EClassVertex) getFactory().createVertex(eclass);
+										EdgeType edgeType = EdgeType.DIRECTED;
+										reference.setEnd(classVertexSub);
+										EReferenceEdge newEdge = getFactory().createEdge(classVertexSource, classVertexSub, reference.getReference());
+										getGraph().addEdge(newEdge, classVertexSource, classVertexSub, edgeType);
+									}
+								}
+							}
+						}
+					}
+				}
+			}
+		}
 	}
 
 	@Override
@@ -67,17 +107,27 @@ public class EPackageGraphAdapter extends EMFGraphAdapter<MetamodelVertex, ERefe
 				EClass eClass = eclassVertex.getEClass();
 				EdgeType edgeType = EdgeType.DIRECTED;
 				List<EReference> references = eClass.getEAllReferences();
-				for (EReference reference : references) {
+				for (EReference reference : references) { //enthaelt Referenzen von EClass mit namen
 					EClassifier targetClassifier = reference.getEType();
 					if(targetClassifier instanceof EClass){
 						EClass targetClass = (EClass) targetClassifier;
 						EClassVertex targetVertex = (EClassVertex) getFactory().createVertex(targetClass);
 						if(index.containsKey(targetVertex.getModelElement())){
-							targetVertex = index.get(targetVertex.getModelElement());
+							targetVertex = index.get(targetVertex.getModelElement()); //Testet ob targetEClass vorhanden
 						} 
 						EReferenceEdge newEdge = getFactory().createEdge(eclassVertex, targetVertex, reference);
 						if(referenceIndex.get(reference) == null){
 							getGraph().addEdge(newEdge, eclassVertex, targetVertex, edgeType);
+							if (targetToSource.containsKey(targetVertex)){
+								if(!targetToSource.get(targetVertex).contains(newEdge)){
+									targetToSource.get(targetVertex).add(newEdge);
+								}
+							}
+							else{
+								Set<EReferenceEdge> edges = new HashSet<EReferenceEdge>();
+								edges.add(newEdge);
+								targetToSource.put(targetVertex, edges);
+							}
 							Set<EReferenceEdge> edges = new HashSet<EReferenceEdge>();
 							edges.add(newEdge);
 							referenceIndex.put(reference, edges);
@@ -92,6 +142,16 @@ public class EPackageGraphAdapter extends EMFGraphAdapter<MetamodelVertex, ERefe
 							if(!found){
 								edges.add(newEdge);
 								getGraph().addEdge(newEdge, eclassVertex, targetVertex, edgeType);
+								if (targetToSource.containsKey(targetVertex)){
+									if(!targetToSource.get(targetVertex).contains(newEdge)){
+										targetToSource.get(targetVertex).add(newEdge);
+									}
+								}
+								else{
+									Set<EReferenceEdge> edgesToAdd = new HashSet<EReferenceEdge>();
+									edgesToAdd.add(newEdge);
+									targetToSource.put(targetVertex, edgesToAdd);
+								}
 							}
 						}
 						addNode(targetVertex);
