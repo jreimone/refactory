74e1c6aa6a2d93673863dfaf4cd5f8ef982c7fcd
jreimann <jreimann@2290cd21-d4b4-4435-9c65-06e28a4c4f12>
2012-12-05 10:33:19 +0000

    better "data transmission without compression"

--- trunk/Evolution/sources/evolution/org.qualitune.refactoring.energy.test/src/org/qualitune/refactoring/energy/test/DataTransmissionWithoutCompression.java
+++ trunk/Evolution/sources/evolution/org.qualitune.refactoring.energy.test/src/org/qualitune/refactoring/energy/test/DataTransmissionWithoutCompression.java
@@ -60,6 +60,42 @@ public class DataTransmissionWithoutCompression {
 		}
 
 	}
+	
+	public static void transmitData(String[] args) throws Exception {
+		if (args.length != 1)  {
+			System.out.println("File path not given");
+			System.exit(1);
+		}
+		HttpClient httpclient = new DefaultHttpClient();
+		try {
+
+			HttpPost httppost = new HttpPost("http://some.url:8080/servlets-examples/servlet/RequestInfoExample");
+			File file = new File(args[0]);
+//			FileBody fileBody = new FileBody(new File(args[0]));
+			FileBody fileBody = new FileBody(file);
+			StringBody comment = new StringBody("A binary file of some kind");
+
+			MultipartEntity reqEntity = new MultipartEntity();
+			reqEntity.addPart("bin", fileBody);
+			reqEntity.addPart("comment", comment);
+
+			httppost.setEntity(reqEntity);
+
+			System.out.println("executing request " + httppost.getRequestLine());
+			HttpResponse response = httpclient.execute(httppost);
+			HttpEntity resEntity = response.getEntity();
+
+			System.out.println("----------------------------------------");
+			System.out.println(response.getStatusLine());
+			if (resEntity != null) {
+				System.out.println("Response content length: " + resEntity.getContentLength());
+			}
+			EntityUtils.consume(resEntity);
+		} finally {
+			try { httpclient.getConnectionManager().shutdown(); } catch (Exception ignore) {}
+		}
+
+	}
 
 	private static File gzipFile(File uncompressedFile){
 		try {
