aa5300cb4412621b7626cd84282bdc96dc954e21
jreimann <jreimann@2290cd21-d4b4-4435-9c65-06e28a4c4f12>
2013-02-04 13:37:06 +0000

    persist with relative path

--- trunk/Lässig/dk.itu.sdg.tracemodel.printer/src/dk/itu/sdg/tracemodel/printer/Printer.java
+++ trunk/Lässig/dk.itu.sdg.tracemodel.printer/src/dk/itu/sdg/tracemodel/printer/Printer.java
@@ -10,10 +10,9 @@ import org.eclipse.emf.ecore.resource.ResourceSet;
 import org.eclipse.emf.ecore.resource.impl.ResourceSetImpl;
 import org.eclipse.emf.ecore.util.EcoreUtil;
 import org.eclipse.emf.ecore.xmi.impl.XMIResourceFactoryImpl;
+import org.osgi.framework.Bundle;
 
 import SimpleTrace.SimpleTraceFactory;
-import SimpleTrace.SimpleTracePackage;
-import SimpleTrace.impl.SimpleTraceFactoryImpl;
 import dk.itu.sdg.tracemodel.runtimedata.DirectedTrace;
 import dk.itu.sdg.tracemodel.runtimedata.Trace;
 import dk.itu.sdg.tracemodel.runtimedata.TraceCollector;
@@ -27,67 +26,76 @@ public class Printer {
 			System.out.println("trace " + EcoreUtil.getURI((EObject)trace.getLeftHand()) + " <--> " + EcoreUtil.getURI((EObject)trace.getRightHand()) + " by " +  trace.getJoinPoint().getSignature().toString() + " in " + trace.getJoinPoint().getSignature().getDeclaringType().toString());
 		}
 	}
-	
+
 	public void structuredToConsole() {
-		
+
 		for (Class clazz : TraceCollector.getInstance().getTracesPerClass().keySet()) {
 			Set<Trace> traces = TraceCollector.getInstance().getTracesPerClass().get(clazz);
-			
+
 			for (Trace trace : traces) {
 				System.out.println("trace " + EcoreUtil.getURI((EObject)trace.getLeftHand()) + " <--> " + EcoreUtil.getURI((EObject)trace.getRightHand()) + " by " +  trace.getJoinPoint().getSignature().toString() + " in " + trace.getJoinPoint().getSignature().getDeclaringType().toString());				
 			}
 		}
 	}
-	
+
 	public void toTraceModel() {
-		
-		String tracemodelPath = "/Users/ropf/Documents/eclipse/juno_dresden/dk.itu.sdg.tracemodel.printer/output/tracemodel.model";
-				
+
+		String tracemodelPath = "output/tracemodel.model";
+
+		Bundle bundle = Activator.getDefault().getBundle();
+
+		String loc1 = bundle.getLocation();
+		String pluginPath = loc1.replace("reference:file:/", "");
+		URI uri = URI.createFileURI(pluginPath + tracemodelPath);
+
 		Resource.Factory.Registry.INSTANCE.getExtensionToFactoryMap().put("model", new XMIResourceFactoryImpl());
 		ResourceSet rs = new ResourceSetImpl();
-		Resource resource = rs.createResource(URI.createURI(tracemodelPath));
-		
-		SimpleTracePackage tracemodelPackageInstance = SimpleTracePackage.eINSTANCE;
-		SimpleTraceFactory factory = new SimpleTraceFactoryImpl();
+		Resource resource = rs.createResource(uri);
 
-		
+		SimpleTraceFactory factory = SimpleTraceFactory.eINSTANCE;
+
+
 		for (Set tracesPerRealTrace : TraceCollector.getInstance().getRealTraces()) {
-			
+
 			SimpleTrace.Trace modelTrace = factory.createTrace();
-			
+
 			for (Object trace : tracesPerRealTrace) {
-				
+
 				SimpleTrace.TraceLink modelTraceLink = factory.createTraceLink();
 				modelTraceLink.getSources().add((EObject)((Trace)trace).getLeftHand());
 				modelTraceLink.getTargets().add((EObject)((Trace)trace).getRightHand());
 				modelTraceLink.setDescription("D: Transformed by " +  ((Trace)trace).getJoinPoint().getSignature().toString() + " in " + ((Trace)trace).getJoinPoint().getSourceLocation().toString());
 				modelTrace.getLinks().add(modelTraceLink);
 			}
-			
+
 			resource.getContents().add(modelTrace);
 		}
 		try {
 			resource.save(System.out, null);
-//			resource.save(null);
+			// resource.save(null);
 		} catch (IOException e) {
 			e.printStackTrace();
 		}
 	}
-	
+
 	public void toFlatTraceModel() {
-		
-		String tracemodelPath = "/Users/ropf/Documents/eclipse/juno_dresden/dk.itu.sdg.tracemodel.printer/output/tracemodel.model";
-				
+
+		String tracemodelPath = "output/tracemodel.model";
+		Bundle bundle = Activator.getDefault().getBundle();
+
+		String loc1 = bundle.getLocation();
+		String pluginPath = loc1.replace("reference:file:/", "");
+		URI uri = URI.createFileURI(pluginPath + tracemodelPath);
+
 		Resource.Factory.Registry.INSTANCE.getExtensionToFactoryMap().put("model", new XMIResourceFactoryImpl());
 		ResourceSet rs = new ResourceSetImpl();
-		Resource resource = rs.createResource(URI.createURI(tracemodelPath));
-		
-		SimpleTracePackage tracemodelPackageInstance = SimpleTracePackage.eINSTANCE;
-		SimpleTraceFactory factory = new SimpleTraceFactoryImpl();
+		Resource resource = rs.createResource(uri);
+
+		SimpleTraceFactory factory = SimpleTraceFactory.eINSTANCE;
 
 		SimpleTrace.Trace modelTrace = factory.createTrace();
 		for (Trace trace : TraceCollector.getInstance().getTraces()) {
-							
+
 			SimpleTrace.TraceLink traceLink = factory.createTraceLink();
 			traceLink.getSources().add((EObject)((Trace)trace).getLeftHand());
 			traceLink.getTargets().add((EObject)((Trace)trace).getRightHand());
@@ -97,12 +105,12 @@ public class Printer {
 				traceLink.setDescription("<-> Transformed by " +  ((Trace)trace).getJoinPoint().getSignature().toString() + " in " + ((Trace)trace).getJoinPoint().getSourceLocation().toString());
 			}
 			modelTrace.getLinks().add(traceLink);
-			
+
 			resource.getContents().add(modelTrace);
 		}
 		try {
 			resource.save(System.out, null);
-//			resource.save(null);
+			// resource.save(null);
 		} catch (IOException e) {
 			e.printStackTrace();
 		}
