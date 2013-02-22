22c0465562ad0818dabb3a74a378231a4e088293
jreimann <jreimann@2290cd21-d4b4-4435-9c65-06e28a4c4f12>
2013-02-22 16:37:00 +0000

    incquery patterns can be registered as smells

--- trunk/smells/org.emftext.refactoring.smell.calculation/src-gen/org/emftext/refactoring/smell/calculation/Structure.java
+++ trunk/smells/org.emftext.refactoring.smell.calculation/src-gen/org/emftext/refactoring/smell/calculation/Structure.java
@@ -2,16 +2,59 @@
  */
 package org.emftext.refactoring.smell.calculation;
 
+import org.eclipse.emf.ecore.EObject;
+import org.eclipse.viatra2.patternlanguage.core.patternLanguage.Pattern;
+
 
 /**
  * <!-- begin-user-doc -->
  * A representation of the model object '<em><b>Structure</b></em>'.
  * <!-- end-user-doc -->
  *
+ * <p>
+ * The following features are supported:
+ * <ul>
+ *   <li>{@link org.emftext.refactoring.smell.calculation.Structure#getPattern <em>Pattern</em>}</li>
+ * </ul>
+ * </p>
  *
  * @see org.emftext.refactoring.smell.calculation.CalculationPackage#getStructure()
  * @model
  * @generated
  */
 public interface Structure extends Calculation {
+
+	/**
+	 * Returns the value of the '<em><b>Pattern</b></em>' reference.
+	 * <!-- begin-user-doc -->
+	 * <p>
+	 * If the meaning of the '<em>Pattern</em>' reference isn't clear,
+	 * there really should be more of a description here...
+	 * </p>
+	 * <!-- end-user-doc -->
+	 * @return the value of the '<em>Pattern</em>' reference.
+	 * @see #setPattern(Pattern)
+	 * @see org.emftext.refactoring.smell.calculation.CalculationPackage#getStructure_Pattern()
+	 * @model
+	 * @generated
+	 */
+	Pattern getPattern();
+
+	/**
+	 * Sets the value of the '{@link org.emftext.refactoring.smell.calculation.Structure#getPattern <em>Pattern</em>}' reference.
+	 * <!-- begin-user-doc -->
+	 * <!-- end-user-doc -->
+	 * @param value the new value of the '<em>Pattern</em>' reference.
+	 * @see #getPattern()
+	 * @generated
+	 */
+	void setPattern(Pattern value);
+
+	/**
+	 * <!-- begin-user-doc -->
+	 * <!-- end-user-doc -->
+	 * @model
+	 * @generated
+	 */
+	CalculationResult calculate(EObject model, float threshold);
 } // Structure
--- trunk/smells/org.emftext.refactoring.smell.calculation/src-gen/org/emftext/refactoring/smell/calculation/impl/StructureImpl.java
+++ trunk/smells/org.emftext.refactoring.smell.calculation/src-gen/org/emftext/refactoring/smell/calculation/impl/StructureImpl.java
@@ -2,8 +2,12 @@
  */
 package org.emftext.refactoring.smell.calculation.impl;
 
+import org.eclipse.emf.common.notify.Notification;
 import org.eclipse.emf.ecore.EClass;
 
+import org.eclipse.emf.ecore.InternalEObject;
+import org.eclipse.emf.ecore.impl.ENotificationImpl;
+import org.eclipse.viatra2.patternlanguage.core.patternLanguage.Pattern;
 import org.emftext.refactoring.smell.calculation.CalculationPackage;
 import org.emftext.refactoring.smell.calculation.Structure;
 
@@ -12,11 +16,25 @@ import org.emftext.refactoring.smell.calculation.Structure;
  * An implementation of the model object '<em><b>Structure</b></em>'.
  * <!-- end-user-doc -->
  * <p>
+ * The following features are implemented:
+ * <ul>
+ *   <li>{@link org.emftext.refactoring.smell.calculation.impl.StructureImpl#getPattern <em>Pattern</em>}</li>
+ * </ul>
  * </p>
  *
  * @generated
  */
 public class StructureImpl extends CalculationImpl implements Structure {
+	/**
+	 * The cached value of the '{@link #getPattern() <em>Pattern</em>}' reference.
+	 * <!-- begin-user-doc -->
+	 * <!-- end-user-doc -->
+	 * @see #getPattern()
+	 * @generated
+	 * @ordered
+	 */
+	protected Pattern pattern;
+
 	/**
 	 * <!-- begin-user-doc -->
 	 * <!-- end-user-doc -->
@@ -34,6 +52,103 @@ public class StructureImpl extends CalculationImpl implements Structure {
 	@Override
 	protected EClass eStaticClass() {
 		return CalculationPackage.Literals.STRUCTURE;
+	}
+
+	/**
+	 * <!-- begin-user-doc -->
+	 * <!-- end-user-doc -->
+	 * @generated
+	 */
+	public Pattern getPattern() {
+		if (pattern != null && pattern.eIsProxy()) {
+			InternalEObject oldPattern = (InternalEObject)pattern;
+			pattern = (Pattern)eResolveProxy(oldPattern);
+			if (pattern != oldPattern) {
+				if (eNotificationRequired())
+					eNotify(new ENotificationImpl(this, Notification.RESOLVE, CalculationPackage.STRUCTURE__PATTERN, oldPattern, pattern));
+			}
+		}
+		return pattern;
+	}
+
+	/**
+	 * <!-- begin-user-doc -->
+	 * <!-- end-user-doc -->
+	 * @generated
+	 */
+	public Pattern basicGetPattern() {
+		return pattern;
+	}
+
+	/**
+	 * <!-- begin-user-doc -->
+	 * <!-- end-user-doc -->
+	 * @generated
+	 */
+	public void setPattern(Pattern newPattern) {
+		Pattern oldPattern = pattern;
+		pattern = newPattern;
+		if (eNotificationRequired())
+			eNotify(new ENotificationImpl(this, Notification.SET, CalculationPackage.STRUCTURE__PATTERN, oldPattern, pattern));
+	}
+
+	/**
+	 * <!-- begin-user-doc -->
+	 * <!-- end-user-doc -->
+	 * @generated
+	 */
+	@Override
+	public Object eGet(int featureID, boolean resolve, boolean coreType) {
+		switch (featureID) {
+			case CalculationPackage.STRUCTURE__PATTERN:
+				if (resolve) return getPattern();
+				return basicGetPattern();
+		}
+		return super.eGet(featureID, resolve, coreType);
+	}
+
+	/**
+	 * <!-- begin-user-doc -->
+	 * <!-- end-user-doc -->
+	 * @generated
+	 */
+	@Override
+	public void eSet(int featureID, Object newValue) {
+		switch (featureID) {
+			case CalculationPackage.STRUCTURE__PATTERN:
+				setPattern((Pattern)newValue);
+				return;
+		}
+		super.eSet(featureID, newValue);
+	}
+
+	/**
+	 * <!-- begin-user-doc -->
+	 * <!-- end-user-doc -->
+	 * @generated
+	 */
+	@Override
+	public void eUnset(int featureID) {
+		switch (featureID) {
+			case CalculationPackage.STRUCTURE__PATTERN:
+				setPattern((Pattern)null);
+				return;
+		}
+		super.eUnset(featureID);
+	}
+
+	/**
+	 * <!-- begin-user-doc -->
+	 * <!-- end-user-doc -->
+	 * @generated
+	 */
+	@Override
+	public boolean eIsSet(int featureID) {
+		switch (featureID) {
+			case CalculationPackage.STRUCTURE__PATTERN:
+				return pattern != null;
+		}
+		return super.eIsSet(featureID);
 	}
 
 } //StructureImpl
