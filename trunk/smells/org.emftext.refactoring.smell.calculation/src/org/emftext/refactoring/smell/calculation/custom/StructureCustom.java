c4ad27e9b1f3b12cc90732a39cead45f3a12d4e3
jreimann <jreimann@2290cd21-d4b4-4435-9c65-06e28a4c4f12>
2013-02-26 13:45:32 +0000

    first try to call incquery matcher

--- trunk/smells/org.emftext.refactoring.smell.calculation/src/org/emftext/refactoring/smell/calculation/custom/StructureCustom.java
+++ trunk/smells/org.emftext.refactoring.smell.calculation/src/org/emftext/refactoring/smell/calculation/custom/StructureCustom.java
@@ -1,6 +1,19 @@
 package org.emftext.refactoring.smell.calculation.custom;
 
+import java.util.Collection;
+import java.util.Set;
+
 import org.eclipse.emf.ecore.EObject;
+import org.eclipse.emf.ecore.resource.Resource;
+import org.eclipse.emf.ecore.resource.ResourceSet;
+import org.eclipse.incquery.patternlanguage.patternLanguage.Pattern;
+import org.eclipse.incquery.runtime.api.IMatchProcessor;
+import org.eclipse.incquery.runtime.api.IMatcherFactory;
+import org.eclipse.incquery.runtime.api.IPatternMatch;
+import org.eclipse.incquery.runtime.api.IncQueryMatcher;
+import org.eclipse.incquery.runtime.exception.IncQueryException;
+import org.eclipse.incquery.runtime.extensibility.MatcherFactoryRegistry;
+import org.emftext.refactoring.smell.calculation.CalculationFactory;
 import org.emftext.refactoring.smell.calculation.CalculationResult;
 import org.emftext.refactoring.smell.calculation.Monotonicity;
 import org.emftext.refactoring.smell.calculation.impl.StructureImpl;
@@ -13,6 +26,34 @@ public class StructureCustom extends StructureImpl {
 	}
 	
 	public CalculationResult calculate(EObject model, float threshold){
-		return null;
+		CalculationResult result = CalculationFactory.eINSTANCE.createCalculationResult();
+		Pattern pattern = getPattern();
+		Resource resource = model.eResource();
+		if(resource != null){
+			ResourceSet rs = resource.getResourceSet();
+			@SuppressWarnings("unchecked")
+			IMatcherFactory<IncQueryMatcher<IPatternMatch>> matcherFactory = (IMatcherFactory<IncQueryMatcher<IPatternMatch>>) MatcherFactoryRegistry.getOrCreateMatcherFactory(pattern);
+			try {
+				final IncQueryMatcher<IPatternMatch> matcher = matcherFactory.getMatcher(rs);
+				String[] parameterNames = matcher.getParameterNames();
+				matcher.forEachMatch(new IMatchProcessor<IPatternMatch>() {
+					@Override
+					public void process(IPatternMatch match) {
+						String[] parameterNames2 = match.parameterNames();
+						for (String name : parameterNames2) {
+							Set<Object> values = matcher.getAllValues(name);
+							Object object = match.get(name);
+							System.out.println(match.prettyPrint());
+						}
+						
+					}
+				});
+				Collection<IPatternMatch> matches = matcher.getAllMatches();
+				
+			} catch (IncQueryException e) {
+				e.printStackTrace();
+			}
+		}
+		return result;
 	}
 }
