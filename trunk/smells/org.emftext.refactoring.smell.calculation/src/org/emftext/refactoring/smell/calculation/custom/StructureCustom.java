d24fd710696d9104345d190df9947867e5a20521
jreimann <jreimann@2290cd21-d4b4-4435-9c65-06e28a4c4f12>
2013-03-28 09:11:59 +0000

    resourceset instead of resource

--- trunk/smells/org.emftext.refactoring.smell.calculation/src/org/emftext/refactoring/smell/calculation/custom/StructureCustom.java
+++ trunk/smells/org.emftext.refactoring.smell.calculation/src/org/emftext/refactoring/smell/calculation/custom/StructureCustom.java
@@ -4,6 +4,7 @@ import java.util.Collection;
 
 import org.eclipse.emf.ecore.EObject;
 import org.eclipse.emf.ecore.resource.Resource;
+import org.eclipse.emf.ecore.resource.ResourceSet;
 import org.eclipse.incquery.patternlanguage.patternLanguage.Pattern;
 import org.eclipse.incquery.patternlanguage.patternLanguage.Variable;
 import org.eclipse.incquery.runtime.api.IMatchProcessor;
@@ -32,44 +33,31 @@ public class StructureCustom extends StructureImpl {
 		Pattern pattern = getPattern();
 		Resource resource = model.eResource();
 		if(resource != null){
-			try {
-				@SuppressWarnings("unchecked")
-				IMatcherFactory<IncQueryMatcher<IPatternMatch>> matcherFactory = (IMatcherFactory<IncQueryMatcher<IPatternMatch>>) MatcherFactoryRegistry.getOrCreateMatcherFactory(pattern);
-				if(matcherFactory != null){
-					final IncQueryMatcher<IPatternMatch> matcher = matcherFactory.getMatcher(resource);
-					matcher.forEachMatch(new IMatchProcessor<IPatternMatch>() {
-
-						@Override
-						public void process(IPatternMatch match) {
-							System.out.println();
-						}
-					});
-					
-					String[] parameterNames2 = matcher.getParameterNames();
-					for (String string : parameterNames2) {
-						int index = matcher.getPositionOfParameter(string);
-						Variable variable = pattern.getParameters().get(index);
-						IPatternMatch partialMatch = matcher.newMatch(variable);
-						Collection<IPatternMatch> matches = matcher.getAllMatches(partialMatch);
-						System.out.println();
-					}
-					
-					
-					Collection<IPatternMatch> matches = matcher.getAllMatches();
-					for (IPatternMatch match : matches) {
-						String[] parameterNames = match.parameterNames();
-						for (String name : parameterNames) {
-							Object object = match.get(name);
-							if(object instanceof EObject){
-								EObject element = (EObject) object;
-								result.getCausingObjects().add(element);
+			ResourceSet resourceSet = resource.getResourceSet();
+			if(resourceSet != null){
+				try {
+					@SuppressWarnings("unchecked")
+					IMatcherFactory<IncQueryMatcher<IPatternMatch>> matcherFactory = (IMatcherFactory<IncQueryMatcher<IPatternMatch>>) MatcherFactoryRegistry.getOrCreateMatcherFactory(pattern);
+					if(matcherFactory != null){
+						// here the matcher must be retrieved by the resourceSet because passing only a resource
+						// no matches are calculated
+						IncQueryMatcher<IPatternMatch> matcher = matcherFactory.getMatcher(resourceSet);
+						Collection<IPatternMatch> matches = matcher.getAllMatches();
+						for (IPatternMatch match : matches) {
+							String[] parameterNames = match.parameterNames();
+							for (String name : parameterNames) {
+								Object object = match.get(name);
+								if(object instanceof EObject){
+									EObject element = (EObject) object;
+									result.getCausingObjects().add(element);
+								}
 							}
+							result.setResultingValue(result.getResultingValue() + 1);
 						}
-						result.setResultingValue(result.getResultingValue() + 1);
 					}
+				} catch (Exception e) {
+					e.printStackTrace();
 				}
-			} catch (Exception e) {
-				e.printStackTrace();
 			}
 		}
 		return result;
