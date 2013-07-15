3434cd0e0bc1cdfdd4d30c0be4b7f681c6bb0296
jreimann <jreimann@2290cd21-d4b4-4435-9c65-06e28a4c4f12>
2013-07-15 15:40:45 +0000

    reused EMF Compare control for preview :)

--- trunk/Plugins/org.emftext.refactoring.ui/src/org/emftext/refactoring/ltk/ModelChangePreviewer.java
+++ trunk/Plugins/org.emftext.refactoring.ui/src/org/emftext/refactoring/ltk/ModelChangePreviewer.java
@@ -17,38 +17,31 @@ package org.emftext.refactoring.ltk;
 
 import java.lang.reflect.InvocationTargetException;
 
-import org.eclipse.compare.CompareConfiguration;
 import org.eclipse.compare.CompareEditorInput;
-import org.eclipse.compare.CompareUI;
 import org.eclipse.core.runtime.NullProgressMonitor;
 import org.eclipse.emf.common.notify.AdapterFactory;
 import org.eclipse.emf.compare.Comparison;
 import org.eclipse.emf.compare.EMFCompare;
 import org.eclipse.emf.compare.domain.ICompareEditingDomain;
 import org.eclipse.emf.compare.domain.impl.EMFCompareEditingDomain;
-import org.eclipse.emf.compare.ide.ui.internal.editor.ComparisonEditorInput;
 import org.eclipse.emf.ecore.EObject;
 import org.eclipse.emf.edit.provider.ComposedAdapterFactory;
 import org.eclipse.ltk.ui.refactoring.ChangePreviewViewerInput;
 import org.eclipse.ltk.ui.refactoring.IChangePreviewViewer;
-import org.eclipse.swt.SWT;
-import org.eclipse.swt.custom.ViewForm;
 import org.eclipse.swt.widgets.Composite;
 import org.eclipse.swt.widgets.Control;
 
 public class ModelChangePreviewer implements IChangePreviewViewer {
 
-	private ViewForm control;
-//	private RefactoringCompareEditorInput compare;
-	//	private CompareViewerPane pane;
+	private Composite parent;
+	private Control previewControl;
 
 	public void createControl(Composite parent) {
-		control = new ViewForm(parent, SWT.BORDER);
-//		compare = new RefactoringCompareEditorInput(control);
+		this.parent = parent;
 	}
 
 	public Control getControl() {
-		return control;
+		return previewControl;
 	}
 
 	public void setInput(ChangePreviewViewerInput input) {
@@ -58,38 +51,13 @@ public class ModelChangePreviewer implements IChangePreviewViewer {
 		ModelRefactoringChange change = (ModelRefactoringChange) input.getChange();
 		EObject originalModel = change.getRefactorer().getOriginalModel();
 		EObject fakeRefactoredModel = change.getRefactorer().getFakeRefactoredModel();
-		
-//		compare.setChange(change);
-//		compare.setOriginalModel(originalModel);
-//		compare.setFakeRefactoredModel(fakeRefactoredModel);
-//		try {
-//			compare.run(new NullProgressMonitor());
-//		} catch (InterruptedException e) {
-//			e.printStackTrace();
-//		} catch (InvocationTargetException e) {
-//			e.printStackTrace();
-//		}
-		//		compare.getControl().pack(true);
-		//		compare.getControl().setVisible(true);
-		
-//		IEObjectMatcher matcher = DefaultMatchEngine.createDefaultEObjectMatcher(UseIdentifiers.NEVER);
-//		IComparisonFactory comparisonFactory = new DefaultComparisonFactory(new DefaultEqualityHelperFactory());
-//		IMatchEngine.Factory matchEngineFactory = new MatchEngineFactoryImpl(matcher, comparisonFactory);
-//		matchEngineFactory.setRanking(20);
-//		IMatchEngine.Factory.Registry matchEngineRegistry = new MatchEngineFactoryRegistryImpl();
-//		matchEngineRegistry.add(matchEngineFactory);
-//		EMFCompare comparator = EMFCompare.builder().setMatchEngineFactoryRegistry(matchEngineRegistry).build();
-//		IComparisonScope scope = EMFCompare.createDefaultScope(originalModel, fakeRefactoredModel);
-//		Comparison comparison = comparator.compare(scope);
 	    
 		EMFCompare comparator = EMFCompare.builder().build();
 	    Comparison comparison = comparator.compare(EMFCompare.createDefaultScope(originalModel, fakeRefactoredModel));
 		
 		ICompareEditingDomain editingDomain = EMFCompareEditingDomain.create(originalModel, fakeRefactoredModel, null);
 		AdapterFactory adapterFactory = new ComposedAdapterFactory(ComposedAdapterFactory.Descriptor.Registry.INSTANCE);
-	    CompareEditorInput compareEditorInput = new ComparisonEditorInput(new CompareConfiguration(), comparison, editingDomain, adapterFactory);
-//	    CompareUI.openCompareDialog(compareEditorInput);
-	    
+	    CompareEditorInput compareEditorInput = new RefactoringCompareEditorInput(comparison, editingDomain, adapterFactory);
 	    try {
 			compareEditorInput.run(new NullProgressMonitor());
 		} catch (InterruptedException e) {
@@ -98,12 +66,7 @@ public class ModelChangePreviewer implements IChangePreviewViewer {
 			e.printStackTrace();
 		}
 	    
-	    AbstractModelCompareDialog dialog = new AbstractModelCompareDialog(control, compareEditorInput);
-	    Control compareControl = dialog.initializeControl();
-		compareControl.setVisible(true);
-	    
-//		CompareUI.openCompareDialog(compare);
-//		System.out.println();
+	    previewControl = compareEditorInput.createContents(parent);
 	}
 
 }
--- trunk/Plugins/org.emftext.refactoring.ui/src/org/emftext/refactoring/ltk/RefactoringCompareEditorInput.java
+++ trunk/Plugins/org.emftext.refactoring.ui/src/org/emftext/refactoring/ltk/RefactoringCompareEditorInput.java
@@ -15,32 +15,14 @@
  ******************************************************************************/
 package org.emftext.refactoring.ltk;
 
-import java.lang.reflect.InvocationTargetException;
-
 import org.eclipse.compare.CompareConfiguration;
-import org.eclipse.compare.CompareEditorInput;
-import org.eclipse.compare.ITypedElement;
-import org.eclipse.compare.structuremergeviewer.ICompareInput;
-import org.eclipse.compare.structuremergeviewer.ICompareInputChangeListener;
-import org.eclipse.core.runtime.IProgressMonitor;
+import org.eclipse.emf.common.notify.AdapterFactory;
 import org.eclipse.emf.compare.Comparison;
-import org.eclipse.emf.compare.EMFCompare;
-import org.eclipse.emf.compare.match.DefaultComparisonFactory;
-import org.eclipse.emf.compare.match.DefaultEqualityHelperFactory;
-import org.eclipse.emf.compare.match.DefaultMatchEngine;
-import org.eclipse.emf.compare.match.IComparisonFactory;
-import org.eclipse.emf.compare.match.IMatchEngine;
-import org.eclipse.emf.compare.match.eobject.IEObjectMatcher;
-import org.eclipse.emf.compare.match.impl.MatchEngineFactoryImpl;
-import org.eclipse.emf.compare.match.impl.MatchEngineFactoryRegistryImpl;
-import org.eclipse.emf.compare.scope.IComparisonScope;
-import org.eclipse.emf.compare.utils.UseIdentifiers;
-import org.eclipse.emf.ecore.EObject;
-import org.eclipse.swt.graphics.Image;
-import org.eclipse.swt.widgets.Composite;
-import org.eclipse.swt.widgets.Control;
+import org.eclipse.emf.compare.domain.ICompareEditingDomain;
+import org.eclipse.emf.compare.ide.ui.internal.editor.ComparisonEditorInput;
 
-public class RefactoringCompareEditorInput extends CompareEditorInput {
+@SuppressWarnings("restriction")
+public class RefactoringCompareEditorInput extends ComparisonEditorInput {
 
 	private static CompareConfiguration config = new CompareConfiguration();
 
@@ -49,81 +31,7 @@ public class RefactoringCompareEditorInput extends CompareEditorInput {
 		config.setRightEditable(false);
 	}
 
-	private EObject originalModel;
-	private EObject fakeRefactoredModel;
-
-	private Control control;
-	private ModelRefactoringChange change;
-
-	public RefactoringCompareEditorInput(Composite parent) {
-		super(config);
-//		control = super.createContents(parent);
-		//		control.pack();
-		//		control.setVisible(true);
-	}
-
-	@Override
-	protected Object prepareInput(IProgressMonitor monitor) throws InvocationTargetException, InterruptedException {
-		if(originalModel == null){
-			throw new InvocationTargetException(new NullPointerException("Original Model mustn't be null"));
-		}
-		if(fakeRefactoredModel == null){
-			throw new InvocationTargetException(new NullPointerException("Fake Refactored Model mustn't be null"));
-		}
-		// new EMF Compare API
-		IEObjectMatcher matcher = DefaultMatchEngine.createDefaultEObjectMatcher(UseIdentifiers.NEVER);
-		IComparisonFactory comparisonFactory = new DefaultComparisonFactory(new DefaultEqualityHelperFactory());
-//		IMatchEngine matchEngine = new DefaultMatchEngine(matcher, comparisonFactory);
-		//		IMatchEngine.Factory.Registry matchEngineRegistry = EMFCompareRCPPlugin.getDefault().getMatchEngineFactoryRegistry();
-		IMatchEngine.Factory matchEngineFactory = new MatchEngineFactoryImpl(matcher, comparisonFactory);
-		matchEngineFactory.setRanking(20);
-		IMatchEngine.Factory.Registry matchEngineRegistry = new MatchEngineFactoryRegistryImpl();
-		matchEngineRegistry.add(matchEngineFactory);
-		EMFCompare comparator = EMFCompare.builder().setMatchEngineFactoryRegistry(matchEngineRegistry).build();
-		IComparisonScope scope = EMFCompare.createDefaultScope(originalModel, fakeRefactoredModel);
-		Comparison comparison = comparator.compare(scope);
-		return new ModelRefactoringCompareInput(comparison, change);
-		
-		// old EMF Compare API
-		//		Map<String, Object> options = new LinkedHashMap<String, Object>();
-		//		options.put(MatchOptions.OPTION_DISTINCT_METAMODELS, true);
-		//		options.put(MatchOptions.OPTION_IGNORE_ID, true);
-		//		options.put(MatchOptions.OPTION_IGNORE_XMI_ID, true);
-		//		options.put(MatchOptions.OPTION_PROGRESS_MONITOR, monitor);
-		//		MatchModel match = MatchService.doContentMatch(originalModel, fakeRefactoredModel, options);
-		//		DiffModel diff = DiffService.doDiff(match, false);
-		//		
-		//		return diff;
-	}
-
-	public EObject getOriginalModel() {
-		return originalModel;
-	}
-
-	public void setOriginalModel(EObject originalModel) {
-		this.originalModel = originalModel;
-	}
-
-	public EObject getFakeRefactoredModel() {
-		return fakeRefactoredModel;
-	}
-
-	public void setFakeRefactoredModel(EObject fakeRefactoredModel) {
-		this.fakeRefactoredModel = fakeRefactoredModel;
-	}
-
-	@Override
-	public Control createContents(Composite parent) {
-		// don't change the parent
-		control = super.createContents(parent);
-		return control;
-	}
-
-	public Control getControl() {
-		return control;
-	}
-
-	public void setChange(ModelRefactoringChange change) {
-		this.change = change;
+	public RefactoringCompareEditorInput(Comparison comparison, ICompareEditingDomain editingDomain, AdapterFactory adapterFactory) {
+		super(config, comparison, editingDomain, adapterFactory);
 	}
 }
