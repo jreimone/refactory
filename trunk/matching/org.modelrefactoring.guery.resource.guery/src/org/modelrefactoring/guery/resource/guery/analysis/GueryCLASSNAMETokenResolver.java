62a3870eefc675b6b84e55347021376ce6280eb4
jreimann <jreimann@2290cd21-d4b4-4435-9c65-06e28a4c4f12>
2013-07-26 10:42:26 +0000

    resolution of class name

--- trunk/matching/org.modelrefactoring.guery.resource.guery/src/org/modelrefactoring/guery/resource/guery/analysis/GueryCLASSNAMETokenResolver.java
+++ trunk/matching/org.modelrefactoring.guery.resource.guery/src/org/modelrefactoring/guery/resource/guery/analysis/GueryCLASSNAMETokenResolver.java
@@ -6,23 +6,35 @@
  */
 package org.modelrefactoring.guery.resource.guery.analysis;
 
+import org.eclipse.emf.ecore.EStructuralFeature;
+import org.modelrefactoring.guery.resource.guery.IGueryTokenResolveResult;
+
 public class GueryCLASSNAMETokenResolver implements org.modelrefactoring.guery.resource.guery.IGueryTokenResolver {
-	
+
 	private org.modelrefactoring.guery.resource.guery.analysis.GueryDefaultTokenResolver defaultTokenResolver = new org.modelrefactoring.guery.resource.guery.analysis.GueryDefaultTokenResolver(true);
-	
+
 	public String deResolve(Object value, org.eclipse.emf.ecore.EStructuralFeature feature, org.eclipse.emf.ecore.EObject container) {
+		if(value instanceof Class<?>){
+			return ((Class<?>) value).getName();
+		}
 		// By default token de-resolving is delegated to the DefaultTokenResolver.
-		String result = defaultTokenResolver.deResolve(value, feature, container, null, null, null);
-		return result;
+		return defaultTokenResolver.deResolve(value, feature, container, null, null, null);
 	}
-	
-	public void resolve(String lexem, org.eclipse.emf.ecore.EStructuralFeature feature, org.modelrefactoring.guery.resource.guery.IGueryTokenResolveResult result) {
-		// By default token resolving is delegated to the DefaultTokenResolver.
-		defaultTokenResolver.resolve(lexem, feature, result, null, null, null);
+
+	public void resolve(String lexem, EStructuralFeature feature, IGueryTokenResolveResult result) {
+		ClassLoader classLoader = this.getClass().getClassLoader();
+		try {
+			Class<?> clazz = classLoader.loadClass(lexem);
+			result.setResolvedToken(clazz);
+		} catch (ClassNotFoundException e) {
+			result.setErrorMessage("Class '" + lexem + "' couldn't be loaded");
+			//	By default token resolving is delegated to the DefaultTokenResolver.
+			defaultTokenResolver.resolve(lexem, feature, result, null, null, null);
+		}
 	}
-	
+
 	public void setOptions(java.util.Map<?,?> options) {
 		defaultTokenResolver.setOptions(options);
 	}
-	
+
 }
