4f13f57e393dfbfdc566dea7e8e6f0c93ac10dd4
Jan <jreimone@users.noreply.github.com>
2013-07-30 14:06:52 +0000

    added comparison of results

--- matching/org.modelrefactoring.guery.test/src/org/modelrefactoring/guery/test/MotifSolvingTest.java
+++ matching/org.modelrefactoring.guery.test/src/org/modelrefactoring/guery/test/MotifSolvingTest.java
@@ -2,6 +2,7 @@ package org.modelrefactoring.guery.test;
 
 import static org.junit.Assert.*;
 
+import java.io.ByteArrayOutputStream;
 import java.io.File;
 import java.io.IOException;
 import java.util.ArrayList;
@@ -11,7 +12,6 @@ import java.util.List;
 import nz.ac.massey.cs.guery.ComputationMode;
 import nz.ac.massey.cs.guery.GQL;
 import nz.ac.massey.cs.guery.Motif;
-import nz.ac.massey.cs.guery.MotifInstance;
 import nz.ac.massey.cs.guery.impl.MultiThreadedGQLImpl;
 
 import org.eclipse.emf.common.util.URI;
@@ -24,7 +24,6 @@ import org.emftext.language.pl0.PL0Package;
 import org.emftext.language.refactoring.rolemapping.RoleMapping;
 import org.emftext.language.refactoring.rolemapping.resource.rolemapping.mopp.RolemappingPrinter2;
 import org.emftext.language.refactoring.roles.RoleModel;
-import org.junit.Ignore;
 import org.junit.Test;
 import org.modelrefactoring.guery.GueryFactory;
 import org.modelrefactoring.guery.MotifModel;
@@ -37,29 +36,45 @@ import org.modelrefactoring.matching.guery.RoleModel2MotifConverter;
 
 public class MotifSolvingTest extends MotifAdapterTest {
 	
-	public static final int THRESHOLD 		= 50;
-	private static final int MAX_PATH_LENGTH	= 3;
-	
-//	@Test
-//	public void testPL0ZZZCompare(){
-//		assertNotNull("No instances found for GUERY parsing", instancesGuery);
-//		assertNotNull("No instances found for EMFText parsing", instancesModel);
-//		compare(instancesGuery, instancesModel);
-//	}
+	private static final int THRESHOLD 		= 50;
+	private static final int MAX_PATH_LENGTH	= 1;
 	
 	@Test
-	public void testSimple(){
+	public void testPL0ExtractXwithReferenceClass(){
+		EPackage pl0MM = PL0Package.eINSTANCE;
+		assertNotNull("PL/0 metamodel couldn't be initialised", pl0MM);
+		Resource pl0MMResource = pl0MM.eResource();
+		assertNotNull("Resource for URI '" + pl0MM.getNsURI() +"' mustn't be null", pl0MMResource);
 		RoleModel roleModel = initRoleModel("rolemodels/ExtractXwithReferenceClass.rolestext");
-		EPackage metamodel = getMetamodel("metamodels/EExtractXWithReferenceClass.ecore");
+		testGueryAndEMFTextParsing(roleModel, pl0MM);
+	}
+	
+	@Test
+	public void testSimpleMM(){
+		testGueryAndEMFTextParsing("rolemodels/ExtractXwithReferenceClass.rolestext", "metamodels/EExtractXWithReferenceClass.ecore");
+	}
+
+	private void testGueryAndEMFTextParsing(String roleModelPath, String metamodelPath){
+		RoleModel roleModel = initRoleModel(roleModelPath);
+		EPackage metamodel = initMetamodel(metamodelPath);
+		testGueryAndEMFTextParsing(roleModel, metamodel);
+	}
+
+	private void testGueryAndEMFTextParsing(RoleModel roleModel, EPackage metamodel) {
+		System.out.println("##################################");
+		System.out.println("Metamodel: " + metamodel.getName());
+		System.out.println("RoleModel: " + roleModel.getName());
+		System.out.println("Max PathLength: " + MAX_PATH_LENGTH);
+		System.out.println("Max Results: " + THRESHOLD);
 		List<org.modelrefactoring.guery.Motif> motifs = convertRoleModel2Motifs(roleModel);
 		for (org.modelrefactoring.guery.Motif motif : motifs) {
 			String uriString = motif.eResource().getURI().toFileString();
 			List<RoleMapping> gueryParsingRoleMappings = testMotifOnMetamodelWithGUERYParsing(uriString, metamodel.eResource(), roleModel);
 			List<RoleMapping> emfTextParsingRoleMappings = testMotifOnMetamodelWithEMFTextParsing(uriString, metamodel.eResource(), roleModel);
-			System.out.println("~~~~~~~~~ GUERY Parsing solving results:");
-			printRoleMappings(gueryParsingRoleMappings);
-			System.out.println("~~~~~~~~~ EMFText Parsing solving results:");
-			printRoleMappings(emfTextParsingRoleMappings);
+			System.out.println("Results:\n");
+			List<String> printedGueryRoleMappings = printRoleMappings(gueryParsingRoleMappings);
+			List<String> printedEMFTextRoleMappings = printRoleMappings(emfTextParsingRoleMappings);
+			compare(printedGueryRoleMappings, printedEMFTextRoleMappings);
 		}
 	}
 
@@ -94,29 +109,6 @@ public class MotifSolvingTest extends MotifAdapterTest {
 		return returnedMotifs;
 	}
 	
-	@Test
-	@Ignore
-	public void testPL0ExtractXwithReferenceClassGuery(){
-		EPackage pl0MM = PL0Package.eINSTANCE;
-		assertNotNull("PL/0 metamodel couldn't be initialised", pl0MM);
-		Resource pl0MMResource = pl0MM.eResource();
-		assertNotNull("Resource for URI '" + pl0MM.getNsURI() +"' mustn't be null", pl0MMResource);
-		RoleModel roleModel = initRoleModel("rolemodels/ExtractXwithReferenceClass.rolestext");
-		List<RoleMapping> roleMappingsGuery = testMotifOnMetamodelWithGUERYParsing("queries/ExtractXwithReferenceClass.guery", pl0MMResource, roleModel);
-	}
-
-	
-	@Test
-	@Ignore
-	public void testPL0ExtractXwithReferenceClassModel(){
-		EPackage pl0MM = PL0Package.eINSTANCE;
-		assertNotNull("PL/0 metamodel couldn't be initialised", pl0MM);
-		Resource pl0MMResource = pl0MM.eResource();
-		assertNotNull("Resource for URI '" + pl0MM.getNsURI() +"' mustn't be null", pl0MMResource);
-		RoleModel roleModel = initRoleModel("rolemodels/ExtractXwithReferenceClass.rolestext");
-		List<RoleMapping> roleMappingsEMFText = testMotifOnMetamodelWithEMFTextParsing("queries/ExtractXwithReferenceClass.guery", pl0MMResource, roleModel);
-	}
-	
 	private RoleModel initRoleModel(String path) {
 		File roleModelFile = new File(path);
 		URI uri = URI.createFileURI(roleModelFile.getAbsolutePath());
@@ -144,8 +136,21 @@ public class MotifSolvingTest extends MotifAdapterTest {
 //		testMotifOnMetamodelWithGUERYParsing(motifPath, resource);
 //	}
 	
-	private void compare(List<MotifInstance<MetamodelVertex, EReferenceEdge>> gueryInstances, List<MotifInstance<MetamodelVertex, EReferenceEdge>> modelInstances) {
-		System.out.println("compare them");
+	private void compare(List<String> gueryRoleMappings, List<String> emftextRoleMappings) {
+		for (String gueryRoleMapping : gueryRoleMappings) {
+			System.out.println("GUERY: \n" + gueryRoleMapping);
+			assertTrue("The following RoleMapping resulting from GUERY parsing couldn't be solved with EMFText parsing:"
+					+ "\n"
+					+ gueryRoleMapping, emftextRoleMappings.contains(gueryRoleMapping));
+		}
+		for (String emftextRoleMapping : emftextRoleMappings) {
+			if(!gueryRoleMappings.contains(emftextRoleMapping)){
+				System.out.println("EMFText: \n" + emftextRoleMapping);
+				fail("The following RoleMapping resulting from EMFText parsing couldn't be solved with GUERY parsing:"
+						+ "\n"
+						+ emftextRoleMapping);
+			}
+		}
 	}
 
 	private List<RoleMapping> solveMotifOnResource(Motif<MetamodelVertex, EReferenceEdge> motif, Resource resource, final RoleModel roleModel){
@@ -157,19 +162,24 @@ public class MotifSolvingTest extends MotifAdapterTest {
 		return foundRoleMappings;
 	}
 	
-	private void printRoleMappings(List<RoleMapping> roleMappings) {
+	private List<String> printRoleMappings(List<RoleMapping> roleMappings) {
+		List<String> printedRoleMappings = new ArrayList<String>();
 		for (RoleMapping roleMapping : roleMappings) {
-			RolemappingPrinter2 printer = new RolemappingPrinter2(System.out, null);
-//			RolemappingPrinter printer = new RolemappingPrinter(System.out, null);
+			ByteArrayOutputStream out = new ByteArrayOutputStream();
+			RolemappingPrinter2 printer = new RolemappingPrinter2(out, null);
+//			RolemappingPrinter printer = new RolemappingPrinter(out, null);
 			try {
 				printer.print(roleMapping);
+				out.close();
+				printedRoleMappings.add(out.toString());
 			} catch (IOException e) {
 				e.printStackTrace();
 			}
 		}
+		return printedRoleMappings;
 	}
 
-	private EPackage getMetamodel(String path){
+	private EPackage initMetamodel(String path){
 		File file = new File(path);
 		assertTrue("File '" + path + "' doesn't exist", file.exists());
 		URI uri = URI.createFileURI(file.getAbsolutePath());
