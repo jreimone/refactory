d208bd15a62bc1e078c88e21242cb61c76c80b18
jreimann <jreimann@2290cd21-d4b4-4435-9c65-06e28a4c4f12>
2014-02-19 10:56:34 +0000

    added test project for demonstration of JaMoPP refactorings

---
+++ trunk/Tests/org.modelrefactoring.jamopp.test/input/SimpleExtractMethodTestClass.java
@@ -0,0 +1,12 @@
+package org.modelrefactoring.jamopp.test;
+
+public class SimpleExtractMethodTestClass {
+
+	public static void main(String[] args) {
+		System.out.println("Hello Karlsruhe");
+		for (int i = 0; i < 10; i++) {
+			System.out.println("value: " + i);
+		}
+	}
+
+}
