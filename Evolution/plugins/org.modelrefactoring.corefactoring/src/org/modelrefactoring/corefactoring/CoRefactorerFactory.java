5f0dd54786e6c46757a2b44b1c7e357cf23a312f
Jan <jreimone@users.noreply.github.com>
2014-03-05 14:30:27 +0000

    further API changes

--- Evolution/plugins/org.modelrefactoring.corefactoring/src/org/modelrefactoring/corefactoring/CoRefactorerFactory.java
+++ Evolution/plugins/org.modelrefactoring.corefactoring/src/org/modelrefactoring/corefactoring/CoRefactorerFactory.java
@@ -1,5 +1,57 @@
 package org.modelrefactoring.corefactoring;
 
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.Collection;
+import java.util.Collections;
+import java.util.HashMap;
+import java.util.HashSet;
+import java.util.List;
+import java.util.Map;
+import java.util.Set;
+
+import org.eclipse.emf.common.util.URI;
+import org.eclipse.emf.ecore.EObject;
+import org.eclipse.emf.ecore.resource.Resource;
+import org.emftext.language.refactoring.roles.Role;
+import org.emftext.refactoring.interpreter.IRefactorer;
+import org.emftext.refactoring.interpreter.IRefactoringInterpreter;
+import org.emftext.refactoring.interpreter.RefactorerFactory;
+import org.modelrefactoring.evolution.registry.IKnowledgeBase;
+import org.modelrefactoring.evolution.registry.IKnowledgeBaseRegistry;
+
 public class CoRefactorerFactory {
 
+	/**
+	 * Creates a list of co-refactorers dependent on the given <code>refactorer</code>.
+	 * Each co-refactorer is for one (initial model)-(dependent model) combination.
+	 * @param refactorer
+	 */
+	public List<CoRefactorer> getCoRefactorers(IRefactorer refactorer) {
+		IRefactoringInterpreter interpreter = refactorer.getInterpreter();
+		List<EObject> boundElements = new ArrayList<EObject>();
+		Map<Role, List<EObject>> roleBindings = interpreter.getRoleRuntimeInstances();
+		for (Role role : roleBindings.keySet()) {
+			boundElements.addAll(roleBindings.get(role));
+		}
+		List<EObject> input = refactorer.getInput();
+		// TODO use bound elements or input???
+		Map<EObject, Collection<EObject>> dependencyMap = new HashMap<EObject, Collection<EObject>>();
+		List<IKnowledgeBase> knowledgeBases = IKnowledgeBaseRegistry.INSTANCE.getKnowledgeBases();
+		for (IKnowledgeBase knowledgeBase : knowledgeBases) {
+			for (EObject element : boundElements) {
+				Resource resource = element.eResource();
+				URI uri = resource.getURI();
+				dependencyMap.putAll(knowledgeBase.getDependencies(uri, resource.getResourceSet()));
+			}
+		}
+		List<CoRefactorer> coRefactorers = new ArrayList<CoRefactorer>();
+		for (EObject dependentModel : dependencyMap.keySet()) {
+			Collection<EObject> dependentElements = dependencyMap.get(dependentModel);
+			List<EObject> elements = Arrays.asList(dependentElements.toArray(new EObject[]{}));
+			CoRefactorer coRefactorer = new CoRefactorerImpl(refactorer, dependentModel, elements);
+			coRefactorers.add(coRefactorer);
+		}
+		return coRefactorers;
+	}
 }
