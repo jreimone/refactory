4a91b6b6bd5a98ec7c25ea03bb1868da5838b135
Jan <jreimone@users.noreply.github.com>
2014-03-27 10:03:00 +0000

    fixed package navigation resolving

--- Plugins/org.emftext.language.refactoring.rolemapping.resource.rolemapping/src/org/emftext/language/refactoring/rolemapping/resource/rolemapping/analysis/ConcreteMappingPackagesOfMetaclassReferenceResolver.java
+++ Plugins/org.emftext.language.refactoring.rolemapping.resource.rolemapping/src/org/emftext/language/refactoring/rolemapping/resource/rolemapping/analysis/ConcreteMappingPackagesOfMetaclassReferenceResolver.java
@@ -6,62 +6,93 @@
  */
 package org.emftext.language.refactoring.rolemapping.resource.rolemapping.analysis;
 
-import java.util.ArrayList;
+import java.util.HashMap;
 import java.util.List;
+import java.util.Map;
 
 import org.eclipse.emf.ecore.EObject;
 import org.eclipse.emf.ecore.EPackage;
+import org.eclipse.emf.ecore.EReference;
+import org.eclipse.emf.ecore.util.EcoreUtil;
+import org.emftext.language.refactoring.rolemapping.ConcreteMapping;
 import org.emftext.language.refactoring.rolemapping.RoleMappingModel;
+import org.emftext.language.refactoring.rolemapping.resource.rolemapping.IRolemappingReferenceResolveResult;
+import org.emftext.language.refactoring.rolemapping.resource.rolemapping.IRolemappingReferenceResolver;
 
-public class ConcreteMappingPackagesOfMetaclassReferenceResolver implements org.emftext.language.refactoring.rolemapping.resource.rolemapping.IRolemappingReferenceResolver<org.emftext.language.refactoring.rolemapping.ConcreteMapping, org.eclipse.emf.ecore.EPackage> {
-	
-	public void resolve(String identifier, org.emftext.language.refactoring.rolemapping.ConcreteMapping container, org.eclipse.emf.ecore.EReference reference, int position, boolean resolveFuzzy, final org.emftext.language.refactoring.rolemapping.resource.rolemapping.IRolemappingReferenceResolveResult<org.eclipse.emf.ecore.EPackage> result) {
-		EObject parent = container.eContainer().eContainer();
+public class ConcreteMappingPackagesOfMetaclassReferenceResolver implements IRolemappingReferenceResolver<ConcreteMapping, EPackage> {
+
+	public void resolve(String identifier, ConcreteMapping concreteMapping, EReference reference, int position, boolean resolveFuzzy, final IRolemappingReferenceResolveResult<EPackage> result) {
+		EObject parent = EcoreUtil.getRootContainer(concreteMapping, true);
+		// new
 		if(parent instanceof RoleMappingModel){
-			RoleMappingModel roleMappingModel = (RoleMappingModel) parent;
-			EPackage targetMetamodel = roleMappingModel.getTargetMetamodel();
-			List<EPackage> imports = roleMappingModel.getImports();
-			List<EPackage> validMetamodels = new ArrayList<EPackage>();
-			List<EPackage> packagesOfMetaclass = new ArrayList<EPackage>(container.getPackagesOfMetaclass());
-			if(packagesOfMetaclass.size() == 0){
-				validMetamodels.add(targetMetamodel);
-				validMetamodels.addAll(imports);
-			} else {
-				EPackage lastMetamodel = packagesOfMetaclass.get(packagesOfMetaclass.size() - 1);
-				if(lastMetamodel.eIsProxy()){ // this is the metamodel for the metaclass intended to resolve here - so it must be a proxy
-					if(packagesOfMetaclass.size() > 1){
-						lastMetamodel = packagesOfMetaclass.get(packagesOfMetaclass.size() - 2);
-						validMetamodels.addAll(lastMetamodel.getESubpackages());
-					} else {
-						validMetamodels.add(targetMetamodel);
-						validMetamodels.addAll(targetMetamodel.getESubpackages());
-						validMetamodels.addAll(imports);
-					}
+			RoleMappingModel mappingModel = (RoleMappingModel) parent;
+			EPackage targetMetamodel = mappingModel.getTargetMetamodel();
+			int index = -1;
+			List<EPackage> packagesOfMetaclass = concreteMapping.getPackagesOfMetaclass();
+			for (int i = 0; i < packagesOfMetaclass.size(); i++) {
+				EPackage epackage = packagesOfMetaclass.get(i);
+				if(epackage.eIsProxy()){
+					index = i;
+					break;
 				}
 			}
-			if(resolveFuzzy){
-				for (EPackage validMetamodel : validMetamodels) {
-					result.addMapping(validMetamodel.getName(), validMetamodel);
-				}
+			Map<String, EPackage> validPackages = null;
+			if(index == -1 || index == 0){
+				validPackages = getValidPackages(mappingModel.getImports(), targetMetamodel);
 			} else {
-				for (EPackage validMetamodel : validMetamodels) {
-					if(validMetamodel.getName().equals(identifier)){
-						result.addMapping(identifier, validMetamodel);
-						return;
-					}
-				}
+				EPackage contextPackage = packagesOfMetaclass.get(index - 1);
+				validPackages = getValidPackages(null, contextPackage);
 			}
-			result.setErrorMessage("No metamodel found for '" + identifier + "'");
+			evaluatePackageName(identifier, resolveFuzzy, result, validPackages);
+			return;
+		} else {
+			result.setErrorMessage("Root container of this model is not a " + RoleMappingModel.class.getSimpleName());
+			return;
 		}
 	}
-	
-	public String deResolve(org.eclipse.emf.ecore.EPackage element, org.emftext.language.refactoring.rolemapping.ConcreteMapping container, org.eclipse.emf.ecore.EReference reference) {
+
+	protected void evaluatePackageName(String identifier, boolean resolveFuzzy, IRolemappingReferenceResolveResult<EPackage> result, Map<String, EPackage> validPackages) {
+		if(resolveFuzzy){
+			for (String packageName : validPackages.keySet()) {
+				result.addMapping(packageName, validPackages.get(packageName));
+			}
+		} else {
+			EPackage ePackage = validPackages.get(identifier);
+			if(ePackage != null){
+				result.addMapping(identifier, ePackage);
+			} else {
+				result.setErrorMessage("No package '" + identifier + "' available");
+			}
+		}
+	}
+
+	private Map<String, EPackage> getValidPackages(List<EPackage> additionalPackages, EPackage contextPackage) {
+		Map<String, EPackage> validPackages = new HashMap<String, EPackage>();
+		if(additionalPackages != null){
+			validPackages.put(contextPackage.getName(), contextPackage);
+			for (EPackage ePackage : additionalPackages) {
+				if(!ePackage.eIsProxy()){
+					validPackages.put(ePackage.getName(), ePackage);
+				}
+			}
+		} else {
+			List<EPackage> subpackages = contextPackage.getESubpackages();
+			for (EPackage subPackage : subpackages) {
+				if(!subPackage.eIsProxy()){
+					validPackages.put(subPackage.getName(), subPackage);
+				}
+			}
+		}
+		return validPackages;
+	}
+
+	public String deResolve(EPackage element, ConcreteMapping container, EReference reference) {
 		return element.getName();
 	}
-	
+
 	public void setOptions(java.util.Map<?,?> options) {
 		// save options in a field or leave method empty if this resolver does not depend
 		// on any option
 	}
-	
+
 }
