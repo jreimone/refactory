b02529f851375007b3bc8e4ea0cb0c69d24ac554
Jan <jreimone@users.noreply.github.com>
2014-04-25 10:12:06 +0000

    removed unused imports

--- Plugins/org.emftext.refactoring.registry.rolemapping/src/org/emftext/refactoring/registry/rolemapping/AbstractRefactoringPostProcessor.java
+++ Plugins/org.emftext.refactoring.registry.rolemapping/src/org/emftext/refactoring/registry/rolemapping/AbstractRefactoringPostProcessor.java
@@ -15,7 +15,6 @@
  ******************************************************************************/
 package org.emftext.refactoring.registry.rolemapping;
 
-import java.util.ArrayList;
 import java.util.List;
 import java.util.Map;
 
