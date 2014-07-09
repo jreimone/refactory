0357faacbef61ec9fde1e1838b43e05cf869217a
jreimann <jreimann@2290cd21-d4b4-4435-9c65-06e28a4c4f12>
2014-07-09 13:23:49 +0000

    added copyright

--- trunk/tools/org.modelrefactoring.team.svn.ui.compare.adapter/src/org/modelrefactoring/team/svn/ui/compare/adapter/ResourceElementIFileRevisionAdapterFactory.java
+++ trunk/tools/org.modelrefactoring.team.svn.ui.compare.adapter/src/org/modelrefactoring/team/svn/ui/compare/adapter/ResourceElementIFileRevisionAdapterFactory.java
@@ -1,3 +1,13 @@
+/*******************************************************************************
+ * Copyright (c) 2014 Jan Reimann.
+ * All rights reserved. This program and the accompanying materials
+ * are made available under the terms of the Eclipse Public License v1.0
+ * which accompanies this distribution, and is available at
+ * http://www.eclipse.org/legal/epl-v10.html
+ * 
+ * Contributors:
+ *     Jan Reimann - initial API and implementation
+ *******************************************************************************/
 package org.modelrefactoring.team.svn.ui.compare.adapter;
 
 import org.eclipse.core.runtime.IAdapterFactory;
@@ -9,7 +19,7 @@ public class ResourceElementIFileRevisionAdapterFactory implements IAdapterFacto
 
 	@Override
 	public Object getAdapter(Object adaptableObject, @SuppressWarnings("rawtypes") Class adapterType) {
-		if(adapterType != null && adaptableObject != null && adapterType.equals(IFileRevision.class) && adaptableObject instanceof ResourceElement){
+		if (IFileRevision.class.equals(adapterType) && adaptableObject instanceof ResourceElement){
 			ResourceElement resourceElement = (ResourceElement) adaptableObject;
 			IRepositoryResource repositoryResource = resourceElement.getRepositoryResource();
 			if(repositoryResource != null){
