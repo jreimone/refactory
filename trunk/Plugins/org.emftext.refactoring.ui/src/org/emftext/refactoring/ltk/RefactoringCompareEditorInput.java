052b4817b03d02277b34acb3a08f60278b0c80fd
jreimann <jreimann@2290cd21-d4b4-4435-9c65-06e28a4c4f12>
2015-02-27 12:48:00 +0000

    better preview

--- trunk/Plugins/org.emftext.refactoring.ui/src/org/emftext/refactoring/ltk/RefactoringCompareEditorInput.java
+++ trunk/Plugins/org.emftext.refactoring.ui/src/org/emftext/refactoring/ltk/RefactoringCompareEditorInput.java
@@ -17,18 +17,24 @@ package org.emftext.refactoring.ltk;
 
 import org.eclipse.compare.CompareConfiguration;
 import org.eclipse.emf.common.notify.AdapterFactory;
+import org.eclipse.emf.common.notify.Notifier;
 import org.eclipse.emf.compare.Comparison;
 import org.eclipse.emf.compare.domain.ICompareEditingDomain;
 import org.eclipse.emf.compare.ide.ui.internal.configuration.EMFCompareConfiguration;
 import org.eclipse.emf.compare.ide.ui.internal.editor.ComparisonEditorInput;
+import org.eclipse.emf.compare.scope.IComparisonScope;
+import org.eclipse.emf.edit.ui.provider.AdapterFactoryLabelProvider;
+import org.eclipse.swt.graphics.Image;
 
 @SuppressWarnings("restriction")
 public class RefactoringCompareEditorInput extends ComparisonEditorInput {
 
 	private EMFCompareConfiguration configuration;
+	private IComparisonScope scope;
 
-	public RefactoringCompareEditorInput(Comparison comparison, ICompareEditingDomain editingDomain, AdapterFactory adapterFactory) {
+	public RefactoringCompareEditorInput(IComparisonScope scope, Comparison comparison, ICompareEditingDomain editingDomain, AdapterFactory adapterFactory) {
 		super(new EMFCompareConfiguration(new CompareConfiguration()), comparison, editingDomain, adapterFactory);
+		this.scope = scope;
 		initCompareConfiguration();
 	}
 
@@ -36,5 +42,23 @@ public class RefactoringCompareEditorInput extends ComparisonEditorInput {
 		configuration = getCompareConfiguration();
 		configuration.setLeftEditable(false);
 		configuration.setRightEditable(false);
+		AdapterFactory adapterFactory = getAdapterFactory();
+		AdapterFactoryLabelProvider labelProvider = new AdapterFactoryLabelProvider(adapterFactory);
+		Notifier original = scope.getRight();
+		Notifier fake = scope.getLeft();
+		Image originalImage = labelProvider.getImage(original);
+		Image fakeImage = labelProvider.getImage(fake);
+		String originalLabel = labelProvider.getText(original);
+		String fakeLabel = labelProvider.getText(fake);
+		configuration.setLeftImage(fakeImage);
+		configuration.setLeftLabel("[after refactoring] " + fakeLabel);
+		configuration.setRightImage(originalImage);
+		configuration.setRightLabel("[before refactoring] " + originalLabel);
 	}
+
+	@Override
+	public EMFCompareConfiguration getCompareConfiguration() {
+		return super.getCompareConfiguration();
+	}
+	
 }
